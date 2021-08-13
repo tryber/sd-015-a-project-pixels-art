fc51bf214d10e29d1a70b197eba7d35435e3eeea
douglas oliveira <douglasgama0@gmail.com>
2021-08-13 17:00:44 -0300

    Criando funçao para os requisitos 1 2 e 3

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let color = document.querySelectorAll('.color');
-for (let index = 0; index < color.length; index++) {
-  if (color[index] === color[0]) {
-    color[index].style.backgroundColor = 'black';
-  } else if (color[index] === color[1]) {
-    color[index].style.backgroundColor = '#FFB703';
-  } else if (color[index] === color[2]) {
-    color[index].style.backgroundColor = '#023047';
-  } else if (color[index] === color[3]) {
-    color[index].style.backgroundColor = '#FB8500';
+function palletColor() {
+  const color = document.querySelectorAll('.color');
+  for (let index = 0; index < color.length; index += 1) {
+    if (color[index] === color[0]) {
+      color[index].style.backgroundColor = 'black';
+    } else if (color[index] === color[1]) {
+      color[index].style.backgroundColor = '#FFB703';
+    } else if (color[index] === color[2]) {
+      color[index].style.backgroundColor = '#023047';
+    } else if (color[index] === color[3]) {
+      color[index].style.backgroundColor = '#FB8500';
+    }
   }
 }
+palletColor();
