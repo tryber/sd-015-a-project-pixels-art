9e6f30749a0671eb1bda8a805aa456343bb6a8c5
guilhermecd15 <guilhermecd15@gmail.com>
2021-08-13 15:43:17 -0300

    4 - Adicione à página um quadro de pixels, com 25 pixels. 5 - Faça com que cada elemento do quadro de pixels possua 40 pixels de largura, 40 pixels de altura e seja delimitado por uma borda preta de 1 pixel.

--- script.js
+++ script.js
@@ -4,16 +4,29 @@ let b = 0;
 
 function criarPaleta() {
   const colorPalette = document.querySelector('#color-palette');
-  for(i = 1; i <5; i += 1){
-  let colorir = `rgb(${r},${g},${b})`;
-  const color = document.createElement('div');
-  color.className = 'color'
-  color.style.backgroundColor = colorir;
-  colorPalette.appendChild(color);
-  r = Math.floor(Math.random()*255);
-  g = Math.floor(Math.random()*255);
-  b = Math.floor(Math.random()*255);
+  for (i = 1; i < 5; i += 1) {
+    let colorir = `rgb(${r},${g},${b})`;
+    const color = document.createElement('div');
+    color.className = 'color'
+    color.style.backgroundColor = colorir;
+    colorPalette.appendChild(color);
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
   }
 }
 
 criarPaleta();
+
+function criarPixel(){
+  const pixelBoard = document.querySelector('#pixel-board');
+  for (l = 1; l < 6; l += 1){
+    for (c = 1; c < 6; c += 1){
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixelBoard.appendChild(pixel);
+    } 
+  }
+}
+
+criarPixel();
