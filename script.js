7ed766a99697f6202ee4b0a2a216f24e17d19c13
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-13 07:04:03 -0300

    Requisito 9 ao 11 feitos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const colorList = document.getElementsByClassName('color');
 const colorContainer = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
+const buttonClear = document.getElementById('clear-board');
+const buttonSize = document.getElementById('generate-board');
 
 function clearSelected() {
   for (let index = 0; index < colorList.length; index += 1) {
@@ -25,3 +27,39 @@ pixelBoard.addEventListener('click', (event) => {
     pixel.target.style.backgroundColor = color;
   }
 }, false);
+
+// Clear colors
+buttonClear.addEventListener('click', () => {
+  const pixels = document.getElementsByClassName('pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+});
+
+function makeBoard(sizeInput) {
+  let size = sizeInput;
+  pixelBoard.innerHTML = '';
+  if (size < 5) {
+    size = 5;
+  }
+  if (size > 50) {
+    size = 50;
+  }
+  pixelBoard.style.width = `${40 * size}px`;
+  pixelBoard.style.height = pixelBoard.style.width;
+  for (let i = 0; i < (size * size); i += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
+}
+
+// Change size
+buttonSize.addEventListener('click', () => {
+  const sizeInput = document.getElementById('board-size');
+  if (!sizeInput.value) {
+    alert('Board inválido!');
+  } else {
+    makeBoard(sizeInput.value);
+  }
+});
