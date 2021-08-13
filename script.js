d01ea056fd3d64e4702574824911eacfe7bda0ca
Anderson Alencar <anderson23alencar@gmail.com>
2021-08-13 17:58:43 -0300

    Requisito 7 - seleciona cor ao clicar na paleta

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function selectedColor () {
-  
+let selectedColor = document.querySelectorAll('.color');
+
+for (let color of selectedColor) {
+  color.addEventListener('click', function() {
+    for (let color of selectedColor){
+      color.classList.remove('selected');
+    } 
+    color.classList.add('selected');
+  });
 }
