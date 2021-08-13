e041c5ef6c7fd745d4f7f5f8edd934a2799256ed
Matheus <mathspa98@gmail.com>
2021-08-13 16:00:50 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
  window.onload=function(){
      let boardSize=5
      let pBoard= document.getElementById("pixel-board");
-
+     paletaCores()
      montaBoard(boardSize);
 
-
+    function paletaCores(){
+        let first= document.getElementById("first").style.backgroundColor="black";
+        firstPreta();
+        let second= document.getElementById("second").style.backgroundColor="aqua";
+        let third= document.getElementById("third").style.backgroundColor="red";
+        let fourth= document.getElementById("fourth").style.backgroundColor="green";
+    }
+    function firstPreta(){
+        let corInicial= document.getElementById("first");
+        let cor= corInicial.className;
+        cor= cor + " selected";
+        corInicial.className=cor;
+    }
      function montaBoard(numero){
 
          for(let i=0; i<numero; i+=1){
