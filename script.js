3f5b55bc649c73499da1a1f1c4bc9355858dfa99
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-08-13 15:57:36 -0300

    Setimo requisito

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const paletaDeCores = ['black', 'red', 'green', 'blue'];
+const colorSlot = document.getElementById('color-palette').children;
 
 window.onload = function () {
   function definirPaletaDeCores() {
-    const colorSlot = document.getElementById('color-palette').children;
-
     for (let index = 0; index < colorSlot.length; index += 1) {
       colorSlot[index].style.backgroundColor = paletaDeCores[index];
     }
@@ -13,3 +12,20 @@ window.onload = function () {
 
   document.querySelector('.color').classList.add('selected');
 };
+
+function adicionarEventos(elemento, evento, funcao) {
+  if(elemento.length > 0) {
+    for(let index = 0; index < elemento.length; index += 1) {
+      elemento[index].addEventListener(evento, funcao);
+    }
+  } else {
+    elemento.addEventListener(evento, funcao);
+  }
+}
+
+function mudarCorPaleta(evento) {
+  document.querySelector('.selected').classList.remove('selected');
+  evento.target.classList.add('selected');
+}
+
+adicionarEventos(colorSlot, 'click', mudarCorPaleta);
