c27ce75921a171d5392370245ec6fe4b56d3aa31
douglas oliveira <douglasgama0@gmail.com>
2021-08-13 17:49:47 -0300

    Atualizando primeira cor da paleta de cores, requisito 3, e finalizaçao do requisito 4

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ function palletColor() {
   const color = document.querySelectorAll('.color');
   for (let index = 0; index < color.length; index += 1) {
     if (color[index] === color[0]) {
-      color[index].style.backgroundColor = 'black';
+      color[index].style.backgroundColor = '#000000';
     } else if (color[index] === color[1]) {
       color[index].style.backgroundColor = '#FFB703';
     } else if (color[index] === color[2]) {
-      color[index].style.backgroundColor = '#023047';
+      color[index].style.backgroundColor = '#219EBC';
     } else if (color[index] === color[3]) {
-      color[index].style.backgroundColor = '#FB8500';
+      color[index].style.backgroundColor = '#90E0EF';
     }
   }
 }
 palletColor();
+
+const boxPixel = document.getElementById('pixel-board');
+const elementBox = document.createElement('li');
+
+for (let i = 1; i <= 25; i += 1) {
+  const elementBox = document.createElement('li');
+  elementBox.classList = 'pixel';
+  elementBox.style.backgroundColor = 'white';
+  boxPixel.appendChild(elementBox);
+}
