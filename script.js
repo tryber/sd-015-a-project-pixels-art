928ae6738e78b4a072a8cf3831533eca564c0ebd
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-08-13 18:53:15 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-let divs = document.querySelectorAll('.color')
-for (let i = 0; i < divs.length; i += 1){
-    if (divs[i] === divs[0]) {
-        divs[i].style.backgroundColor = '#000000';
-    } else if(divs[i] === divs[1]) {
-        divs[i].style.backgroundColor = '#ef233c';
-    } else if(divs[i] === divs[2]) {
-        divs[i].style.backgroundColor = '#9ae19d';
-    } else if(divs[i] === divs[3]) {
-        divs[i].style.backgroundColor = '#564d80';
-    }
+function cores() {
+const divs = document.querySelectorAll('.color');
+for (let i = 0; i < divs.length; i += 1) {
+  if (divs[i] === divs[0]) {
+    divs[i].style.backgroundColor = '#000000';
+  } else if (divs[i] === divs[1]) {
+    divs[i].style.backgroundColor = '#ef233c';
+  } else if (divs[i] === divs[2]) {
+    divs[i].style.backgroundColor = '#9ae19d';
+  } else if (divs[i] === divs[3]) {
+    divs[i].style.backgroundColor = '#564d80';
+  }
+ }
 }
+cores();
+const pixelsId = document.getElementById('pixel-board')
+const pixels = document.createElement('li');
 
-
-
+for(let i = 0; i < 25; i += 1){
+  const pixels = document.createElement('li');
+  pixels.classList = 'pixel';
+  pixels.style.backgroundColor = 'white'
+  pixelsId.appendChild(pixels) 
+}
