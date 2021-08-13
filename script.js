e2858576fd3164d291a909934e4becd1887a7d6e
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-13 16:55:49 -0300

    Requisito Número 06

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let pixelsBoard = document.createElement("div");
 pixelsBoard.id = "pixel-board";
 document.body.appendChild(pixelsBoard);
+function pixels25() {
 for(let countPx = 1; countPx <= 25; countPx += 1) {
 let board = document.createElement("div");
 board.className = "pixel";
 pixelsBoard.appendChild(board);
 }
+} pixels25();
+
+let firstColor = document.querySelector(".black");
+function changeBlackClass() {
+    firstColor.className += " selected";
+} changeBlackClass();
