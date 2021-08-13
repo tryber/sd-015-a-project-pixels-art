61598820d417a885fe287017bbc66b6dbda5f91e
Thiago <thiago_ssfr@hotmail.com>
2021-08-13 16:49:40 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const quadroPixel = document.getElementById('pixel-board');
+const corSelected = document.querySelector('.selected');
 const numberOfLines = 5;
 const numberOfColums = 5;
+
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
+const black = document.getElementById('black');
 
 numberLines(numberOfLines);
 
 const lines = document.querySelectorAll('.line');
 
 createBox(lines, numberOfColums);
+//corSelected.addEventListener('click', trocaClass);
 
-function numberLines(number) {       
+function numberLines(number) {
   for (let index = 0; index < number; index += 1) {
     const div = document.createElement('div');
     div.className = 'line';
@@ -16,7 +23,7 @@ function numberLines(number) {
   }
 }
 
-function createBox(lines,colunas){
+function createBox(lines, colunas) {
   for (let index = 0; index < lines.length; index += 1) {
     for (let j = 0; j < colunas; j += 1) {
       const positionLine = lines[index];
@@ -26,3 +33,22 @@ function createBox(lines,colunas){
     }
   }
 }
+
+function trocaClass() {
+  if (corSelected.className === 'color') {
+    corSelected.className = 'color selected';
+  } else {
+    corSelected.className = 'color';
+  }
+}
+
+function trocaSelecionaDay(event) {
+  const techElement = document.querySelector('.selected');
+  techElement.classList.remove('selected');
+  event.target.classList.add('selected');  
+}
+
+red.addEventListener('click', trocaSelecionaDay);
+green.addEventListener('click', trocaSelecionaDay);
+blue.addEventListener('click', trocaSelecionaDay);
+black.addEventListener('click', trocaSelecionaDay);
