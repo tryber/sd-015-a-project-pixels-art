8863c9620d3848da9a8b9f1a61b8ee03118f5a90
Neander Menezes <exemplo@seuemail.com.br>
2021-08-13 18:10:07 -0300

    exercicio 7 feito

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ window.onload = function() {
   const firstColorBlack = document.querySelector('#black');
   firstColorBlack.className = classSelect;
 }
+
+
+function selectColor() {
+  const colors = document.getElementById('color-palette')
+  colors.addEventListener('click', function(event) {
+    const selectColor = 'color selected';
+    const unselectColors = 'color';
+    const colorsPath = document.querySelector('#color-palette');
+
+    for (let i = 0; i < colorsPath.children.length; i += 1) {
+      let currentColor = colorsPath.children[i];
+      currentColor.className = unselectColors;
+    }
+    event.target.className = selectColor;
+  })
+}
+
+
+
+selectColor()
