49b541dda44ac3c3ad91470e55fb503fc092ffd6
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-08-13 19:19:20 -0300

    Exercise 8 solved

--- script.js
+++ script.js
@@ -2,31 +2,40 @@ window.onload = function colors() {
     for(let i = 0; i < 25; i += 1) {
         document.getElementsByClassName('pixel')[i].style.backgroundColor = 'white'
     }
-}
 
-paletteListener();
-pixelBoardListener();
+    const firstColor = document.getElementById('firstColor')
+    firstColor.style.backgroundColor = 'black';
+    const secondColor = document.getElementById('secondColor')
+    secondColor.style.backgroundColor = 'green';
+    const thirdColor = document.getElementById('thirdColor')
+    thirdColor.style.backgroundColor = 'yellow';
+    const fourthColor = document.getElementById('fourthColor')
+    fourthColor.style.backgroundColor = 'purple';
 
-function selectColor(paletteClick) {
-    let selected = document.getElementsByClassName('selected')[0];
-    selected.classList.remove('selected');
-    paletteClick.target.classList.add('selected');
-}
+    paletteListener();
+    pixelBoardListener();
 
-function printColor(pixelClick) {
-    pixelClick.target.style.backgrounColor = selected.style.backgrounColor;
-}
+    function selectColor(paletteClick) {
+        let selected = document.getElementsByClassName('selected')[0];
+        selected.classList.remove('selected');
+        paletteClick.target.classList.add('selected');
+    }
 
-function paletteListener() {
-    let colorPalette = document.getElementsByClassName('color');
-    for(let i = 0; i < colorPalette.length; i += 1) {
-        colorPalette[i].addEventListener('click', selectColor);
-    } 
-}
+    function printColor(pixelClick) {
+        pixelClick.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+    }
 
-function pixelBoardListener() {
-    let pixelBoard = document.getElementsByClassName('pixel');
-    for(let i = 0; i < pixelBoard.length; i += 1) {
-        pixelBoard[i].addEventListener('click', printColor);
+    function paletteListener() {
+        let colorPalette = document.getElementsByClassName('color');
+        for(let i = 0; i < colorPalette.length; i += 1) {
+            colorPalette[i].addEventListener('click', selectColor);
+        } 
+    }
+
+    function pixelBoardListener() {
+        let pixelBoard = document.getElementsByClassName('pixel');
+        for(let i = 0; i < pixelBoard.length; i += 1) {
+            pixelBoard[i].addEventListener('click', printColor);
+        }
     }
 }
