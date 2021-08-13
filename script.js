30e368938ed3ff5ba21f2f0b567d7d579d7bbfc8
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-08-13 17:17:19 -0300

    feitas correçoes no lint.

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ function criarDivsDasCores() {
 criarDivsDasCores();
 // cores atribuidas resgatando cada elemento nó e atribuindo cor de fundo. caracteristicas gerais no css
 const color1 = document.querySelectorAll('.color')[0];
-color1.style.backgroundColor = 'black'
+color1.style.backgroundColor = 'black';
 const color2 = document.querySelectorAll('.color')[1];
-color2.style.backgroundColor = 'red'
+color2.style.backgroundColor = 'red';
 const color3 = document.querySelectorAll('.color')[2];
-color3.style.backgroundColor = 'blue'
+color3.style.backgroundColor = 'blue';
 const color4 = document.querySelectorAll('.color')[3];
 color4.style.backgroundColor = 'green';
 
 // criar os quadros de pixels
 
 function criarOQuadroDeArt() {
-  let divDoQuadro = document.querySelector('#pixel-board');
+  const divDoQuadro = document.querySelector('#pixel-board');
 
   for (let index = 0; index < 5; index += 1) {
     for (let index2 = 0; index2 < 5; index2 += 1) {
-      let quadroPixel = document.createElement('div');
+      const quadroPixel = document.createElement('div');
       quadroPixel.classList = 'pixel';
       divDoQuadro.appendChild(quadroPixel);
     }
