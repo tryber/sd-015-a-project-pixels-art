4898b816eacfbc168885f86168e5a87c1f2bfb9a
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-08-13 16:03:43 -0300

    exercicio 4

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-// divPallet.innerHTML = 'divtext'
+
 
 let cores = ['black', 'blue', 'green', 'purple']
 
 for (index = 0; index < cores.length; index +=1){
-  let section1 = document.createElement('div')
-  let divPallet = document.querySelector('#color-palette').appendChild(section1)
-  divPallet.className = 'color' 
-  divPallet.style.backgroundColor = cores[index]
-  divPallet.innerHTML = cores[index]
+  let motherPalette = document.createElement('div')
+  let divPalette = document.querySelector('#color-palette').appendChild(motherPalette)
+  divPalette.className = 'color' 
+  divPalette.style.backgroundColor = cores[index]
+  divPalette.innerHTML = cores[index]
+}
+
+for (idx = 0; idx < 5; idx +=1){
+  let motherPixel = document.createElement('div')
+  let eachPixel = document.querySelector('#pixel-board').appendChild(motherPixel)
+  eachPixel.innerHTML = 'oi'
+  eachPixel.className = 'pixel'
+  for (idx2 = 0; idx2 < 4; idx2 +=1) {
+  let motherPixel2 = document.createElement('div')
+  let eachPixel2 = document.querySelector('#pixel-board').appendChild(motherPixel2)
+  eachPixel2.innerHTML = 'oi'
+  eachPixel2.className = 'pixel'
+  }
+
 }
