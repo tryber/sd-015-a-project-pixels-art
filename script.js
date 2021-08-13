22a9811ba105f46af1782317766601d437d6a58c
guilhermecd15 <guilhermecd15@gmail.com>
2021-08-13 16:01:16 -0300

    6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ let r = 0;
 let g = 0;
 let b = 0;
 
+
+window.onload = function () {
+  criarPaleta();
+  criarPixel();
+
+  const cor1 = document.getElementById('cor1')
+  const cor2 = document.getElementById('cor2')
+  const cor3 = document.getElementById('cor3')
+
+  cor1.className = 'selected color';
+}
+
 function criarPaleta() {
   const colorPalette = document.querySelector('#color-palette');
   for (i = 1; i < 5; i += 1) {
     let colorir = `rgb(${r},${g},${b})`;
     const color = document.createElement('div');
     color.className = 'color'
+    color.id = `cor${i}`;
     color.style.backgroundColor = colorir;
     colorPalette.appendChild(color);
     r = Math.floor(Math.random() * 255);
@@ -16,17 +29,14 @@ function criarPaleta() {
   }
 }
 
-criarPaleta();
 
-function criarPixel(){
+function criarPixel() {
   const pixelBoard = document.querySelector('#pixel-board');
-  for (l = 1; l < 6; l += 1){
-    for (c = 1; c < 6; c += 1){
+  for (l = 1; l < 6; l += 1) {
+    for (c = 1; c < 6; c += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
       pixelBoard.appendChild(pixel);
-    } 
+    }
   }
 }
-
-criarPixel();
