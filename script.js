246d9fc29aa59b57637ac0f29542d63415f81a6d
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-08-13 20:01:48 -0300

    Correções para fazer o Lint parar de reclamar

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-let coresDaPaleta = document.getElementsByClassName('color');
+const coresDaPaleta = document.getElementsByClassName('color');
 document.getElementById('black').style.backgroundColor = 'black';
-for (let i=1; i < coresDaPaleta.length; i += 1) {
-  let r = Math.random() * 255;
-  let g = Math.random() * 255;
-  let b = Math.random() * 255;
-  coresDaPaleta[i].style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b +')'
+for (let i = 1; i < coresDaPaleta.length; i += 1) {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+  coresDaPaleta[i].style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+function pintaPixel(pixel) {
+  const pixelPintado = pixel.target;
+  pixelPintado.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
 }
 
 function criaQuadro(valor) {
+  let checkValor = valor;
   if (valor > 50) {
-    valor = 50;
+    checkValor = 50;
   } else if (valor < 5) {
-    valor = 5;
+    checkValor = 5;
   }
-  let pixelBoard = document.createElement("div");
+  const pixelBoard = document.createElement('div');
   pixelBoard.id = 'pixel-board';
   document.querySelector('body').appendChild(pixelBoard);
-  let numeroDeLinhas = valor;
+  const numeroDeLinhas = checkValor;
   for (let linha = 0; linha < numeroDeLinhas; linha += 1) {
     for (let i = 0; i < numeroDeLinhas; i += 1) {
       const pixel = document.createElement('div');
@@ -39,37 +45,33 @@ for (let i = 0; i < document.querySelectorAll('.color').length; i += 1) {
   document.querySelectorAll('.color')[i].addEventListener('click', selectColor);
 }
 
-function pintaPixel(pixel) {
-  pixel.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-}
-
-let botao = document.createElement('button');
+const botao = document.createElement('button');
 botao.innerText = 'Limpar';
 botao.id = 'clear-board';
 botao.style.marginBottom = '10px';
-document.querySelector('body').insertBefore(botao, document.getElementById('pixel-board'))
-botao.onclick = function() {
-  for (let i = 0; i < document.getElementsByClassName("pixel").length; i += 1) {
-    document.getElementsByClassName("pixel")[i].style.backgroundColor = 'white';
+document.querySelector('body').insertBefore(botao, document.getElementById('pixel-board'));
+botao.onclick = function limpaQuadro () {
+  for (let i = 0; i < document.getElementsByClassName('pixel').length; i += 1) {
+    document.getElementsByClassName('pixel')[i].style.backgroundColor = 'white';
   }
-}
+};
 
-let tamanho = document.createElement('input');
+const tamanho = document.createElement('input');
 tamanho.id = 'board-size';
 tamanho.type = 'number';
 tamanho.min = '1';
 tamanho.max = '50';
-let resizeBoard = document.createElement('button');
+const resizeBoard = document.createElement('button');
 resizeBoard.id = 'generate-board';
-resizeBoard.innerText = 'VQV!'
+resizeBoard.innerText = 'VQV!';
 resizeBoard.onclick = function limpaQuadro() {
   if (tamanho.value < 1) {
-    alert('Board inválido!')
-  } else{
-    let pixelBoard = document.querySelector('#pixel-board');
+    alert('Board inválido!');
+  } else {
+    const pixelBoard = document.querySelector('#pixel-board');
     pixelBoard.parentNode.removeChild(pixelBoard);
     criaQuadro(tamanho.value);
   }
-}
-document.querySelector('body').insertBefore(tamanho, document.getElementById('pixel-board'))
-document.querySelector('body').insertBefore(resizeBoard, document.getElementById('pixel-board'))
+};
+document.querySelector('body').insertBefore(tamanho, document.getElementById('pixel-board'));
+document.querySelector('body').insertBefore(resizeBoard, document.getElementById('pixel-board'));
