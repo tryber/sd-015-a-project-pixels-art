4bf5cee7cf6cffcf02aaf0d0ae685f27b6b6afb0
deobrafantinif <deboraferfan@gmail.com>
2021-08-13 19:57:14 -0300

    resolucao do requisito 10

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 const pixelBoard = document.querySelector('#pixel-board');
 const pixelPallet = document.querySelector('#color-palette');
 const colorsPallet = document.querySelectorAll('.color');
-const buttonClear = document.querySelector('button');
+const buttonClear = document.querySelector('#clear-board');
+const buttonSizeBoard = document.querySelector('#generate-board');
+let valueBoard = 5;
 
 for (let i = 0; i < colorsPallet.length; i += 1) {
   colorsPallet[i].style.backgroundColor = colorsPallet[i].id;
 }
 
-for (let i = 1; i < 6; i += 1) {
-  const line = document.createElement('div');
-  pixelBoard.appendChild(line);
-  for (let ind = 1; ind < 6; ind += 1) {
-    const pixel = document.createElement('div');
-    pixel.style.width = '40px';
-    pixel.style.height = '40px';
-    pixel.style.backgroundColor = 'rgb(255, 255, 255)';
-    pixel.style.border = '1px solid black';
-    pixel.style.display = 'inline-block';
-    pixel.className = 'pixel';
-    line.appendChild(pixel);
+function createBoard(size) {
+  for (let i = 1; i <= size; i += 1) {
+    const line = document.createElement('div');
+    line.style.height = '40px';
+    pixelBoard.appendChild(line);
+    for (let ind = 1; ind <= size; ind += 1) {
+      const pixel = document.createElement('div');
+      pixel.style.width = '40px';
+      pixel.style.height = '40px';
+      pixel.style.backgroundColor = 'rgb(255, 255, 255)';
+      pixel.style.border = '1px solid black';
+      pixel.style.display = 'inline-block';
+      pixel.className = 'pixel';
+      line.appendChild(pixel);
+    }
   }
 }
 
 function initialColor() {
   const pixelBlack = document.querySelector('#black');
   pixelBlack.classList.add('selected');
+  createBoard(valueBoard);
 }
 
 window.onload = initialColor;
@@ -58,3 +64,30 @@ function clearBoard() {
 }
 
 buttonClear.addEventListener('click', clearBoard);
+
+function removeBoard() {
+  const pixelDiv = document.getElementsByClassName('pixel');
+
+  while (pixelDiv.length > 0) {
+    for (let i = 0; i < pixelDiv.length; i += 1) {
+      const element = pixelDiv[i];
+      const pai = element.parentElement;
+      pai.parentElement.removeChild(pai);
+    }
+  }
+}
+
+function changeBoard() {
+  const inputSize = document.querySelector('#board-size').value;
+  console.log(inputSize);
+  console.log(inputSize);
+  if (inputSize > 0) {
+    removeBoard();
+    createBoard(inputSize);
+  } else {
+    alert('Board inválido!');
+    //window.location.reload();
+  }
+}
+
+buttonSizeBoard.addEventListener('click', changeBoard);
