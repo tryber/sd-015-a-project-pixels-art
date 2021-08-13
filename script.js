35fd088a8a261a2d0995b1cc775ff9bfaeb2b9f6
João Felipe P <jf.pelliccione@gmail.com>
2021-08-13 18:47:19 -0300

    Conclusão do requisito 7.

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.onload = function() {
   let blackColorFromPalette = document.querySelector('#color1');
   blackColorFromPalette.classList.add('selected');
+  selectPaletteColor()
 }
+
+//Criando função para selecionar cor da paleta:
+function selectPaletteColor() {
+  let paletteColorsArray = document.getElementById('color-palette').children;
+
+  for (let element of paletteColorsArray) {
+    element.addEventListener('click', function() {
+      let currentSelectedElement = document.querySelector('.selected');
+      currentSelectedElement.classList.remove('selected');
+      element.classList.add('selected');
+      console.log(element);
+    })
+  }
+}
+
