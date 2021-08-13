501ab3b1eb192107f7c67970ae4b53d446d68ee8
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-08-13 16:37:22 -0300

    requisitos 3 4 5 6

--- script.js
+++ script.js
@@ -4,11 +4,26 @@ function divColor() {
   for (let index in color) {
     let newColor = color[index];
     let newDivColor = document.createElement('div');
-    newDivColor.className = "color"
+    newDivColor.className = 'color';
 
     newDivColor.style.backgroundColor = newColor;
     document.querySelector('#color-palette').appendChild(newDivColor);
   }
 }
 
-divColor()
+function creatBoard() {
+  for (let index = 0; index < 25; index += 1) {
+    let newDivEmpty = document.createElement('div');
+    newDivEmpty.className = 'pixel';
+    document.querySelector('#pixel-board').appendChild(newDivEmpty);
+  }
+}
+
+function addClassToBlack() {
+  document
+    .querySelector('#color-palette')
+    .firstElementChild.classList.add('selected');
+}
+divColor();
+creatBoard();
+addClassToBlack();
