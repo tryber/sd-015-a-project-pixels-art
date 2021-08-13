ab0c21a20f4dcb79e973d23d105f41e2449c80b0
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-08-13 18:59:37 -0300

    8-requisito

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+//Seleciona a cor
 let cor = document.querySelectorAll('.color');
 let selected = document.querySelector('.selected')
-
 for (let i = 0; i < cor.length; i++) {
     cor[i].addEventListener('click', trocaCor)
     function trocaCor () {
@@ -9,3 +9,15 @@ for (let i = 0; i < cor.length; i++) {
         selected = cor[i];
     }
 }
+
+//Pinta o pixel com a cor selecionada
+let pixel = document.querySelectorAll('.pixel')
+for (let i = 0; i < pixel.length; i++){
+    pixel[i].addEventListener('click', pintar)
+    function pintar (){
+        pixel[i].style.backgroundColor =  selected.classList[1]
+    }
+}
+
+
+
