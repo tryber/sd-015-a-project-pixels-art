0438faa537670ded0cb79279566c957552c4199a
Caio Lima <caiojlimah@gmail.com>
2021-08-13 12:34:23 -0300

    Requisito 10 e 11

--- script.js
+++ script.js
@@ -55,3 +55,63 @@ function clearAll() {
 
 const clearButton = document.getElementById('clear-board');
 clearButton.addEventListener('click', clearAll);
+
+const input = document.getElementById('board-size');
+const sizeButton = document.getElementById('generate-board');
+const pixelBoard = document.getElementById('pixel-board');
+
+function inputLessFive() {
+  pixelBoard.innerHTML = '';
+  for (let a = 1; a <= 5; a += 1) {
+    const tr = document.createElement('tr');
+    pixelBoard.appendChild(tr);
+    for (let b = 0; b < 5; b += 1) {
+      const newTd = document.createElement('td');
+      newTd.className = 'pixel';
+      tr.appendChild(newTd);
+      newTd.addEventListener('click', paintPixels);
+    }
+  }
+}
+
+function inputPlusFifth() {
+  pixelBoard.innerHTML = '';
+  for (let a = 1; a <= 50; a += 1) {
+    const tr = document.createElement('tr');
+    pixelBoard.appendChild(tr);
+    for (let b = 0; b < 50; b += 1) {
+      const newTd = document.createElement('td');
+      newTd.className = 'pixel';
+      tr.appendChild(newTd);
+      newTd.addEventListener('click', paintPixels);
+    }
+  }
+}
+
+function inputOkay() {
+  pixelBoard.innerHTML = '';
+  for (let a = 1; a <= parseInt(input.value, 10); a += 1) {
+    const tr = document.createElement('tr');
+    pixelBoard.appendChild(tr);
+    for (let b = 0; b < input.value; b += 1) {
+      const newTd = document.createElement('td');
+      newTd.className = 'pixel';
+      tr.appendChild(newTd);
+      newTd.addEventListener('click', paintPixels);
+    }
+  }
+}
+
+function changeBoardSize() {
+  if (input.value === '') {
+    alert('Board inválido!');
+  } else if (input.value < 5) {
+    inputLessFive();
+  } else if (input.value > 50) {
+    inputPlusFifth();
+  } else {
+    inputOkay();
+  }
+}
+
+sizeButton.addEventListener('click', changeBoardSize);
