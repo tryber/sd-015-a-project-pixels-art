d204c53340074c0d9ff510c0b40ae666a88c6ed8
Daniel Tavares <daniel1tavares@outlook.com>
2021-08-13 19:10:18 -0300

    finaliza o requisito 4 gloria a deux

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ divColor[0].style.backgroundColor = 'black';
 divColor[1].style.backgroundColor = 'blue';
 divColor[2].style.backgroundColor = 'red';
 divColor[3].style.backgroundColor = 'green';
+
+let paiDeTodos = document.querySelector('body');
+let criarDiv = document.createElement('ul');
+paiDeTodos.appendChild(criarDiv);
+criarDiv.id = 'pixel-board';
+
+
+let numeroDeQuadrados = 25
+for (index = 1; index <= numeroDeQuadrados; index +=1){
+    let quadro = document.querySelector('#pixel-board');
+    let criarQuadrado = document.createElement('li');
+    quadro.appendChild(criarQuadrado);
+    criarQuadrado.classList.add('pixel')
+}
+
