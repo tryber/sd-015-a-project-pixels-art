0c5d6466bf247d9143a7f85117580460fa470b92
William <william.mk1@outlook.com>
2021-08-13 18:41:20 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,23 @@
+let size = 5;
+let board = document.querySelector('#pixel-board')
+let pixelPalett = document.querySelector('#color-palette');
+
+function createLine(size) {
+  for (let line = 1; line <= size; line += 1) {
+    let createBoardLines = document.createElement('div');
+    board.appendChild(createBoardLines);
+    for (let column = 1; column <= size; column += 1) {
+      let createBoard = document.createElement('div');
+      createBoard.className = 'pixel';
+      createBoard.style.width = '40px';
+      createBoard.style.height = '40px';
+      createBoard.style.border = '1px solid black';
+      createBoardLines.appendChild(createBoard);
+    }
+  }
+} 
+
+
+createLine(size);
+
+
