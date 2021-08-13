37753146030a48ca30c66d6ac30264f8bfe769ab
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-08-13 18:38:53 -0300

    7-requisito

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-window.onload = function() {
-    document.querySelector('.black').classList.add("selected")
-};
+let cor = document.querySelectorAll('.color');
+let selected = document.querySelector('.selected')
+
+for (let i = 0; i < cor.length; i++) {
+    cor[i].addEventListener('click', trocaCor)
+    function trocaCor () {
+        selected.classList.remove('selected');
+        cor[i].classList.add('selected');
+        selected = cor[i];
+    }
+}
