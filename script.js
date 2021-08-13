746ce8688235d9c2cb51139c339675e6af7d1785
André <andre.bonaf@gmail.com>
2021-08-13 18:26:59 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const colors = ['black', 'red', 'green', 'blue'];
+window.onload = function(){
+  const colors = ['black', 'red', 'green', 'blue',];
+  const gridHeight = 5;
+  const gridWidth = 5;
+  const gridSize = gridHeight * gridWidth;
 
-function palette(howMany){
+  function palette(howMany) {
     for (let i = 0; i < howMany.length; i += 1) {
-        const divP = document.createElement('div');
-        const parent = document.querySelector('#color-palette');
-        divP.className = 'color';
-        divP.classList.add(howMany[i]);
-        divP.style.backgroundColor = howMany[i];
+      const divP = document.createElement('div');
+      const parent = document.querySelector('#color-palette');
+      divP.className = 'color';
+      divP.classList.add(howMany[i]);
+      divP.style.backgroundColor = howMany[i];
 
-        parent.appendChild(divP);
+      parent.appendChild(divP);
     }
-}
+  }
 
-palette(colors);
+  palette(colors);
+
+  function createGrid() {
+    for (let i = 0; i < gridSize; i += 1) {
+      const divG = document.createElement('div');
+      const parent = document.querySelector('#pixel-board');
+      divG.className = 'pixel';
+
+      parent.appendChild(divG);
+    }
+  }
+
+  createGrid();
+
+}
