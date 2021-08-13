4416d854828d196d9f48a039bec8de5c83432405
alexandrexpj <alexandrexpjr@hotmail.com>
2021-08-13 16:34:52 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -10,4 +10,23 @@ window.onload = function() {
             event.target.className = 'color selected';
         }
     })
+
+    let getPixelBoard = document.querySelector('#pixel-board');
+
+    getPixelBoard.addEventListener('click', function(event) {
+        let pixel = event.target.style.backgroundColor;
+        let cssBackground = window.getComputedStyle(getSelected[0]).backgroundColor;
+        if (pixel !== cssBackground) {
+            event.target.style.backgroundColor = cssBackground;
+        }
+    })
 }
+// ### 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
+
+// **O que será verificado:**
+
+// - Verifica se ao carregar a página deve ser possível pintar os pixels de preto
+
+// - Verifica se após selecionar uma outra cor na paleta, é possível pintar os pixels com essa cor
+
+// - Verifica se somente o pixel que foi clicado foi preenchido com a cor selecionada, sem influenciar na cor dos demais pixels.
