12bf081ed21163bfc0d37b28a3e33b6c52d3872f
Renato Moreno <renats.mc@gmail.com>
2021-08-13 17:48:38 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function selectBlack() {
   selectFirst.classList.add('selected');
 }
 selectBlack();
+
+let firstColor = document.getElementById('black');
+let secondColor = document.getElementById('red');
+let thirdColor = document.getElementById('blue');
+let fourthColor = document.getElementById('green');
+
+function changeSelected(newColor) {
+  let changeColor = document.getElementsByClassName('selected')[0];
+  changeColor.classList.remove('selected');
+  newColor.target.classList.add('selected');
+}
+
+firstColor.addEventListener('click', changeSelected);
+secondColor.addEventListener('click', changeSelected);
+thirdColor.addEventListener('click', changeSelected);
+fourthColor.addEventListener('click', changeSelected);
+
