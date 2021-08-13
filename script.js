e9b96b10b081e4ccaa8f00b9fbedf76937e0e451
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-08-13 15:41:38 -0300

    requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,14 @@
+function divColor() {
+  let color = ['black', 'red', 'blue', 'yellow', "brown"];
+
+  for (let index in color) {
+    let newColor = color[index];
+    let newDivColor = document.createElement('div');
+    newDivColor.className = "color"
+
+    newDivColor.style.backgroundColor = newColor;
+    document.querySelector('#color-palette').appendChild(newDivColor);
+  }
+}
+
+divColor()
