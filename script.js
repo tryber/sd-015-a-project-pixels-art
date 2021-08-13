828cf25e50cbbccc91b31d38106209e44364ebb9
biizerrah <biizerrah@outlook.com>
2021-08-13 18:01:34 -0300

    requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,5 @@
+let black = document.getElementById('black');
+let red = document.getElementById('red');
+let blue = document.getElementById('blue');
+let yellow = document.getElementById('yellow');
+
