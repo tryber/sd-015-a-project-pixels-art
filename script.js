5f37a60de2b08c8866ce286918e5b601140af21b
Igor Campos Moreira <igorcampos200@gmail.com>
2021-08-13 18:45:33 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function() {
   let base = 5;
   let height = 5;
  
@@ -8,11 +8,10 @@ window.onload = function(){
     
     let pixelboard = document.querySelector("#pixel-board")
     
-    for (let i = 0; i < base ; i += 1 ){
-      for (let j = 0; j < height ; j += 1 ){
+    for (let i = 0; i < base ; i += 1 ) {
+      for (let j = 0; j < height ; j += 1 ) {
         let pixel = createPixel("pixel");
         pixelboard.appendChild(pixel);
-             
     }
   }
 
@@ -23,5 +22,17 @@ function createPixel(className) {
   let pixel = document.createElement("div");
   pixel.className = className;
   return pixel;
+}
+
+
+}
+let pClick = document.getElementsByClassName("color");
+
+pClick[0].classList.add("selected");
+
+for(let index = 0 ; index < pClick.length ; index +=1 ) {
+  pClick[index].addEventListener("click", mudaCor);
+function mudaCor(index){
+  
 }
 }
