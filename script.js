984110217ab9a364e82da2933c7ec777fd656ec5
viniciusldn <viniciusldn0@gmail.com>
2021-08-13 16:09:42 -0300

    R6 Answered

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 for (let i = 0; i < 4; i += 1) {
   let colorPalette = document.getElementById("color-palette");
   let color = document.createElement("div");
-  let colors = ["black", "orange", "red", "green"]
+  let colors = ["black", "orange", "red", "green"];
   color.className = "color";
   colorPalette.appendChild(color);
   color.style.backgroundColor = colors[i];
@@ -12,4 +12,13 @@ for (let i = 0; i < 25; i += 1) {
   pixel.className = "pixel";
   pixelBoard.appendChild(pixel);
   pixel.style.backgroundColor = "white";
+}
+
+window.onload = function() {
+  let selected = document.querySelectorAll(".color");
+  for (let i = 0; i < selected.length; i += 1) {
+    if (selected[i].style.backgroundColor === "black") {
+      selected[i].classList.add("selected");
+    }
+  }
 }
