ed2f75da066433491ec94a67449a8cc92a5f048b
JunioASilva <junioalves2005@hotmail.com>
2021-08-13 18:33:09 -0300

    Adicionando cores a paleta

---
+++ script.js
@@ -0,0 +1,12 @@
+function paletaDeCores(){
+    let colorGreen = document.getElementsByClassName("color")[1].style.backgroundColor = "blue";
+
+    let colorRed = document.getElementsByClassName("color")[2].style.backgroundColor = "red";
+
+    let colorBrown = document.getElementsByClassName("color")[3].style.backgroundColor = "brown";
+
+}
+paletaDeCores();
+
+
+
