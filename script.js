e06c13125252aca8e5e3ed37f88678102ea80d25
Fernando Sena <fernandosenacruz@gmail.com>
2021-08-13 19:37:44 -0300

    4 ok

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let colorDiv = document.querySelectorAll('.color');
 let styleColor = ['black', 'red', 'pink', 'purple'];
-
+let pixelBord = document.getElementById('pixel-board');
+let columnDiv = 5;
+let lineDiv = 5;
 
 
 function changeColorDiv() {
@@ -12,4 +14,16 @@ function changeColorDiv() {
 }
 changeColorDiv();
 
-
+function creatPixelBord() {
+    for (let index = 0; index < columnDiv; index += 1) {
+        let creatBordColumn = document.createElement('div');
+        pixelBord.appendChild(creatBordColumn);
+        creatBordColumn.className = 'pixel';        
+        for (let index = 1; index < lineDiv; index +=1) {
+            let creatBordLine = document.createElement('div');
+            pixelBord.appendChild(creatBordLine);
+            creatBordLine.className = 'pixel';
+        }
+    }    
+}
+creatPixelBord();
