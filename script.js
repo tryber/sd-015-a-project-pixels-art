d0145a8e9fb08316d3516a4101485c31040c3b6c
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-08-13 17:08:48 -0300

    finishing exercise 8

--- script.js
+++ script.js
@@ -4,18 +4,29 @@ window.onload = function colors() {
     }
 }
 
-eventListener();
+paletteListener();
+pixelBoardListener();
 
-function selectColor(click) {
+function selectColor(paletteClick) {
     let selected = document.getElementsByClassName('selected')[0];
     selected.classList.remove('selected');
-    click.target.classList.add('selected');
+    paletteClick.target.classList.add('selected');
 }
 
-function eventListener() {
+function printColor(pixelClick) {
+    pixelClick.target.style.backgrounColor = selected.style.backgrounColor;
+}
+
+function paletteListener() {
     let colorPalette = document.getElementsByClassName('color');
     for(let i = 0; i < colorPalette.length; i += 1) {
         colorPalette[i].addEventListener('click', selectColor);
     } 
 }
 
+function pixelBoardListener() {
+    let pixelBoard = document.getElementsByClassName('pixel');
+    for(let i = 0; i < pixelBoard.length; i += 1) {
+        pixelBoard[i].addEventListener('click', printColor);
+    }
+}
