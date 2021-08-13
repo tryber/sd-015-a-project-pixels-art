eeecbc9c5533bbbf14f87fb4a038b53552dc40be
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-08-13 15:45:29 -0300

    Requisito 3 e 4

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
   let coresDaPaleta = document.getElementsByClassName("color")
-  console.log(coresDaPaleta)
-  for (i=0; i < coresDaPaleta.length; i += 1) {
+  for (let i=0; i < coresDaPaleta.length; i += 1) {
     coresDaPaleta[i].style.backgroundColor = coresDaPaleta[i].id;
   }
+
+  function criaQuadro() {
+    let pixelBoard = document.createElement("div")
+    pixelBoard.id = 'pixel-board';
+    document.getElementsByTagName("body")[0].appendChild(pixelBoard);
+    let numeroDeLinhas = 5;
+    let numeroDePixels = 5
+    for (let linha = 0; linha < numeroDeLinhas; linha += 1) {
+      for (let i = 0; i < numeroDePixels; i += 1) {
+        let pixel = document.createElement("div")
+        pixel.className = "pixel";
+        pixelBoard.appendChild(pixel);
+      }
+    }
+
+  }
+
+  criaQuadro();
