6dec411f2a5082cdeba65e8f91796d23263c53e1
Rafael Augusto Scherer <rafaelaugustscherer@gmail.com>
2021-08-13 15:55:46 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Function to fill pixel with selected color
+function fillWithColor(evt) {
+  evt.target.style.backgroundColor = sessionStorage.getItem('selectedColor');
+}
+
+// Function to change selected color
+function changeSelectedColor(evt) {
+  sessionStorage.setItem('selectedColor', evt.target.style.backgroundColor);
+
+  let paletteColors = document.getElementsByClassName('color');
+
+  for (color of paletteColors) {
+    if (color.classList.contains('selected')) {
+      color.classList.remove('selected');
+    }
+    if (color.style.backgroundColor === evt.target.style.backgroundColor) {
+      color.classList.add('selected');
+    }
+  }
+}
+
 // Create Color Palette Boxes
 const colorPaletteSection = document.getElementById('color-palette');
 
@@ -9,9 +30,10 @@ function createPalette(colors) {
     colorBox.className = 'color';
     colorBox.style.backgroundColor = color;
     colorBox.style.border = '1px solid black';
+    colorBox.addEventListener('click', changeSelectedColor);
 
     if (color === 'black') {
-        colorBox.classList.add('selected')
+      colorBox.classList.add('selected');
     }
 
     colorPaletteSection.appendChild(colorBox);
@@ -26,19 +48,30 @@ const pixelBoardSection = document.getElementById('pixel-board');
 function fillPixelBoard(lines, boxesPerLine) {
   for (let idx = 0; idx < lines; idx += 1) {
     let lineDiv = document.createElement('div');
-    lineDiv.className = 'line'
+    lineDiv.className = 'line';
 
     for (let innerIdx = 0; innerIdx < boxesPerLine; innerIdx += 1) {
       let pixelBox = document.createElement('div');
 
       pixelBox.className = 'pixel';
-      pixelBox.backgroundColor = 'white';
       pixelBox.style.border = '1px solid black';
+      pixelBox.addEventListener('click', fillWithColor);
 
-      lineDiv.appendChild(pixelBox)
+      lineDiv.appendChild(pixelBox);
     }
-    pixelBoardSection.appendChild(lineDiv)
+    pixelBoardSection.appendChild(lineDiv);
   }
 }
 
-fillPixelBoard(5, 5)
+fillPixelBoard(5, 5);
+
+// Set initial selected color to black (default)
+window.onload = () => {
+  let paletteColors = document.getElementsByClassName('color');
+
+  for (color of paletteColors) {
+    if (color.classList.contains('selected')) {
+      sessionStorage.selectedColor = color.style.backgroundColor;
+    }
+  }
+};
