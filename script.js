0c41683d2578955c27e2d3311bbc7c8490a2e584
Arthur <arthurteixeira0805@gmail.com>
2021-08-13 18:18:51 -0300

    req 1 done

---
+++ script.js
@@ -0,0 +1,7 @@
+function createColorsPallete() {
+    let ch1 = document.createElement('h1');
+    ch1.id = 'title';
+    ch1.innerText = 'Paleta de Cores';
+    document.body.appendChild(ch1)
+}
+createColorsPallete()
