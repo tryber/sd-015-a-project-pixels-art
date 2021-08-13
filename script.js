4f8e5e7cd0bdff8787316f113983cbae3fe5eb9d
viniciusldn <viniciusldn0@gmail.com>
2021-08-13 18:00:47 -0300

    Sintax rev01

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
-let colorPalette = document.getElementById("color-palette");
-let pixelBoard = document.getElementById("pixel-board");
-let color = document.getElementsByClassName("color");
-let pixel = document.getElementsByClassName("pixel");
-let selectedColor = "black";
-let clearBtn = document.getElementById("clear-board");
+const colorPalette = document.getElementById('color-palette');
+const pixelBoard = document.getElementById('pixel-board');
+const color = document.getElementsByClassName('color');
+const pixel = document.getElementsByClassName('pixel');
+const clearBtn = document.getElementById('clear-board');
+const initialSelection = document.querySelectorAll('.color');
+let selectedColor = 'black';
 
 window.onload = function() {
-  let selected = document.querySelectorAll(".color");
-  for (let i = 0; i < selected.length; i += 1) {
-    if (selected[i].style.backgroundColor === "black") {
-      selected[i].classList.add("selected");
-    };
-  };
-};
+  for (let i = 0; i < initialSelection.length; i += 1) {
+    if (initialSelection[i].style.backgroundColor === 'black') {
+      initialSelection[i].classList.add('selected');
+    } else {
+      initialSelection[0].classList.add('selected');
+    }
+  }
+}
 
 for (let i = 0; i < 4; i += 1) {
-  let createColor = document.createElement("div");
-  let colors = ["black", "orange", "red", "green"];
-  createColor.className = "color";
+  let createColor = document.createElement('div');
+  let colors = ['black', 'orange', 'red', 'green'];
+  createColor.className = 'color';
   colorPalette.appendChild(createColor);
   createColor.style.backgroundColor = colors[i];
-};
+}
 
 for (let i = 0; i < 25; i += 1) {
-  let pixelBoard = document.getElementById("pixel-board");
-  let createPixel = document.createElement("div");
-  createPixel.className = "pixel";
+  let pixelBoard = document.getElementById('pixel-board');
+  let createPixel = document.createElement('div');
+  createPixel.className = 'pixel';
   pixelBoard.appendChild(createPixel);
-  createPixel.style.backgroundColor = "white";
-};
+  createPixel.style.backgroundColor = 'white';
+}
 
 for (let i = 0; i < color.length; i += 1) {
-  color[i].addEventListener("click", function() {
+  color[i].addEventListener('click', function() {
     for (let i = 0; i < color.length; i += 1) {
-      color[i].classList.remove("selected");
-    };
-    color[i].classList.add("selected");
+      color[i].classList.remove('selected');
+    }
+    color[i].classList.add('selected');
     selectedColor = color[i].style.backgroundColor;
-  });
-};
+  })
+}
 
 for (let i = 0; i < pixel.length; i += 1) {
-  pixel[i].addEventListener("click", function() {
+  pixel[i].addEventListener('click', function() {
     let selectedPixel = pixel[i];
     selectedPixel.style.backgroundColor = selectedColor;
     console.log(selectedColor);
-  });
-};
+  })
+}
 
-clearBtn.addEventListener("click", function() {
+clearBtn.addEventListener('click', function() {
   for (let i = 0; i < pixel.length; i += 1) {
-    pixel[i].style.backgroundColor = "white";
-  };
-});
+    pixel[i].style.backgroundColor = 'white';
+  }
+})
