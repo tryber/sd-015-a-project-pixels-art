fec3702076a88433e9d011a411a66ce2075e38a8
André <andre.bonaf@gmail.com>
2021-08-13 18:34:55 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-window.onload = function(){
-  const colors = ['black', 'red', 'green', 'blue',];
-  const gridHeight = 5;
-  const gridWidth = 5;
-  const gridSize = gridHeight * gridWidth;
+const colors = ['black', 'red', 'green', 'blue'];
+const gridHeight = 5;
+const gridWidth = 5;
+const gridSize = gridHeight * gridWidth;
 
-  function palette(howMany) {
-    for (let i = 0; i < howMany.length; i += 1) {
-      const divP = document.createElement('div');
-      const parent = document.querySelector('#color-palette');
-      divP.className = 'color';
-      divP.classList.add(howMany[i]);
-      divP.style.backgroundColor = howMany[i];
+function palette(howMany) {
+  for (let i = 0; i < howMany.length; i += 1) {
+    const divP = document.createElement('div');
+    const parent = document.querySelector('#color-palette');
+    divP.className = 'color';
+    divP.classList.add(howMany[i]);
+    divP.style.backgroundColor = howMany[i];
 
-      parent.appendChild(divP);
+    if(howMany[i] === 'black'){
+      divP.classList.add('selected');
     }
+    parent.appendChild(divP);
   }
-
-  palette(colors);
-
-  function createGrid() {
-    for (let i = 0; i < gridSize; i += 1) {
-      const divG = document.createElement('div');
-      const parent = document.querySelector('#pixel-board');
-      divG.className = 'pixel';
-
-      parent.appendChild(divG);
-    }
-  }
-
-  createGrid();
-
 }
+
+palette(colors);
+
+function createGrid() {
+  for (let i = 0; i < gridSize; i += 1) {
+    const divG = document.createElement('div');
+    const parent = document.querySelector('#pixel-board');
+    divG.className = 'pixel';
+
+    parent.appendChild(divG);
+  }
+}
+
+createGrid();
