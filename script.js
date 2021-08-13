35d72878736f3dddb6b0d1e2a1472e3351393cea
Henrique_Veras <theveras1@gmail.com>
2021-08-13 16:07:57 -0300

    Correção exercício 2

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function divGenerator(e) {
   let colorPallete = document.getElementById('color-palette');
 
   let newDiv = document.createElement('div');
-  newDiv.className = 'color'
+  newDiv.className = 'color';
   newDiv.id = e;
   newDiv.style.backgroundColor = e;
   colorPallete.appendChild(newDiv);
 }
 //Aplicação da função 'divGenerator' para criar as divs com cores, sendo a primeira 'preto' e proibindo a criação de 'div's com a cor 'branco'//
-let arreyPalette = ['black', 'red', 'green', 'blue', 'yellow', 'purple','brown'];
+let arreyPalette = ['black', 'red', 'green', 'blue'];
 for(let index = 0; index < arreyPalette.length; index += 1) {
   if(arreyPalette[index] !== 'white') {
     divGenerator(arreyPalette[index]);  
