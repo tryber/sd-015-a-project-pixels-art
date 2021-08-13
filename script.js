f76694f3710cb2bf0d2fd28c65faa6ac9a9378c1
guilhermecd15 <guilhermecd15@gmail.com>
2021-08-13 19:25:33 -0300

    Req Bonus

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let armazenaCor = `rgb(${r},${g},${b})`;
 
 
 window.onload = function () {
-  
+
   criarPaleta();
-  criarPixel();
+  criarPixel(5);
 
   const cor1 = document.getElementById('cor1')
   const cor2 = document.getElementById('cor2')
@@ -35,17 +35,20 @@ function criarPaleta() {
 const pixelBoard = document.querySelector('#pixel-board');
 
 
-function criarPixel() {
-  for (let l = 1; l < 6; l += 1) {
+function criarPixel(n) {
+  for (let l = 1; l <= n; l += 1) {
     const linha = document.createElement('div');
     linha.className = 'line';
-    for (let c = 1; c < 6; c += 1) {
+    for (let c = 1; c <= n; c += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
       linha.appendChild(pixel);
     }
     pixelBoard.appendChild(linha);
+    let tamanhoPixelBoard = n * 40 * 1.05; 
+    pixelBoard.style.width = `${tamanhoPixelBoard}px`
   }
+
 }
 
 function selectedColor(evento) {
@@ -57,7 +60,7 @@ function selectedColor(evento) {
 
 colorPalette.addEventListener('click', selectedColor);
 
-function colorir(evento){
+function colorir(evento) {
   evento.target.style.backgroundColor = armazenaCor;
 }
 
@@ -65,12 +68,38 @@ pixelBoard.addEventListener('click', colorir);
 
 const botao = document.querySelector('#clear-board');
 
-function clear(evento){
+function clear() {
   const apagarPixel = document.querySelectorAll('.pixel')
-  for (let i = 0; i < apagarPixel.length; i += 1){
+  for (let i = 0; i < apagarPixel.length; i += 1) {
     let aux = apagarPixel[i]
     aux.style.backgroundColor = 'white';
   }
 }
 
-botao.addEventListener('click', clear)
+botao.addEventListener('click', clear);
+
+let inputPizelSize = document.querySelector('#board-size');
+const generateBoardButton = document.querySelector('#generate-board');
+
+generateBoardButton.addEventListener('click', function () {
+  let tamanhoBoard = inputPizelSize.value;
+  
+  if (tamanhoBoard === '') {
+    alert('Board inválido!');
+  } 
+  else {
+    if (tamanhoBoard < 5){
+      tamanhoBoard = 5;
+    }
+    if (tamanhoBoard > 50){
+      tamanhoBoard = 50;
+    }
+    const apagarLine = document.querySelectorAll('.line')
+    for (let i = 0; i < apagarLine.length; i += 1) {
+      let aux = apagarLine[i]
+      aux.remove();
+    }
+    criarPixel(`${tamanhoBoard}`);
+  }
+})
+
