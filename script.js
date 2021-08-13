462a78deee230ae3b97d186ed8738f18bd8b8b8d
MuriloAmaral <muralha2000@gmail.com>
2021-08-13 18:50:33 -0300

    requisito 9

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ window.onload = function(){
     document.querySelector('.color').classList.add('selected')
 }
 
-const colorOne = document.getElementById("first");
-const colorTwo = document.getElementById("second");
-const colorThree = document.getElementById("third");
-const colorFour = document.getElementById("fourth");
-const pixelBoard = document.getElementById("pixel-board");
+let colorOne = document.getElementById("first");
+let colorTwo = document.getElementById("second");
+let colorThree = document.getElementById("third");
+let colorFour = document.getElementById("fourth");
+let pixelBoard = document.getElementById("pixel-board");
+let eraseButton = document.getElementById("clear-board")
+let getSquares = document.querySelectorAll(".pixel");
+let setNewColor = 'white';
+
 
 colorOne.style.backgroundColor = "black";
 colorTwo.style.backgroundColor = "green";
@@ -19,12 +23,20 @@ colorThree.addEventListener('click', addSelectedClass);
 colorFour.addEventListener('click', addSelectedClass);
 pixelBoard.addEventListener('click', changeColor);
 
+
 function addSelectedClass(event) {
     let checkSelected = document.querySelector('.selected');
     checkSelected.classList.remove('selected');
     event.target.classList.add('selected');
    }
+
 function changeColor(event) {
-    const colorSelected = document.querySelector(".selected").style.backgroundColor;
+    let colorSelected = document.querySelector(".selected").style.backgroundColor;
     event.target.style.backgroundColor = colorSelected;
 }
+
+eraseButton.addEventListener('click', function(){
+   for (let i = 0; i < getSquares.length; i += 1){
+       getSquares[i].style.backgroundColor = setNewColor;
+   }
+});
