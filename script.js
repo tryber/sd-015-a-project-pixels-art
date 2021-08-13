b20af48244d02377b42c0927bc3f9b8f09d7dba8
ricardo-zabir <ricardofonseca.zabir@hotmail.com>
2021-08-13 16:34:09 -0300

    Realização dos primeiros requisitos do projeto

---
+++ script.js
@@ -0,0 +1,5 @@
+let arrayOfColors = document.querySelectorAll(".color");
+arrayOfColors[0].style.backgroundColor = "black";
+arrayOfColors[1].style.backgroundColor = "red";
+arrayOfColors[2].style.backgroundColor = "brown";
+arrayOfColors[3].style.backgroundColor = "green";
