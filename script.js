312c257f97e7b907328f374f0b7cddffe7a31b54
devgeorge <glalmeida.dev@gmail.com>
2021-08-13 18:13:04 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,4 @@
+let paletadecores = document.getElementsByClassName("color");
+for (let i = 0; i < paletadecores.length; i += 1){
+    paletadecores[i].style.backgroundColor = paletadecores[i].i;
+}
