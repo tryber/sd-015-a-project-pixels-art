c0ef0ee7dc2d91eb9c1f6e4f0e4512fb2cb58fb0
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-13 19:00:07 -0300

    Requisito Número 07

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let pixelsBoard = document.createElement("div");
 pixelsBoard.id = "pixel-board";
 document.body.appendChild(pixelsBoard);
+
 function pixels25() {
-for(let countPx = 1; countPx <= 25; countPx += 1) {
-let board = document.createElement("div");
-board.className = "pixel";
-pixelsBoard.appendChild(board);
-}
+    for (let countPx = 1; countPx <= 25; countPx += 1) {
+        let board = document.createElement("div");
+        board.className = "pixel";
+        pixelsBoard.appendChild(board);
+    }
 } pixels25();
 
 let firstColor = document.querySelector(".black");
 function changeBlackClass() {
-    firstColor.className += " selected";
+    firstColor.classList.add("selected");
 } changeBlackClass();
+
+let initialColors = document.getElementById("color-palette");
+initialColors.addEventListener("click", selectColor);
+function selectColor(event) {
+    let colors = document.querySelectorAll(".color");
+    for (let index of colors) {
+        index.classList.remove("selected");
+        event.target.classList.add("selected");
+    }
+}
