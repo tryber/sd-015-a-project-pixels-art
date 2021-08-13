09a7223c6b8658f038f49de76b673234db92fb64
lucas-morais <lucasmoraisss@gmail.com>
2021-08-13 15:48:38 -0300

    requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // função para criar div com id e nome da cor
 function createElementColor(color) {
-  const pallete = document.getElementById('color-palet');
+  const pallete = document.getElementById('color-palette');
   const colorCreated = document.createElement('div');
   colorCreated.className = 'color';
   colorCreated.id = color;
@@ -9,6 +9,6 @@ function createElementColor(color) {
 }
 
 // cores escolhidas para a paleta de cores
-const myColors = ['red', 'blue', 'green', 'black'];
+const myColors = ['black', 'red', 'blue', 'green'];
 // aplicando fução de criar cores
 myColors.forEach(createElementColor);
