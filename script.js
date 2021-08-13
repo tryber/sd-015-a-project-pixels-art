2e0199129062385dc309a54be788e6d223f73c2a
Matheus <mathspa98@gmail.com>
2021-08-13 15:24:29 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
- 
+ window.onload=function(){
+     let boardSize=5
+     let pBoard= document.getElementById("pixel-board");
+
+     montaBoard(boardSize);
+
+
+     function montaBoard(numero){
+
+         for(let i=0; i<numero; i+=1){
+             let linha= document.createElement("div")
+            for(let j=0; j<numero; j+=1){
+                let unidade = montaPixels("pixel");
+                linha.appendChild(unidade);
+            }
+             linha.style.margin='0'
+             pBoard.appendChild(linha);
+        }
+
+     }
+     function montaPixels (classe){
+         let pixel= document.createElement('div');
+         pixel.className = classe
+         return pixel
+     }
+ }
