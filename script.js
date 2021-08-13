6e8f8abf259d85464e3bb1ef154f447db287e2a5
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-08-13 16:19:59 -0300

    Nono requisito e último obrigatorio

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const paletaDeCores = ['black', 'red', 'green', 'blue'];
 const colorSlot = document.getElementById('color-palette').children;
+const pixels = document.getElementsByClassName('pixel');
+const botaoLimpar = document.getElementById('clear-board');
 
 window.onload = function () {
   function definirPaletaDeCores() {
@@ -14,8 +16,8 @@ window.onload = function () {
 };
 
 function adicionarEventos(elemento, evento, funcao) {
-  if(elemento.length > 0) {
-    for(let index = 0; index < elemento.length; index += 1) {
+  if (elemento.length > 0) {
+    for (let index = 0; index < elemento.length; index += 1) {
       elemento[index].addEventListener(evento, funcao);
     }
   } else {
@@ -29,11 +31,16 @@ function mudarCorPaleta(evento) {
 }
 
 function pintarPixels(evento) {
-  let cor = document.querySelector('.selected').style.backgroundColor;
+  const cor = document.querySelector('.selected').style.backgroundColor;
   evento.target.style.backgroundColor = cor;
 }
 
-const pixels = document.getElementsByClassName('pixel');
+function pintarDeBranco() {
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
 
 adicionarEventos(colorSlot, 'click', mudarCorPaleta);
 adicionarEventos(pixels, 'click', pintarPixels);
+adicionarEventos(botaoLimpar, 'click', pintarDeBranco);
