836519215a2ed93de4e28bedff47a15563c19d01
Arthur Simões <arthursimoes155@gmail.com>
2021-08-13 17:20:35 -0300

    Adiciona requisito 4

---
+++ script.js
@@ -0,0 +1,21 @@
+let div = document.createElement("div")
+div.className = "pixel"
+
+let divs = 5
+
+let fatherDiv = document.getElementById("pixel-board")
+
+
+for (let i = 0; i < divs; i +=1){
+    for (let index = 0; index < divs; index += 1){  
+        let div = document.createElement("div")
+        div.className = "pixel"
+        fatherDiv.appendChild(div)
+    }
+    let quebra = document.createElement("br")
+    fatherDiv.appendChild(quebra)
+}
+
+
+        
+    
