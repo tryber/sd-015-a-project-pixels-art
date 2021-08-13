ee6250c27a1f22940297230ceccbd921cbd2e415
Luan Vitor <luan_vittor@hotmail.com>
2021-08-13 15:14:18 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let corInicial = document.querySelector(".color");
-corInicial.classList += " selected"
-// window.onload = function definirClasseCorInicial ()
-// corInicial.className = "selected"
+// ex 6
+const corInicial = document.querySelector(".color");
+corInicial.classList += " selected";
+
+//ex 7
+let tdColor = document.querySelectorAll(".color");
+for (let element of tdColor){
+  element.addEventListener("click", changeSelected);
+}
+function changeSelected(event) {
+  let removeClass = document.querySelector(".selected")
+  removeClass.classList.remove("selected");
+  event.target.className = "color selected"
+}
