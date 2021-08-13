011b2d27e6b0494c3b90f70250f867e2f87ef67c
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-08-13 15:47:04 -0300

    Correções do Lint

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-  let coresDaPaleta = document.getElementsByClassName("color")
-  for (let i=0; i < coresDaPaleta.length; i += 1) {
-    coresDaPaleta[i].style.backgroundColor = coresDaPaleta[i].id;
-  }
+let coresDaPaleta = document.getElementsByClassName("color");
+for (let i=0; i < coresDaPaleta.length; i += 1) {
+  coresDaPaleta[i].style.backgroundColor = coresDaPaleta[i].id;
+}
 
-  function criaQuadro() {
-    let pixelBoard = document.createElement("div")
-    pixelBoard.id = 'pixel-board';
-    document.getElementsByTagName("body")[0].appendChild(pixelBoard);
-    let numeroDeLinhas = 5;
-    let numeroDePixels = 5
-    for (let linha = 0; linha < numeroDeLinhas; linha += 1) {
-      for (let i = 0; i < numeroDePixels; i += 1) {
-        let pixel = document.createElement("div")
-        pixel.className = "pixel";
-        pixelBoard.appendChild(pixel);
-      }
-    }
+function criaQuadro() {
+  let pixelBoard = document.createElement("div");
+  pixelBoard.id = 'pixel-board';
+  document.getElementsByTagName("body")[0].appendChild(pixelBoard);
+  let numeroDeLinhas = 5;
+   let numeroDePixels = 5;
+   for (let linha = 0; linha < numeroDeLinhas; linha += 1) {
+     for (let i = 0; i < numeroDePixels; i += 1) {
+       let pixel = document.createElement("div");
+       pixel.className = "pixel";
+       pixelBoard.appendChild(pixel);
+     }
+   }
+ }
 
-  }
-
-  criaQuadro();
+criaQuadro();
