a83508183cb417bf4957f41f2cf5b8a6f398e9b9
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-13 16:31:40 -0300

    add pixel board

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 function createBox(parent) {
   const colors = ['black', 'blue', 'green', 'yellow'];
   for (let i = 0; i < colors.length; i += 1) {
@@ -8,7 +8,19 @@ function createBox(parent) {
 
     newDiv.className = 'color';
     newDiv.style.backgroundColor = colors[i];
-    newLi.innerHTML = '  ';
+    parentElement.appendChild(newDiv);
+    newDiv.appendChild(newLi);
+  }
+}
+
+function createPixel(parent) {
+  for (let i = 0; i < 25; i += 1) {
+    const newDiv = document.createElement('div');
+    const newLi = document.createElement('li');
+    const parentElement = document.getElementById(parent);
+
+    newDiv.className = 'pixel';
+    newDiv.style.backgroundColor = 'white';
     parentElement.appendChild(newDiv);
     newDiv.appendChild(newLi);
   }
