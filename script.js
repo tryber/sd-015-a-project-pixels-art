7f1e95598b471300674aec1f951d0da388133385
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-13 15:50:50 -0300

    add color-palette

---
+++ script.js
@@ -0,0 +1,14 @@
+function createBox(parent) {
+  const colors = ['red', 'blue', 'green', 'yellow'];
+  for (let i = 0; i < colors.length; i += 1) {
+    const newDiv = document.createElement('div');
+    const newLi = document.createElement('li');
+    const parentElement = document.getElementById(parent);
+
+    newDiv.className = 'color';
+    newDiv.style.backgroundColor = colors[i];
+    newLi.innerHTML = '  ';
+    parentElement.appendChild(newDiv);
+    newDiv.appendChild(newLi);
+  }
+}
