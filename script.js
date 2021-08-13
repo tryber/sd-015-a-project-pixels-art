ab821ac9c7ad9439de8b15a25e178fe04f876b66
Luan Vitor <luan_vittor@hotmail.com>
2021-08-13 15:51:53 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // ex 6
+
 const corInicial = document.querySelector(".color");
 corInicial.classList += " selected";
 
 //ex 7
+
 let tdColor = document.querySelectorAll(".color");
 for (let element of tdColor){
   element.addEventListener("click", changeSelected);
@@ -10,5 +12,18 @@ for (let element of tdColor){
 function changeSelected(event) {
   let removeClass = document.querySelector(".selected")
   removeClass.classList.remove("selected");
-  event.target.className = "color selected"
+  event.target.className = "color selected";
+}
+
+//ex 8 
+let paint = document.querySelectorAll(".pixel");
+
+for (let element of paint){
+  element.addEventListener("click", changeColor);
+}
+
+function changeColor(event) {
+  let colorSelected = document.getElementsByClassName("selected")[0];
+  let colorPaint = getComputedStyle(colorSelected);
+  event.target.style.backgroundColor = colorPaint.backgroundColor
 }
