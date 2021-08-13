eba5008660f54545e5a42cc66c3f73ef238da61b
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-13 17:29:47 -0300

    desafio 2

---
+++ script.js
@@ -0,0 +1,10 @@
+// requisito 6 adicionando selected ao primeiro elemento
+let colocaClasse = document.querySelector('.selected');
+
+
+
+let selecionandoBlack = document.querySelector('.black');
+
+function targetColor (){
+  
+}
