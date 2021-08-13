c32b32d2ddbd7728431155af212c2e6a1433c48d
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-13 06:58:17 -0300

    Requisito 8 feito

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const colorList = document.getElementsByClassName('color');
 const colorContainer = document.getElementById('color-palette');
+const pixelBoard = document.getElementById('pixel-board');
 
 function clearSelected() {
   for (let index = 0; index < colorList.length; index += 1) {
@@ -12,3 +13,15 @@ colorContainer.addEventListener('click', (color) => {
   clearSelected();
   color.target.classList.add('selected');
 });
+
+// Paint pixels
+pixelBoard.addEventListener('click', (event) => {
+  if (event.target.classList.contains('pixel')) {
+    const pixel = event;
+    const selectedColor = document.getElementsByClassName('selected')[0];
+    console.log(selectedColor);
+    const color = getComputedStyle(selectedColor).backgroundColor;
+    console.log(color);
+    pixel.target.style.backgroundColor = color;
+  }
+}, false);
