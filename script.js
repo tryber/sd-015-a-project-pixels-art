b626ed2fea40e76a76671d52fdb85638d913010e
Thiago <thiago_ssfr@hotmail.com>
2021-08-13 18:57:59 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 const cores = document.getElementsByClassName('color');
+const rgb = 'rgb(255,255,255)';
 cores[0].style.background = 'black';
 cores[1].style.background = 'red';
 cores[2].style.background = 'blue';
 cores[3].style.background = 'green';
 
 const quadroPixel = document.getElementById('pixel-board');
-const corSelected = document.querySelector('.selected');
+const btn = document.querySelector('button');
 const numberOfLines = 5;
 const numberOfColums = 5;
-
-numberLines(numberOfLines);
-
-const lines = document.querySelectorAll('.line');
-
-createBox(lines, numberOfColums);
-
-cores[0].addEventListener('click', trocaCor);
-cores[1].addEventListener('click', trocaCor);
-cores[2].addEventListener('click', trocaCor);
-cores[3].addEventListener('click', trocaCor);
+const pixelTotal = numberOfLines * numberOfColums;
 
 function numberLines(number) {
   for (let index = 0; index < number; index += 1) {
@@ -27,11 +18,14 @@ function numberLines(number) {
     quadroPixel.appendChild(div);
   }
 }
+numberLines(numberOfLines);
 
-function createBox(lines, colunas) {
-  for (let index = 0; index < lines.length; index += 1) {
+const lines = document.querySelectorAll('.line');
+
+function createBox(linhas, colunas) {
+  for (let index = 0; index < linhas.length; index += 1) {
     for (let j = 0; j < colunas; j += 1) {
-      const positionLine = lines[index];
+      const positionLine = linhas[index];
       const divGrid = document.createElement('div');
       divGrid.className = 'pixel';
       positionLine.appendChild(divGrid);
@@ -45,15 +39,39 @@ function trocaCor(event) {
   event.target.classList.add('selected');
 }
 
-quadroPixel.addEventListener('click',pintaGrid);
-
-function pintaGrid(){
-  let taskDiv = document.querySelectorAll('.color');  
-  for (let index = 0; index < taskDiv.length; index += 1) {
-    if (taskDiv[index].className == 'color selected') {
-      let taskColor = taskDiv[index].style.background;
-      event.target.style.background = taskColor;
+function pintaGrid() {
+  const colorDiv = document.querySelectorAll('.color');
+  for (let index = 0; index < colorDiv.length; index += 1) {
+    if (colorDiv[index].className === 'color selected') {
+      const selectCor = colorDiv[index].style.background;
+      event.target.style.background = selectCor;
     }
   }
-  
 }
+
+function boxClear() {
+  for (let index = 0; index < lines.length; index += 1) {
+    const clearDiv = document.querySelectorAll('.line');
+    clearDiv[index].style.background = rgb;
+  }
+  for (let j = 0; j < pixelTotal; j += 1) {
+    const box = document.querySelectorAll('.pixel');
+    box[j].style.background = rgb;
+  }
+}
+
+function clearQuadro() {
+  quadroPixel.style.background = rgb;
+  boxClear();
+}
+
+createBox(lines, numberOfColums);
+
+cores[0].addEventListener('click', trocaCor);
+cores[1].addEventListener('click', trocaCor);
+cores[2].addEventListener('click', trocaCor);
+cores[3].addEventListener('click', trocaCor);
+
+quadroPixel.addEventListener('click', pintaGrid);
+
+btn.addEventListener('click', clearQuadro);
