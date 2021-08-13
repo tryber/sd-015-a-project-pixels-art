6d89b7c776e5289ee4fb371362a728bd8578f88c
alexandrexpj <alexandrexpjr@hotmail.com>
2021-08-13 19:01:44 -0300

    adiciona requisito 12

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ window.onload = function() {
     let getSelected = document.getElementsByClassName('color selected');
     let getPallet = document.querySelector('#color-palette');
     let getColor = document.querySelectorAll('.color');
+    let getRandom = document.getElementsByClassName('color random');
+
+    for(index = 0; index < getRandom.length; index += 1) {
+        let arrayOfRandom = ['rgb(' + Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255) + ')'];
+        let stringRandom = arrayOfRandom.toString();
+        getRandom[index].style.backgroundColor = stringRandom;
+        console.log(stringRandom);
+    }
 
     getPallet.addEventListener('click', function(event) {
         let selected = event.target.className;
@@ -29,5 +37,6 @@ window.onload = function() {
             getPixel[index].style.backgroundColor = "rgb(255,255,255)";
         }
     })
-}
 
+
+}
