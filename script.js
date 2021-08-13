23147f8b5b866e302ae4122eb7870a1d16b255c0
Daniel Tavares <daniel1tavares@outlook.com>
2021-08-13 15:42:59 -0300

    adiciona o segundo requisito

---
+++ script.js
@@ -0,0 +1,5 @@
+const divColor = document.querySelectorAll('.color')
+divColor[0].style.backgroundColor = 'blue';
+divColor[1].style.backgroundColor = 'black';
+divColor[2].style.backgroundColor = 'red';
+divColor[3].style.backgroundColor = 'green';
