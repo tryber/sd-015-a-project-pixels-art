372264bac11f8ca16c83ef5eceb8da1896b04134
Anderson-Zobel <anderson.zobels@gmail.com>
2021-08-13 18:41:11 -0300

    requisito 2 e 3

---
+++ script.js
@@ -0,0 +1,11 @@
+//color pallete
+color1 = document.querySelectorAll('.color')[0];
+color2 = document.querySelectorAll('.color')[1];
+color3 = document.querySelectorAll('.color')[2];
+color4 = document.querySelectorAll('.color')[3];
+color1.style.backgroundColor = 'black';
+color2.style.backgroundColor = 'red';
+color3.style.backgroundColor = 'green';
+color4.style.backgroundColor = 'blue';
+
+
