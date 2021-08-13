9fcae607c2e11d55386a1f2e654f3d2405d563d3
William Schmidt <9williamschmidt@gmail.com>
2021-08-13 17:15:31 -0300

    exercicios 2 e 3

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const h1 = document.createElement('h1');
+const sectionCollorPalette = document.createElement('section');
+sectionCollorPalette.id = 'color-palette';
 
-document.body.appendChild(h1);
-h1.innerText = 'Paleta de cores';
-h1.id = `title`;
+function bodyChildren(elemento) { 
+  document.body.appendChild(elemento);
+}
+
+h1.innerText = 'Paleta de Cores';
+h1.id = 'title';
+
+for (let i = 0; i < 4; i += 1) {
+  let element = document.createElement('div');
+  sectionCollorPalette.appendChild(element);
+  element.className = 'color';
+}
+
+bodyChildren(h1);
+bodyChildren(sectionCollorPalette);
