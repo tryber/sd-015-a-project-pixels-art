f695f48cb8dfb1a9235bd554e8fd214838dfbeb2
lucas-morais <lucasmoraisss@gmail.com>
2021-08-13 16:11:51 -0300

    requisito 4

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function createElementColor(color) {
 const myColors = ['black', 'red', 'blue', 'green'];
 // aplicando fução de criar cores
 myColors.forEach(createElementColor);
+
+// criando grade de pixels
+function createPixelBoard(size) {
+  const pixelBoard = document.getElementById('pixel-board');
+  for (let line = 0; line < size; line += 1) {
+    const pixelLine = document.createElement('div');
+    pixelLine.className = 'pixel-line';
+    pixelBoard.appendChild(pixelLine);
+    for (let column = 0; column < size; column += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixelLine.appendChild(pixel);
+    }
+  }
+}
+
+createPixelBoard(5);
