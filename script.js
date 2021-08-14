67aa8db8d0eb8b976fca69fa5d0689bae9be7f9b
Ricardo <ricardoanj.14@gmail.com>
2021-08-14 12:24:45 -0300

    algumas alteracoes no arquivo js

--- script.js
+++ script.js
@@ -11,17 +11,21 @@
 //     board.appendChild(quadrados)
 // }
 //criarPixels();
-const divs = []
-function colorForChange () {
- const color1 = document.getElementsByClassName('color-palette')
+// const divs = []
+// function colorForChange () {
+//  const color1 = document.getElementsById('color-palette')
 
- for (let index = 0; index < color1.length; index += 1) {
-     let inColor = color1[index]
-     const divColor = document.createElement('div')
-     if (inColor = color1[0])
-     divColor.className = 'color'
-     divColor.innerHTML = inColor
-     color1.appendChild(divColor)
- }
-}
-colorForChange(color1)
+//  for (let index = 0; index < color1.length; index += 1) {
+//      let inColor = color1[index]
+//      const divColor = document.createElement('div')
+//      if (inColor = inColor[0])
+//      divColor.className = 'color'
+//      divColor.innerHTML = inColor
+//      color1.appendChild(divColor)
+//  }
+// }
+// colorForChange(color1)
+const divs = document.getElementById('pixel-board')
+const div25 = document.createElement('div')
+div25.className = 'color'
+divs.appendChild(div25)
