98ae64432f86aaf114a1e92d21fba4985d9acb14
Ricardo <ricardoanj.14@gmail.com>
2021-08-14 11:59:33 -0300

    de alguma forma minha nota zerou, tentando concertar

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let base = 5 
-let linhas = base
-let board = document.querySelector('#pixel-board')
+// const base = 5 
+// const linhas = base
+// const board = document.querySelector('#pixel-board')
 
-function criarPixels () {
-    for (let i = 0; i < base; i += 1);
-    let pixel = base[i];
-    let quadrados = document.createElement('div')
-    quadrados.className = 'color';
-    board.appendChild(quadrados)
+// function criarPixels () {
+//     for (let i = 0; i < base; i += 1);
+//     let pixel = base[i];
+//     let quadrados = document.createElement('div')
+//     quadrados.className = 'color';
+//     quadrados.innerHTML = pixel;
+//     board.appendChild(quadrados)
+// }
+//criarPixels();
+const divs = []
+function colorForChange () {
+ const color1 = document.getElementsByClassName('color-palette')
+
+ for (let index = 0; index < color1.length; index += 1) {
+     let inColor = color1[index]
+     const divColor = document.createElement('div')
+     if (inColor = color1[0])
+     divColor.className = 'color'
+     divColor.innerHTML = inColor
+     color1.appendChild(divColor)
+ }
 }
-criarPixels();
+colorForChange(color1)
