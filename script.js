228d3071be449db6d07fb33244fed39bdd156011
devgeorge <glalmeida.dev@gmail.com>
2021-08-14 15:48:58 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 let selected = document.querySelector(".color");
 selected.classList.add("selected");
+
+let pegacor = document.querySelectorAll(".color");
+for (let i = 0; i < pegacor.length; i += 1){
+    pegacor[i].addEventListener("click", takecor);
+};
+function takecor(event){
+    let corselecionada = document.querySelector(".selected");
+    corselecionada.classList.remove("selected");
+    event.target.classList.add("selected");
+};
