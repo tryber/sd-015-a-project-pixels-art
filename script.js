f3f5242ecb32e5bc23e7af07bfd20e2d2a7c6249
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-08-14 20:39:47 -0300

    requisitos 1 ao 8 refeitos

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Requisito 3
-function firstBlock() {
-  const mainBlock = document.getElementsByClassName('color')[0]
-  mainBlock.id = 'main-block'
-  mainBlock.classList.add('selected')
-}
-firstBlock();
+// Define o bloco 1 como selected
+const mainBlock = document.getElementById('block-one')
+mainBlock.classList.add('selected')
 
 // Cria tabela de pixels
 function createTable() {
@@ -21,12 +17,39 @@ function createTable() {
 }
 createTable();
 
-// Escolhe cor
-const palette = document.querySelector('#color-palette')
-palette.addEventListener('click', selectColor)
+// Seleciona cor
+  const allColors = document.querySelectorAll('.color')
+  for (let index = 0; index < allColors.length; index += 1) {
+    let selectedColor = document.querySelectorAll('.color')[index]                 
+    selectedColor.addEventListener('click', selectColor)                            
+  }
 
-function selectColor(event) {
-  let selected = document.querySelector('.selected')
-  selected.classList.remove('selected')
-  event.target.classList.add('selected')
+  function selectColor(event) {
+    let selected = document.querySelector('.selected') 
+    selected.classList.remove('selected') 
+    event.target.classList.add('selected')
+  }
+
+// Define cor
+let definedColor = 'black'
+
+const palette = document.querySelector('#color-palette')
+palette.addEventListener('click', defineColor)                              // ao clicar em qq lugar da paleta ele redefine a cor
+
+function defineColor() {
+  const selectedColor = document.querySelector('.selected')
+  definedColor = window.getComputedStyle(selectedColor).getPropertyValue('background-color')
+  console.log('ativou')
 }
+
+// Pinta os pixels 
+const allPixels = document.querySelectorAll('.pixel')                        // define o tamanho do 'for' abaixo com base no numero de pixels
+for (let index = 0; index < allPixels.length; index += 1) {
+  let selectedPixel = document.querySelectorAll('.pixel')[index]                 // seleciona pixel desejado
+  selectedPixel.addEventListener('click', paintPixel)                            // pinta o pixel desejado
+}
+
+function paintPixel(event) {
+  event.target.style.backgroundColor = definedColor
+}
+
