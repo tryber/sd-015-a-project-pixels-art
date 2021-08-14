cb99e0b185cd0f5e7984e4867f58a47f74bbb0bf
João Felipe P <jf.pelliccione@gmail.com>
2021-08-14 13:26:46 -0300

    Refatoração de acordo com Eslint.

--- script.js
+++ script.js
@@ -1,73 +1,77 @@
-window.onload = function() {
-  let blackColorFromPalette = document.querySelector('#color1');
-  blackColorFromPalette.classList.add('selected');
-  randomColors();
-  selectPaletteColor();
-  coloring();
-  cleanPixels();
-}
-
-//Criando função para aleatoriedade de cores:
+// Criando função para aleatoriedade de cores:
 function randomColors() {
-  let colorsPaletteArray = ["color2", "color3", "color4", "color5", "color6", "color7", "color8", "color9", "color10", "color11", "color12"];
+  const colorsPaletteArray = ['color2', 'color3', 'color4', 'color5', 'color6', 'color7', 'color8',
+    'color9', 'color10', 'color11', 'color12'];
 
-  let colorsPaletteObject = {
-    color2: "Yellow",
-    color3: "Cyan",
-    color4: "Orangered",
-    color5: "Darkviolet",
-    color6: "Coral",
-    color7: "Dodgerblue",
-    color8: "Deeppink",
-    color9: "Darkslategray",
-    color10: "Lightsalmon",
-    color11: "Tan",
-    color12: "Darkgoldenrod"
+  const colorsPaletteObject = {
+    color2: 'Yellow',
+    color3: 'Cyan',
+    color4: 'Orangered',
+    color5: 'Darkviolet',
+    color6: 'Coral',
+    color7: 'Dodgerblue',
+    color8: 'Deeppink',
+    color9: 'Darkslategray',
+    color10: 'Lightsalmon',
+    color11: 'Tan',
+    color12: 'Darkgoldenrod'
   };
 
-  let paletteColorsArray = document.getElementById('color-palette').children; 
+  const paletteColorsArray = document.getElementById ('color-palette').children; 
 
   for (let index = 1; index < paletteColorsArray.length; index += 1) {
-    let randomId = colorsPaletteArray[Math.floor(Math.random()*colorsPaletteArray.length)]; //https://css-tricks.com/snippets/javascript/select-random-item-array/
+    const randomId = colorsPaletteArray[Math.floor(Math.random()*colorsPaletteArray.length)]; // https://css-tricks.com/snippets/javascript/select-random-item-array/
     paletteColorsArray[index].id = randomId
     paletteColorsArray[index].innerText = colorsPaletteObject[randomId]
   }
 }
 
-//Criando função para selecionar cor da paleta:
+// Criando função para selecionar cor da paleta:
 function selectPaletteColor() {
-  let paletteColorsArray = document.getElementById('color-palette').children;
+  const paletteColorsArray = document.getElementById('color-palette').children;
 
   for (let element of paletteColorsArray) {
-    element.addEventListener('click', function() {
+    element.addEventListener('click', function () {
       let currentSelectedElement = document.querySelector('.selected');
       currentSelectedElement.classList.remove('selected');
+      currentSelectedElement.style.fontSize = '1.5em';
       element.classList.add('selected');
+      element.style.fontSize = '2em';
     })
   }
 }
 
-//Criando função para pintar os quadrados com a cor selecionada:
+// Criando função para pintar os quadrados com a cor selecionada:
 function coloring() {
-  let pixelsArray = document.getElementById('pixel-board').children;
+  const pixelsArray = document.getElementById('pixel-board').children;
 
   for (let pixel of pixelsArray) {
-    pixel.addEventListener('click', function() {
+    pixel.addEventListener('click', function () {
       let currentSelectedElement = document.querySelector('.selected');
       pixel.style.backgroundColor = currentSelectedElement.innerText;
     })
   }
 }
 
-//Criando função para limpar o quadrados pintados:
+// Criando função para limpar o quadrados pintados:
 function cleanPixels() {
-  let getCleanButton = document.getElementById('clear-board');
+  const getCleanButton = document.getElementById('clear-board');
 
-  getCleanButton.addEventListener('click', function() {
-    let pixelsArray = document.getElementById('pixel-board').children;
+  getCleanButton.addEventListener('click', function () {
+    const pixelsArray = document.getElementById('pixel-board').children;
 
     for (let pixel of pixelsArray) {
       pixel.style.backgroundColor = 'white';
     }
   })
+}
+
+window.onload = function () {
+  const blackColorFromPalette = document.querySelector('#color1');
+  blackColorFromPalette.classList.add('selected');
+  blackColorFromPalette.style.fontSize = '2em';
+  randomColors();
+  selectPaletteColor();
+  coloring();
+  cleanPixels();
 }
