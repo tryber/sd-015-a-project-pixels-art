89b8dd2f40714b1e253751955039b7fe65bf4a33
Daniel Tavares <daniel1tavares@outlook.com>
2021-08-14 11:27:19 -0300

    configura identaçao de 2 espaços

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const divColor = document.querySelectorAll('.color')
+const divColor = document.querySelectorAll('.color');
 divColor[0].style.backgroundColor = 'black';
 divColor[1].style.backgroundColor = 'blue';
 divColor[2].style.backgroundColor = 'red';
@@ -9,36 +9,41 @@ let criarDiv = document.createElement('ul');
 paiDeTodos.appendChild(criarDiv);
 criarDiv.id = 'pixel-board';
 
-
-let numeroDeQuadrados = 25
-for (index = 1; index <= numeroDeQuadrados; index +=1){
-    let quadro = document.querySelector('#pixel-board');
-    let criarQuadrado = document.createElement('li');
-    quadro.appendChild(criarQuadrado);
-    criarQuadrado.classList.add('pixel')
+let numeroDeQuadrados = 25;
+for (index = 1; index <= numeroDeQuadrados; index += 1) {
+  let quadro = document.querySelector('#pixel-board');
+  let criarQuadrado = document.createElement('li');
+  quadro.appendChild(criarQuadrado);
+  criarQuadrado.classList.add('pixel');
 }
 
-
-function pretoPrimeiro(){
-    let estiloPreto = document.querySelector('.color');
-    window.onload = function(){
-        if(estiloPreto.style.backgroundColor === 'black'){
-            estiloPreto.classList.add('selected')
+function pretoPrimeiro() {
+  let estiloPreto = document.querySelector('.color');
+  window.onload = function () {
+    if (estiloPreto.style.backgroundColor === 'black') {
+      estiloPreto.classList.add('selected');
     }
-}
+  };
 }
 
 pretoPrimeiro();
 
-function cliqueConsole(event){
-    let paletaDeCores = document.querySelectorAll('.color');
-    for (index = 0; index < paletaDeCores.length; index +=1){
-        paletaDeCores[index].addEventListener('click', function(event){
-            let classeSelected = document.querySelector('.selected')
-            classeSelected.classList.remove('selected');
-            event.target.classList.add('selected');
-        })
-    }
+function cliqueConsole(event) {
+  let paletaDeCores = document.querySelectorAll('.color');
+  for (index = 0; index < paletaDeCores.length; index += 1) {
+    paletaDeCores[index].addEventListener('click', function (event) {
+      let classeSelected = document.querySelector('.selected');
+      classeSelected.classList.remove('selected');
+      event.target.classList.add('selected');
+    });
+  }
+}
+cliqueConsole();
+
+function cor() {
+  let corSelecionada =
+    document.querySelectorAll('.color')[0].style.backgroundColor;
+  console.log(corSelecionada);
 }
 
-cliqueConsole();
+cor();
