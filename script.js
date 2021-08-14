d6d78cc2c696b271cd31d7152f9fa95d3014bc49
devgeorge <glalmeida.dev@gmail.com>
2021-08-14 14:57:00 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-let linhaColuna = ["" , "" , "", "", "", "", "", "","","","","","","","","","","","","","","","","",""];
-function makeQ(){
-    let divs = document.getElementById("pixel-board")
-    for (let i= 0; i  < linhaColuna.length; i += 1){
-        let linha = linhaColuna[i];
-        let div25 = document.createElement("div")
-        div25.innerHTML = linha
-        divs.appendChild(div25)
-    }
-}
-makeQ()
+let selected = document.querySelector(".color");
+selected.classList.add("selected");
