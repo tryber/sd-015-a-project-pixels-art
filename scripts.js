70b4094816309e905bbb89d332456f3bfdcc2547
Thiago <thiivieira@gmail.com>
2021-08-14 13:13:36 -0300

    Arrumando lint

--- scripts.js
+++ scripts.js
@@ -1,38 +1,37 @@
-  const clickButton = document.getElementById('color-palette');
-    clickButton.addEventListener('click', color);
+const clickButton = document.getElementById('color-palette');
+clickButton.addEventListener('click', color);
 
-  function color(event) {
-    const colorsAdd = document.querySelectorAll('.color');
+function color(event) {
+  const colorsAdd = document.querySelectorAll('.color');
   for (const colors of colorsAdd) {
     colors.classList.remove('selected');
-  };
-    event.target.classList.add('selected');
-  };
-
-  
-  const changeColor = document.getElementsByClassName('pixel')
-
-  for (let index = 0; index < changeColor.length; index += 1){
-    changeColor[index].addEventListener('click', selectColor)
   }
+  event.target.classList.add('selected');
+}
 
-  function selectColor(event) {
-    const inColors = document.querySelector('.selected');
-    const change = getComputedStyle(inColors)
-    event.target.style.backgroundColor = change.backgroundColor;
-  };
+const changeColor = document.getElementsByClassName('pixel');
 
-    const resetButton = document.getElementById('clear-board')
-    
-    resetButton.addEventListener('click', selectPaintPixel)
-    
-    function selectPaintPixel() {
-    let pixels = document.querySelectorAll('.pixel')
-    for (const pixel of pixels) {
-    makePixelsWhite(pixel)  
-    }
-    };
+for (let index = 0; index < changeColor.length; index += 1) {
+  changeColor[index].addEventListener('click', selectColor);
+}
 
-    function makePixelsWhite(pixel) {
-    pixel.style.backgroundColor = 'white'
-    };
+function selectColor(event) {
+  const inColors = document.querySelector('.selected');
+  const change = getComputedStyle(inColors);
+  event.target.style.backgroundColor = change.backgroundColor;
+}
+
+const resetButton = document.getElementById('clear-board');
+
+resetButton.addEventListener('click', selectPaintPixel);
+
+function selectPaintPixel() {
+  let pixels = document.querySelectorAll('.pixel');
+  for (const pixel of pixels) {
+    makePixelsWhite(pixel);
+  }
+}
+
+function makePixelsWhite(pixel) {
+  pixel.style.backgroundColor = 'white';
+}
