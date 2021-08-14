4049de7d0a0d1429401b522f9dc1231fb8a14e97
Caio César <caiocesarmcruz@gmail.com>
2021-08-14 20:30:20 -0300

    requisitos 4 e 5

---
+++ script.js
@@ -0,0 +1,16 @@
+let rowcol = 5;
+let board = document.getElementById("pixel-board");
+
+function pxlgrid() {
+    for (let i = 0; i < rowcol; i += 1) {
+        let test = document.createElement("div");
+        test.className = "pixels";
+        board.appendChild(test);
+        for (let ind = 0; ind < rowcol; ind += 1) {
+            let test2 = document.createElement("div");
+            test2.className = "pixel";
+            test.appendChild(test2);
+        }
+    } 
+}
+pxlgrid()
