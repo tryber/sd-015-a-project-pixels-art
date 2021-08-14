b89ccc498b0d0c607c6ff393e443fe6a1a65d4b3
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2021-08-14 00:02:55 -0300

    Requisito 3 e 4 ok

--- script.js
+++ script.js
@@ -6,17 +6,43 @@ function createTag(tag) {
 
 const palette = document.getElementById('color-palette');
 
-const color1 = 1;
-const color2 = 1;
-const color3 = 1;
-const color4 = 1;
-
-
-
 for (let number = 0; number < 4; number += 1) {
   palette.appendChild(createTag('div'));
 }
 const selector = document.querySelectorAll('#color-palette div');
 
-console.log(selector);
 selector.forEach((elemen) => elemen.classList.add('color'));
+
+const board = document.getElementById('pixel-board');
+
+function sizeOfBoard(size) {
+  for (let indexLine = 0; indexLine < size; indexLine += 1) {
+    const createLine = createTag('div');
+    createLine.classList.add('line');
+    board.appendChild(createLine);
+
+    for (let indexCol = 0; indexCol < size; indexCol += 1) {
+      const createColumn = createTag('div');
+      createLine.appendChild(createColumn);
+      createColumn.classList.add('pixel');
+    }
+  }
+}
+
+function generateBoard(size) {
+  if (size < 5) {
+    const sizeMin = 5;
+    sizeOfBoard(sizeMin);
+  } else if (size > 50) {
+    const sizeMax = 50;
+    sizeOfBoard(sizeMax);
+  } else {
+    sizeOfBoard(size);
+  }
+}
+
+generateBoard(5);
+
+// const pixels = document.querySelectorAll('#pixel-board div');
+
+// pixels.forEach((elemen) => elemen.classList.add('pixels'));
