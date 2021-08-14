b72da3a01eb48c254d933e4f8fb44baea096197e
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-08-14 20:51:38 -0300

    adequa ao lint

--- script.js
+++ script.js
@@ -19,10 +19,6 @@ createTable();
 
 // Seleciona cor
 const allColors = document.querySelectorAll('.color');
-for (let index = 0; index < allColors.length; index += 1) {
-  const selectedColor = document.querySelectorAll('.color')[index];                
-  selectedColor.addEventListener('click', selectColor);             
-}
 
 function selectColor(event) {
   let selected = document.querySelector('.selected');
@@ -30,24 +26,30 @@ function selectColor(event) {
   event.target.classList.add('selected');
 }
 
+for (let index = 0; index < allColors.length; index += 1) {
+  const selectedColor = document.querySelectorAll('.color')[index];
+  selectedColor.addEventListener('click', selectColor);
+}
+
 // Define cor
 let definedColor = 'black';
 
 const palette = document.querySelector('#color-palette');
-palette.addEventListener('click', defineColor);                      
+palette.addEventListener('click', defineColor);
 
 function defineColor() {
   const selectedColor = document.querySelector('.selected');
   definedColor = window.getComputedStyle(selectedColor).getPropertyValue('background-color');
 }
 
-// Pinta os pixels 
-const allPixels = document.querySelectorAll('.pixel');                      
-for (let index = 0; index < allPixels.length; index += 1) {
-  let selectedPixel = document.querySelectorAll('.pixel')[index];      
-  selectedPixel.addEventListener('click', paintPixel);                       
-}
+// Pinta os pixels
+const allPixels = document.querySelectorAll('.pixel');
 
 function paintPixel(event) {
   event.target.style.backgroundColor = definedColor;
+}
+
+for (let index = 0; index < allPixels.length; index += 1) {
+  const selectedPixel = document.querySelectorAll('.pixel')[index];
+  selectedPixel.addEventListener('click', paintPixel);
 }
