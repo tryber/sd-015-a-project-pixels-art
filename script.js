8c3dbc745aefbfb7fade467e3aa302f72b1474f9
SaraRabelo <rabelorsara@gmail.com>
2021-08-14 04:28:44 -0300

    Corrigindo lint

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-window.onload = function(){
-  let selecao = document.getElementsByClassName('color') [0];
-  selecao.classList.add('selected'); 
-}
+window.onload = function () {
+  const selecao = document.getElementsByClassName('color')[0];
+  selecao.classList.add('selected');
+};
 const paletaPixel = document.querySelector('#color-palette');
 const qtePixelPaleta = 4;
 for (let index = 0; index < qtePixelPaleta; index += 1) {
   const paletadiv = document.createElement('div');
   paletadiv.className = 'color';
   if (index === 0) {
-    paletadiv.id = "Black";   
+    paletadiv.id = 'Black';   
     paletadiv.style.backgroundColor = 'black';
-  } 
+  }; 
   if (index === 1) {
-    paletadiv.id = "Blue";  
+    paletadiv.id = 'Blue';  
     paletadiv.style.backgroundColor = 'blue';
-  } 
+  };
   if (index === 2) {
-    paletadiv.id = "Yellow";  
+    paletadiv.id = 'Yellow';  
     paletadiv.style.backgroundColor = 'yellow';    
-  }   
+  };
   if (index === 3) {
-    paletadiv.id = "red";        
+    paletadiv.id = 'red';
     paletadiv.style.backgroundColor = 'red';
-  }
+  };
 
   paletadiv.style.border = '1px solid black';
   paletadiv.style.width = '50px';
   paletadiv.style.height = '50px';
-  paletaPixel.appendChild(paletadiv);     
+  paletaPixel.appendChild(paletadiv); 
 };
 
-let pixels = document.querySelector("#pixel-board");
-let qtePixel = 25;
+const pixels = document.querySelector('#pixel-board');
+const qtePixel = 25;
 for (let index = 0; index < qtePixel; index += 1) {
-  let paletadiv = document.createElement('div');
+  const paletadiv = document.createElement('div');
   paletadiv.className = 'pixel';
   paletadiv.style.border = '1px solid';
   paletadiv.style.width = '40px';
   paletadiv.style.height = '40px';
   paletadiv.style.backgroundColor = 'rgb(255,255,255)';
-  pixels.appendChild(paletadiv);  
-}
+  pixels.appendChild(paletadiv);
+};
