206386b060e4fd6f147aaf65f12ac3c7a5a8ffd3
William <william.mk1@outlook.com>
2021-08-13 21:43:13 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let size = 5;
 let board = document.querySelector('#pixel-board');
 let pixelPalett = document.querySelector('#color-palette');
+let colors = document.querySelectorAll('.color');
+let boardPixels = document.querySelectorAll('.board-pixels');
 
 function createLine(size) {
   for (let line = 1; line <= size; line += 1) {
     let createBoardLines = document.createElement('div');
+    createBoardLines.className = 'board-pixels';
     board.appendChild(createBoardLines);
     for (let column = 1; column <= size; column += 1) {
       let createBoard = document.createElement('div');
@@ -27,4 +30,10 @@ pixelPalett.addEventListener('click', function(event) {
   let selectedColor = document.querySelector('.selected');
   selectedColor.classList.remove('selected');
   event.target.classList.add('selected');
+})
+
+
+board.addEventListener('click', function(event) {
+  event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+
 })
