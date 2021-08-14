adf45aed8f04a580acb00b548460bd489e66701f
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-08-14 08:39:29 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+function className() {
+  const nameClassBlack = document.querySelector('.color');
+  nameClassBlack.classList.add('selected');
+}
+
+function createPixel() {
+  for (let indexLine = 0; indexLine < 5; indexLine += 1) {
+    for (let index = 0; index < 5; index += 1) {
+      const divPixelLine = document.querySelectorAll('.line');
+      const divPixel = document.createElement('div');
+      divPixel.style.background = 'rgb(255,255,255)';
+      divPixel.className = 'pixel';
+      divPixelLine[indexLine].appendChild(divPixel);
+    }
+  }
+  className()
+}
+function createLinePixel() {
+  for (let indexLine = 0; indexLine < 5; indexLine += 1) {
+    const divPixelBoard = document.querySelector('#pixel-board');
+    const divLine = document.createElement('div');
+    divLine.className = 'line';
+    divPixelBoard.appendChild(divLine);
+  }
+  createPixel();
+}
+
 // Base da Função utilizada do site https://stackoverflow.com/questions/11162664/generate-color-palette-using-javascript.
 
 // Para atender o Requisito 02 foram usadas duas funções, a createDiv fica responsável para criar divs onde serão armazenasdas as paletas de cores.
@@ -30,26 +57,4 @@ function createColorPalette() {
   }
   createLinePixel();
 }
-
-function createLinePixel() {
-  for (let indexLine = 0; indexLine < 5; indexLine += 1) {
-    const divPixelBoard = document.querySelector('#pixel-board');
-    const divLine = document.createElement('div');
-    divLine.className = 'line';
-    divPixelBoard.appendChild(divLine);
-  }
-  createPixel();
-}
-
-function createPixel() {
-  for (let indexLine = 0; indexLine < 5; indexLine += 1) {
-    for (let index = 0; index < 5; index += 1) {
-      const divPixelLine = document.querySelectorAll('.line');
-      const divPixel = document.createElement('div');
-      divPixel.style.background = 'rgb(255,255,255)';
-      divPixel.className = 'pixel';
-      divPixelLine[indexLine].appendChild(divPixel);
-    }
-  }
-}
-window.onload = createColorPalette();
+createColorPalette();
