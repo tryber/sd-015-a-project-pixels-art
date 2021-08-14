4ec781eb74c5d2d381855d57582ae6dde5adb161
Adadezer <adadezer@gmail.com>
2021-08-13 23:14:07 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const paletaCores = document.querySelector("#color-palette");
+const paletaCores = document.querySelector('#color-palette');
 
 function cores() {
   for (let index = 0; index < 4; index += 1) {
     const cores = document.createElement('div');
     cores.className = 'color';
+
     if (index === 0) {
-      cores.style.backgroundColor = 'black';
+      cores.id = 'black';
       cores.className = 'color selected';
+      cores.style.backgroundColor = 'black';
     }
     if (index === 1) {
+      cores.id = 'green';
       cores.style.backgroundColor = 'green';
     }
     if (index === 2) {
+      cores.id = 'red';
       cores.style.backgroundColor = 'red';
     }
     if (index === 3) {
+      cores.id = 'blue';
       cores.style.backgroundColor = 'blue';
     }
     paletaCores.appendChild(cores);
@@ -22,23 +27,42 @@ function cores() {
 }
 cores();
 
-function linhaPixel() {
-  const quadroPixels = document.createElement('div');
-  quadroPixels.id = 'pixel-board';
-  document.body.appendChild(quadroPixels);
+const quadroPixels = document.querySelector('#pixel-board');
+
+function criarlinhaPixel() {
+  // crio a linha que vai ter os pixels
+  const linhaPixel = document.createElement('div');
+  document.body.appendChild(linhaPixel);
 
   for (let i = 0; i < 5; i += 1) {
+    // crio cada pixel que vai dentro de 1 linha
     const pixel = document.createElement('div');
     pixel.className = 'pixel';
-    quadroPixels.appendChild(pixel);
+    linhaPixel.appendChild(pixel);
+    quadroPixels.appendChild(linhaPixel);
   }
 }
 
 function repetelinhaPixel() {
   for (let i = 0; i < 5; i += 1) {
-    linhaPixel();
+    // crio a linha várias vezes
+    criarlinhaPixel();
   }
 }
 repetelinhaPixel();
 
+function selecionado(event) {
+  const selected = document.querySelector('.selected'); // pego a classe selected
+  selected.classList.remove('selected'); // removo de onde ela está
+  event.target.classList.add('selected'); // coloco em quem chamou o evento
+}
 
+const colorBlack = document.querySelector('#black');
+const colorGreen = document.querySelector('#green');
+const colorRed = document.querySelector('#red');
+const colorBlue = document.querySelector('#blue');
+
+colorBlack.addEventListener('click', selecionado);
+colorGreen.addEventListener('click', selecionado);
+colorRed.addEventListener('click', selecionado);
+colorBlue.addEventListener('click', selecionado);
