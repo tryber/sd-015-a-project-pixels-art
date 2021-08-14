e17f2d69497fdab39174f339856656189eaa3337
Ricardo <ricardoanj.14@gmail.com>
2021-08-14 13:26:00 -0300

    pequenas alteraçoes gerais

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// const base = 5 
-// const linhas = base
-// const board = document.querySelector('#pixel-board')
-
-// function criarPixels () {
-//     for (let i = 0; i < base; i += 1);
-//     let pixel = base[i];
-//     let quadrados = document.createElement('div')
-//     quadrados.className = 'color';
-//     quadrados.innerHTML = pixel;
-//     board.appendChild(quadrados)
-// }
-//criarPixels();
-// const divs = []
-// function colorForChange () {
-//  const color1 = document.getElementsById('color-palette')
-
-//  for (let index = 0; index < 26; index += 1) {
-//      let inColor = color1[index]
-//      const divColor = document.createElement('div')
-//      if (inColor = inColor[0])
-//      divColor.className = 'color'
-//      divColor.innerHTML = inColor
-//      color1.appendChild(divColor)
-//  }
-
 const cont = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']; 
 function criarQ () {
 const divs = document.getElementById('pixel-board')
