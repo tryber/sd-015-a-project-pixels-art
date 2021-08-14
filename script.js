8b05fbf2b16134797916a74d56afee86e2017674
Ricardo <ricardoanj.14@gmail.com>
2021-08-14 13:16:02 -0300

    alteraçoes no arquivo css

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const divs = document.getElementById('pixel-board')
 for (let i = 0; i < cont.length; i += 1) {
     let linha = cont[i];
     const div25 = document.createElement('div')
-    div25.className = 'color'
+    div25.className = 'pixel'
     div25.innerHTML = linha
     divs.appendChild(div25)
 }
