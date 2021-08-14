3bcfbe6dd50d63e2c8e98ca187ba8a32a549d6a9
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-08-14 15:17:06 -0300

    10-requisito

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.onload = function () {
+    paintPixel();
+
+}
+
+
 //Seleciona a cor
 let cor = document.querySelectorAll('.color');
 let selected = document.querySelector('.selected')
@@ -11,6 +17,7 @@ for (let i = 0; i < cor.length; i++) {
 }
 
 //Pinta o pixel com a cor selecionada
+function paintPixel () {
 let pixel = document.querySelectorAll('.pixel')
 for (let i = 0; i < pixel.length; i++){
     pixel[i].addEventListener('click', pintar)
@@ -18,10 +25,11 @@ for (let i = 0; i < pixel.length; i++){
         pixel[i].style.backgroundColor = selected.classList[1]
     }
 }
+}
 
 //Botão para limpar o quadro
-let btn = document.getElementById('clear-board')
-btn.addEventListener('click', limparQuadro);
+let btnClear = document.getElementById('clear-board')
+btnClear.addEventListener('click', limparQuadro);
 function limparQuadro () {
     let todosPixel = document.querySelectorAll('.pixel')
     for (let i = 0; i < todosPixel.length; i++) {
@@ -30,4 +38,45 @@ function limparQuadro () {
 }
 
 
+//Defini o número de pixels
+let btnGenerate = document.querySelector('#generate-board');
+// Resgata o valor do input
+btnGenerate.addEventListener('click', value);
+function value () {
+    if (document.querySelector('#board-size').value <= 0 || document.querySelector('#board-size').value === '') {
+        alert('Board inválido!');
+    } else { 
+        generate();
+    }
+}
 
+// Gera o quadro novo
+function generate () {
+    let valueBoard = document.querySelector('#board-size').value
+
+    //Apaga todo o quadro
+    let pixelBoard = document.querySelector('#pixel-board')
+    let borderPixel = document.querySelector('.border-pixel-board');
+    pixelBoard.removeChild(borderPixel);
+
+    //Cria um novo quadro
+    let createBorderPixel = document.createElement('div')
+    pixelBoard.appendChild(createBorderPixel).className = 'border-pixel-board';
+
+    //Cria as linhas
+    for (let i = 0; i < valueBoard; i++){
+        let createLine = document.createElement('div');
+        createBorderPixel.appendChild(createLine).className = 'linha';
+    }
+
+    // Cria os pixels
+    let lines = document.querySelectorAll('.linha')
+    for (let i = 0; i < lines.length; i++) {
+    for (let i = 0; i < lines.length; i++) {
+        let createPixel = document.createElement('div')
+        lines[i].appendChild(createPixel).className = 'pixel';
+    } 
+    }
+    // Chama a função para atualizar a lista de pixel
+    paintPixel();
+}
