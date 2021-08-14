98c728234edf108caf745e0320d9c14a691de162
Anderson Alencar <anderson23alencar@gmail.com>
2021-08-14 11:44:34 -0300

    Requisito 8 - pinta o pixel com a cor selecionada na paleta

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ for (let color of selectedColor) {
     color.classList.add('selected');
   });
 }
+//
+let selectedPixel = document.querySelectorAll('.pixel');
+
+for (let pixel of selectedPixel) {
+  pixel.addEventListener('click', function(){
+    let actualSelected = document.querySelector('.selected');
+
+    if (actualSelected === selectedColor[0]){
+      pixel.classList.remove('pink', 'blue', 'green');
+      pixel.classList.add('black');
+    } else if (actualSelected === selectedColor[1]){
+      pixel.classList.remove('black', 'blue', 'green');
+      pixel.classList.add('pink');
+    } else if (actualSelected === selectedColor[2]){
+      pixel.classList.remove('pink', 'black', 'green');
+      pixel.classList.add('blue');
+    } else if (actualSelected === selectedColor[3]){
+      pixel.classList.remove('pink', 'blue', 'black');
+      pixel.classList.add('green');
+    }
+  });
+}
+
