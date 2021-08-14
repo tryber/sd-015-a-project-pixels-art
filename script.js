3d76cedf101a5045b785a40a30550c6d48e32287
antoniofortisneto <antoniofortis@gmail.com>
2021-08-14 01:55:02 -0300

    ta quase indo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("batata")
+window.onload = function(){
+}
+
+function criadorCaixa(){
+ let criador = document.querySelector( "#pixel-board");
+ for (let cont = 1; cont < 6; cont +=1 ){
+    let coluna = document.createElement("div");
+    criador.appendChild(coluna);
+ for (let i = 1; i < 6;i +=1 ){
+    let linha = document.createElement("div");
+    coluna.appendChild(linha);
+    linha.className = 'pixel';
+
+  }
+ }
+}
