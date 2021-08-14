7cb0153c824dc92fa7e1acd5740fc2bef4e62ebc
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-14 01:14:55 -0300

    add randomic colors

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 /* eslint-disable no-unused-vars */
 const BOARDPIXEL = 'pixel-board';
 let NUMOFPIXELS = 25;
-function createBox(parent) {
-  const colors = ['black', 'blue', 'green', 'yellow'];
-  for (let i = 0; i < colors.length; i += 1) {
-    const newDiv = document.createElement('div');
-    const newLi = document.createElement('li');
-    const parentElement = document.getElementById(parent);
 
-    newDiv.className = 'color';
-    newDiv.style.backgroundColor = colors[i];
-    parentElement.appendChild(newDiv);
-    newDiv.appendChild(newLi);
+function createBoxHelper(parent, color) {
+  const newDiv = document.createElement('div');
+  const newLi = document.createElement('li');
+  const parentElement = document.getElementById(parent);
+
+  newDiv.className = 'color';
+  newDiv.style.backgroundColor = color;
+  parentElement.appendChild(newDiv);
+  newDiv.appendChild(newLi);
+}
+function createBox(parent) {
+  createBoxHelper(parent, 'black');
+  for (let i = 0; i < 3; i += 1) {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    createBoxHelper(parent, `rgb(${red}, ${green}, ${blue})`);
   }
 }
 
