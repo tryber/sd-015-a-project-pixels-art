a3f5c0f98b3919ff1491d6503e9882f102735d1e
Arthur <arthurteixeira0805@gmail.com>
2021-08-15 18:57:05 -0300

    req 5 done and 6 in development

--- script.js
+++ script.js
@@ -24,15 +24,58 @@ function createColors() {
     }
 }
 
-function settingBorder() {
-    for (let i = 0; i <= 3; i += 1) {
-    let colors = document.getElementsByClassName('color')[i];
-    colors.style.borderWidth = '1px';
-    }    
+function boardOfP() {
+    let sec = document.createElement('section');
+    sec.id = 'pixel-board';
+    elementMain.appendChild(sec);
 }
+
+function pixels() {
+    for (let i = 0; i <= 4; i += 1) {
+        let palB = document.getElementById('pixel-board');
+        let line = document.createElement('div');
+        line.className = 'line';
+        palB.appendChild(line);
+        lineP(i);
+
+    }
+}
+
+function lineP(ln) {
+    for (let i2 = 1; i2 <= 5; i2 += 1) {
+        let getPB = document.getElementsByClassName('line')[ln];
+        let pix = document.createElement('div');
+        pix.className = 'pixel clicked';
+        pix.id = 'a' + i2;
+        getPB.appendChild(pix);
+        
+    }
+}
+
+function brush() {
+    document.addEventListener('click', selecting())
+}
+
+function selecting() {
+    
+}
+
+let selected = 'black'
+let selectable1 = document.querySelector('#c1');
+let selectable2 = document.querySelector('#c2');
+let selectable3 = document.querySelector('#c3');
+let selectable4 = document.querySelector('#c4');
+
+// function settingBorder() {
+//     for (let i = 0; i <= 3; i += 1) {
+//     let colors = document.getElementsByClassName('color')[i];
+//     colors.style.borderWidth = '1px';
+//     }    
+// }
 
 createColorsPalleteTitle()
 createPallete()
 createColors()
-settingBorder()
-
+// settingBorder()
+boardOfP()
+pixels()
