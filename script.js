81c59b5a757c4c7a98c0e0c2ddc2f07c8139a910
André <andre.bonaf@gmail.com>
2021-08-15 17:05:26 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -27,21 +27,41 @@ function createGrid() {
 
     parent.appendChild(divG);
   }
+  document.querySelector('.pixel').classList.add('selectedColor');
 }
 
 createGrid();
 
+let toPaint = document.querySelector('.selected').classList[1];
+
 function setSelected() {
   const selectedBlock = document.querySelector('.selected');
   const clickedBlock = event.currentTarget;
-  
+
   if (clickedBlock.classList.contains('selected') === false) {
     clickedBlock.classList.add('selected');
     selectedBlock.classList.remove('selected');
+    toPaint = clickedBlock.classList[1];
   }
+}
+
+function setSelectedColor() {
+  const selectedBlock = document.querySelector('.selectedColor');
+  const clickedBlock = event.currentTarget;
+
+  if (clickedBlock.classList.contains('selectedColor') === false) {
+    clickedBlock.classList.add('selectedColor');
+    selectedBlock.classList.remove('selectedColor');
+  }
+  clickedBlock.style.backgroundColor = toPaint;
 }
 
 for (let i = 0; i < colors.length; i += 1) {
   const blockColors = document.querySelectorAll('.color')[i];
   blockColors.addEventListener('click', setSelected);
+}
+
+for (let i = 0; i < gridSize; i += 1) {
+  const blockColors = document.querySelectorAll('.pixel')[i];
+  blockColors.addEventListener('click', setSelectedColor);
 }
