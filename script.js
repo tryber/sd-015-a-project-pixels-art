d16c8a112e3f6754964754845df6c41a03957ad5
Anderson-Zobel <anderson.zobels@gmail.com>
2021-08-15 13:56:50 -0300

    adicionado: aprimaramentos, requisito 4 e 5

--- script.js
+++ script.js
@@ -8,4 +8,24 @@ function createPalette() {
 }
 createPalette();
 
+// Create Pixe-Board
 
+function createBoard() {
+    let pixelBoard = document.createElement('section');
+    pixelBoard.id = 'pixel-board';
+    document.body.appendChild(pixelBoard);
+ 
+    
+    for (i = 0 ; i < 5; i += 1) {
+        let line = document.createElement('div');  
+        line.className = 'line'      
+        pixelBoard.appendChild(line);        
+        for (x = 0 ; x < 5; x += 1){
+        let pixelBlock = document.createElement('div');
+        pixelBlock.className = 'pixel';
+        pixelBlock.style.backgroundColor = 'white';
+        line.appendChild(pixelBlock);
+        }
+    }
+}
+createBoard();
