6231f0bb57e9198cc9ca3fef1e8c08b897c3062e
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-08-15 00:37:56 -0300

    Adiciona Requisito 10 e 11

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const palette = document.getElementById('color-palette');
 const colors = ['rgb(0, 0, 0)', 'rgb(142, 35, 107)', 'rgb(255, 127, 0)', 'rgb(33, 94, 33)'];
-const board = document.getElementById('pixel-board');
+
 
 
 function createItemsPalette() {
@@ -19,18 +19,48 @@ function createItemsPalette() {
 }
 createItemsPalette();
 
+function removeLines () {
+    let parent = document.getElementById('pixel-board');
+    let line = document.querySelectorAll('.lines');
+    for (let i = 0; i < line.length; i += 1) {
+        parent.removeChild(line[i]);
+    }
+}
+const buttonVqv = document.getElementById('generate-board');
+buttonVqv.addEventListener('click', resizeBoard);
+function resizeBoard () {
+    let inputValue = document.getElementById('board-size').value;
+    if (inputValue === "") {
+        alert("Board Inválido!")
+    } else {
+        removeLines();
+        let numberValue = parseInt(inputValue, 10);
+        createDiv(numberValue);
+    }
+}
+
 //Cria 5 divs filhas do board que serão as linhas
+const board = document.getElementById('pixel-board');
 function createDiv(n) {
     for (let i = 1; i <= n; i += 1) {
     let line = document.createElement('div');
     line.className = 'lines';
     board.appendChild(line);
     }
+    createPixels();
 }
-createDiv(5);
 
-const lines = document.querySelectorAll(".lines");
+function conditionToCreate () {
+    let inputValue = document.getElementById('board-size').value
+    if (inputValue === "") {
+        createDiv(5)
+    }
+}
+conditionToCreate();
+
+
 function createPixels() {
+    let lines = document.querySelectorAll(".lines");
     for (let index = 0; index < lines.length; index += 1) {
         for (let index2 = 1; index2 <= lines.length; index2 += 1) {
             let pixel = document.createElement('div');
@@ -39,17 +69,13 @@ function createPixels() {
             lines[index].appendChild(pixel);
         }
     }
+    pixelsListener();
 }
-createPixels()
-
-
-
 window.onload = colorOnload;
 const color = document.getElementsByClassName('color');
 function colorOnload () {
     let colorBlack = color[0];
     colorBlack.classList.add('selected');
-    let selectColor = document.querySelector('.selected');
 }
 colorOnload();
 
@@ -60,13 +86,20 @@ function selecionaCor (event) {
     event.target.classList.add('selected');
     
 }
-board.addEventListener('click', paintPixels);
+function pixelsListener () {
+    let pixels = document.querySelectorAll('.pixel');
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].addEventListener('click', paintPixels);
+    }
+}
+
+
 function paintPixels (event) {
     let colorCurrent = document.querySelector('.selected');
     let rgbColorCurrent = colorCurrent.style.backgroundColor;
     event.target.style.backgroundColor = rgbColorCurrent;
-
 }
+
 const buttonClear = document.querySelector('#clear-board');
 buttonClear.addEventListener('click', clearPixels);
 function clearPixels () {
