0bea0dc9f38ca6e3abdc665d9e6a9ccb89d48a35
Ismael Peccin <ismaelpeccin@gmail.com>
2021-08-15 16:38:45 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-  const boardSection = document.getElementById('pixel-board');
-  const n = 5;
+const boardSection = document.getElementById('pixel-board');
+const n = 5;
 
-  function createLines() {
-    for (let index = 0; index < n; index += 1) {
-      const pixelBox = document.createElement('div');
-      boardSection.appendChild(pixelBox);
-      pixelBox.className = 'pixel';
-      pixelBox.style.backgroundColor = 'white';
-    }
+function createLines() {
+  for (let index = 0; index < n; index += 1) {
+    const pixelBox = document.createElement('div');
+    boardSection.appendChild(pixelBox);
+    pixelBox.className = 'pixel';
+    pixelBox.style.backgroundColor = 'white';
   }
-   function createColumns() {
-     for (let index = 0; index < n; index += 1) {
-       createLines();
-     }
-   }
-  createColumns();
-  
-  function clickColor (){
-    let palette = document.querySelectorAll('.color');
-
-    for (let index2 = 0; index2 < palette.length; index2 += 1) {
-      palette[index2].addEventListener('click', function (event){
-        document.querySelector('.selected').classList.remove('selected');
-        event.target.classList.add('selected');
-      });
-    }
+}
+function createColumns() {
+  for (let index = 0; index < n; index += 1) {
+    createLines();
   }
-  clickColor(); 
+}
+createColumns();
 
-  
-  
+function clickColor() {
+  const palette = document.querySelectorAll('.color');
+
+  for (let index2 = 0; index2 < palette.length; index2 += 1) {
+    palette[index2].addEventListener('click', function (event){
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+    });
+  }
+}
+clickColor();
+
+function colorPixels() {
+  let pixels = document.querySelectorAll('.pixel');
+
+  for (let index3 = 0; index3 < pixels.length; index3 +=1) {
+    pixels[index3].addEventListener('click', function (event){
+      event.target.style.backgroundColor = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
+    });
+  }
+}
+colorPixels(); 
+
+
 
