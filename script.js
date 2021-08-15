c1e80140cf560f7034b1d53762033e675bad2140
Augusto Alves <augusto.malves@yahoo.com.br>
2021-08-15 01:29:13 -0300

    finalizando 100%

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const colorSelector = document.getElementsByClassName('color');
 const colorMenu = document.getElementById('color-palette');
 const canvas = document.getElementById('pixel-board');
-const linha = document.querySelectorAll('.line');
-const botao = document.getElementById('clear-board');
+const linha = document.getElementsByClassName('line');
+const botao1 = document.getElementById('clear-board');
+const botao2 = document.getElementById('generate-board');
+const boardSize = document.getElementById('board-size');
 let tamanho = 5;
 
 // cor random
@@ -18,7 +20,18 @@ function setColor() {
     }
   }
 }
-setColor();
+// cria div da linha do canvas
+function createLineElement(classe) {
+  const line = document.createElement('div');
+  line.className = classe;
+  return line;
+}
+
+function createLine(pai) {
+  for (let i = 0; i < tamanho; i += 1) {
+    pai.appendChild(createLineElement('line'));
+  }
+}
 
 // cria o box, ideia retirada do exercicio do final do bloco 5
 function createPixel(classe) {
@@ -39,10 +52,7 @@ function preencheBox(qualLinha) {
     preencheLinha(qualLinha[i]);
   }
 }
-
-preencheBox(linha);
-// final da criação do box
-
+//
 // clique na cor
 function removeSelected() {
   for (let i = 0; i < colorSelector.length; i += 1) {
@@ -54,19 +64,17 @@ function selecionaCor(clicado) {
   removeSelected();
   clicado.target.classList.add('selected');
 }
-colorMenu.addEventListener('click', selecionaCor);
-// final do eventListener de seleção de cor
+
 // pinta o canvas
 function pintaCor(colorir) {
   const selecionada = document.getElementsByClassName('selected');
   const cor = selecionada[0].style.backgroundColor;
+  const colorido = colorir.target;
   if (colorir.target.className === 'pixel') {
-    colorir.target.style.backgroundColor = cor;
+    colorido.style.backgroundColor = cor;
   }
 }
-
-canvas.addEventListener('click', pintaCor);
-// final do pintar o canvas
+//
 // botão para limpar o canvas
 function clear() {
   const cor = document.getElementsByClassName('pixel');
@@ -74,6 +82,44 @@ function clear() {
     cor[i].style.backgroundColor = 'white';
   }
 }
-botao.addEventListener('click', clear);
-// final do botão para limpar o canvas
 
+function defineTamanho() {
+  const tamanhoDefinido = boardSize.value;
+  if (tamanhoDefinido < 5) {
+    tamanho = 5;
+  } else if (tamanhoDefinido > 50) {
+    tamanho = 50;
+  } else {
+    tamanho = boardSize.value;
+  }
+  console.log(tamanho);
+}
+
+function defineCanvas() {
+  if (!boardSize.value) {
+    alert('Board inválido!');
+  } else {
+    defineTamanho();
+    resetaCanvas();
+  }
+}
+function resetaCanvas() {
+  for (let i = 0; i < linha.length; i += 0) {
+    linha[i].remove();
+  }
+  createLine(canvas);
+  preencheBox(linha);
+}
+
+function montaPagina() {
+  // defineCanvas();
+  // iniciaCanvas();
+  createLine(canvas);
+  preencheBox(linha);
+  setColor();
+  colorMenu.addEventListener('click', selecionaCor);
+  canvas.addEventListener('click', pintaCor);
+  botao1.addEventListener('click', clear);
+  botao2.addEventListener('click', defineCanvas);
+}
+montaPagina();
