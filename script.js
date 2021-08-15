6e32378ae826db9e3c1cd760934400db7d77ea14
Bruno Miranda Pereira <brunompe@gmail.com>
2021-08-15 14:56:49 -0400

    Requirements 04 and 05 - improved ESLINT

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = function () {
 
   function createColorPalette () {
-    const colorPaletteSection = document.querySelector("#color-palette");
+    const colorPaletteSection = document.querySelector('#color-palette');
     for (let indexColorPalette = 1; indexColorPalette < 5; indexColorPalette += 1) {
-      let colorPaletteDiv = document.createElement("div");
-      colorPaletteDiv.className = "color"
-      colorPaletteDiv.id = "color" + indexColorPalette;
+      let colorPaletteDiv = document.createElement('div');
+      colorPaletteDiv.className = 'color';
+      colorPaletteDiv.id = 'color' + indexColorPalette;
       colorPaletteSection.appendChild(colorPaletteDiv);
     } 
   }
@@ -15,11 +15,11 @@ window.onload = function () {
     let line = 5;
     let column = 5;
 
-    for ( i = 0; i < line; i += 1) {
-      for (j = 0; j < column; j += 1) {
-        let pixel = document.createElement("div");
-        pixel.className = "pixel"
-        document.getElementById("pixel-board").appendChild(pixel);
+    for (let i = 0; i < line; i += 1) {
+      for (let j = 0; j < column; j += 1) {
+        const pixel = document.createElement('div');
+        pixel.className = "pixel";
+        document.getElementById('pixel-board').appendChild(pixel);
       }
     }
   }
