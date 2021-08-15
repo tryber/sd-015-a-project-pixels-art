922a65e8a9ca3bb817fd43addb67021dc2b25482
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-08-15 14:33:28 -0300

    Correção Identação

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ function generate() {
   if (valueBoard < 5) {
     valueBoard = 5;
   }
-//Apaga todo o quadro//
+  //Apaga todo o quadro// 
   const pixelBoard = document.querySelector('#pixel-board');
   const borderPixel = document.querySelector('.border-pixel-board');
   pixelBoard.removeChild(borderPixel);
-//Cria um novo quadro//
+  //Cria um novo quadro// 
   const createBorderPixel = document.createElement('div');
   pixelBoard.appendChild(createBorderPixel).className = 'border-pixel-board';
-//Cria as linhas//
+  //Cria as linhas//  
   for (let i = 0; i < valueBoard; i += 1) {
     const createLine = document.createElement('div');
     createBorderPixel.appendChild(createLine).className = 'linha';
   }
-//Cria os pixels//
+  //Cria os pixels//  
   const lines = document.querySelectorAll('.linha');
   for (let i = 0; i < lines.length; i += 1) {
     for (let i2 = 0; i2 < lines.length; i2 += 1) {
@@ -36,46 +36,48 @@ function generate() {
       lines[i2].appendChild(createPixel).className = 'pixel';
     }
   }
-//Chama a função para atualizar a lista de pixel//
+//Chama a função para atualizar a lista de pixel//  
 }
-//Defini o número de pixels//
+//Defini o número de pixels// 
 const btnGenerate = document.querySelector('#generate-board');
-//Resgata o valor do input//
+//Resgata o valor do input//  
 function value() {
   const valueBoard = document.querySelector('#board-size').value;
   if (valueBoard <= 0 || valueBoard === '') {
-    alert('Board inválido!');
-  }
-  else generate(); }
+    alert('Board inválido!'); }
+  else generate(); 
+}
 btnGenerate.addEventListener('click', value);
-//Gerador de cores aleatórias//
+//Gerador de cores aleatórias// 
 const borderColor = document.querySelector('.border');
 const color = document.createElement('li');
-borderColor.appendChild(color).className = 'color selected'
+borderColor.appendChild(color).className = 'color selected';
 color.style.backgroundColor = 'black';
-for (let i = 0; i < 3; i++){
+for (let i = 0; i < 3; i += 1) {
   let color = document.createElement('li');
-  borderColor.appendChild(color).className = 'color'
+  borderColor.appendChild(color).className = 'color';
   const a = Math.random() * 255 + 1;
   const b = Math.random() * 255 + 1;
   const c = Math.random() * 255 + 1;
   color.style.backgroundColor = 'rgb' + '(' + a + ',' + b + ',' + c + ')';
 }
-//Seleciona a cor//
+//Seleciona a cor// 
 const cor = document.querySelectorAll('.color');
 let selected = document.querySelector('.selected');
-for (let i = 0; i < cor.length; i =+ 1) {
-  function trocaCor() {
-    selected.classList.remove('selected');
-    cor[i].classList.add('selected');
-    selected = cor[i];
+
+for (let i = 0; i < cor.length; i++) {
+    function trocaCor() {
+      selected.classList.remove('selected');
+      cor[i].classList.add('selected');
+      selected = cor[i]; 
+    }
     cor[i].addEventListener('click', trocaCor);
 }
-}
+
 //Pinta o pixel com a cor selecionada//
 function paintPixel() {
   const pixel = document.querySelectorAll('.pixel');
-  for (let i = 0; i < pixel.length; i =+ 1) {
+  for (let i = 0; i < pixel.length; i += 1) {
     function pintar() {
       console.log(selected.style.backgroundColor);
       pixel[i].style.backgroundColor = selected.style.backgroundColor
@@ -87,4 +89,4 @@ paintPixel();
 
 window.onload = function carregamento () {
   paintPixel();
-}
+} ;
