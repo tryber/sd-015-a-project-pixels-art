b112c7b8eddaf60ad829853ed0fb23e7c2e4caa1
Anderson-Zobel <anderson.zobels@gmail.com>
2021-08-15 00:38:39 -0300

    code improvements

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //color pallete
-color1 = document.querySelectorAll('.color')[0];
-color2 = document.querySelectorAll('.color')[1];
-color3 = document.querySelectorAll('.color')[2];
-color4 = document.querySelectorAll('.color')[3];
-color1.style.backgroundColor = 'black';
-color2.style.backgroundColor = 'red';
-color3.style.backgroundColor = 'green';
-color4.style.backgroundColor = 'blue';
+function createPalette() {
+    for (let i = 0; i <  4; i += 1) {
+    let colorsArray = ['black', 'red', 'green', 'blue'];
+    let color = document.querySelectorAll('.color')[i];
+    color.style.backgroundColor = colorsArray[i];
+   }
+}
+createPalette();
 
 
