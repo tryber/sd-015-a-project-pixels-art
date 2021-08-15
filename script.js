fe32d456b02887b6e68db119cbc3525f96b937c3
Gustavo Lima <lima_gus@outlook.com>
2021-08-15 20:03:39 -0300

    Refatora requisito 7

--- script.js
+++ script.js
@@ -34,17 +34,24 @@ function fillBoard() {
 }
 fillBoard();
 
-// Adiciona a classe selected ao elemento selecionado e remove do anterior
+// Adiciona a classe selected ao elemento principal
 const firstColor = document.getElementById('black');
 firstColor.classList.add('selected');
 
-function newSelected(color) {
-  const selected = document.querySelector('.selected');
-  selected.classList.remove('selected');
-  color.target.classList.add('selected');
-};
+// https://trybecourse.slack.com/archives/C0273HYKPGT/p1628910728162200?thread_ts=1628910663.162100&cid=C0273HYKPGT
+// Código refatorado após consultar a thread do Daniel Tavares no Slack > Adiciona evento de click para todos elementos com a classe color > Adiciona uma função para remover a classe selected do elemento principal e outra para reatribuir a classe selected ao elemento selecionado.
+const palette = document.querySelectorAll('.color');
+function changeSelected() {
+  for (let i = 0; i < palette.length; i += 1) {
+    palette[i].classList.remove('selected');
+  }
+}
 
-black.addEventListener('click', newSelected);
-secondColor.addEventListener('click', newSelected);
-thirdColor.addEventListener('click', newSelected);
-fourthColor.addEventListener('click', newSelected);
+function newSelected(color) {
+  changeSelected();
+  color.target.classList.add('selected');
+}
+
+for (let i = 0; i < palette.length; i += 1) {
+  palette[i].addEventListener('click', newSelected);
+}
