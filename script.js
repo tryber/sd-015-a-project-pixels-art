3731170376f0bda4ad8f28958e7ffdce351ba8a5
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-08-15 13:37:11 -0300

    Correção Identação

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-//Botão para limpar o quadro
-let btnClear = document.getElementById('clear-board')
+//Botão para limpar o quadro //
+const btnClear = document.getElementById('clear-board');
+function limparQuadro() {
+  const todosPixel = document.querySelectorAll('.pixel');
+  for (let i = 0; i < todosPixel.length; i+= 1) {
+    todosPixel[i].style.backgroundColor = 'white';
+  }
+}
 btnClear.addEventListener('click', limparQuadro);
-function limparQuadro () {
-    let todosPixel = document.querySelectorAll('.pixel')
-    for (let i = 0; i < todosPixel.length; i++) {
-        todosPixel[i].style.backgroundColor = 'white'
-    }
-}
 
-
-//Defini o número de pixels
-let btnGenerate = document.querySelector('#generate-board');
-// Resgata o valor do input
-btnGenerate.addEventListener('click', value);
-
-
-function value () {
-    let valueBoard = document.querySelector('#board-size').value;
-    if (valueBoard <= 0 || valueBoard  === '') {
-        alert('Board inválido!');
-    }
-    else generate();
-}
-
-// Gera o quadro novo
+// Gera o quadro novo//
 function generate () {
     let valueBoard = document.querySelector('#board-size').value;
     if (valueBoard > 50) {
@@ -33,22 +18,22 @@ function generate () {
         valueBoard = 5
     }
     console.log(valueBoard)
-    //Apaga todo o quadro
+    //Apaga todo o quadro//
     let pixelBoard = document.querySelector('#pixel-board')
     let borderPixel = document.querySelector('.border-pixel-board');
     pixelBoard.removeChild(borderPixel);
 
-    //Cria um novo quadro
+    //Cria um novo quadro//
     let createBorderPixel = document.createElement('div')
     pixelBoard.appendChild(createBorderPixel).className = 'border-pixel-board';
 
-    //Cria as linhas
+    //Cria as linhas//
     for (let i = 0; i < valueBoard; i++){
         let createLine = document.createElement('div');
         createBorderPixel.appendChild(createLine).className = 'linha';
     }
 
-    // Cria os pixels
+    // Cria os pixels//
     let lines = document.querySelectorAll('.linha')
     for (let i = 0; i < lines.length; i++) {
     for (let i = 0; i < lines.length; i++) {
@@ -56,15 +41,22 @@ function generate () {
         lines[i].appendChild(createPixel).className = 'pixel';
     } 
     }
-    // Chama a função para atualizar a lista de pixel
+    // Chama a função para atualizar a lista de pixel//
     paintPixel();
 }
-
-
-// Gerador de cores aleatórias
+//Defini o número de pixels //
+const btnGenerate = document.querySelector('#generate-board');
+// Resgata o valor do input //
+function value() {
+  const valueBoard = document.querySelector('#board-size').value;
+  if (valueBoard <= 0 || valueBoard === '') {
+    alert('Board inválido!');
+  }
+  else generate();}
+btnGenerate.addEventListener('click', value);
+// Gerador de cores aleatórias //
 let borderColor = document.querySelector('.border')
 let color = document.createElement('li')
-
 borderColor.appendChild(color).className = 'color selected'
 color.style.backgroundColor = 'black';
 for (let i = 0; i < 3; i++){
@@ -75,12 +67,7 @@ for (let i = 0; i < 3; i++){
     let c = Math.random() * 255 + 1;
     color.style.backgroundColor = 'rgb' + '(' + a + ',' + b + ',' + c + ')';
 }
-
-
-
-
-
-//Seleciona a cor
+//Seleciona a cor //
 let cor = document.querySelectorAll('.color');
 let selected = document.querySelector('.selected')
 for (let i = 0; i < cor.length; i++) {
@@ -91,19 +78,17 @@ for (let i = 0; i < cor.length; i++) {
         selected = cor[i];
     }
 }
-
-//Pinta o pixel com a cor selecionada
+//Pinta o pixel com a cor selecionada //
 function paintPixel () {
-    let pixel = document.querySelectorAll('.pixel')
-    for (let i = 0; i < pixel.length; i++){
-        pixel[i].addEventListener('click', pintar)
-        function pintar (){
-            console.log(selected.style.backgroundColor)
-            pixel[i].style.backgroundColor = selected.style.backgroundColor
-        }
+  let pixel = document.querySelectorAll('.pixel')
+  for (let i = 0; i < pixel.length; i++){
+    pixel[i].addEventListener('click', pintar)
+    function pintar (){
+    console.log(selected.style.backgroundColor)
+     pixel[i].style.backgroundColor = selected.style.backgroundColor
     }
-    }
-
+  }
+}
 window.onload = function () {
   paintPixel();
 }
