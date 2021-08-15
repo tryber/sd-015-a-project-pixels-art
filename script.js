95f3138174d10a84663120b13fba00e64a8d5944
Leandro Basilio <leandroab07@gmail.com>
2021-08-15 04:08:32 -0300

    Adiciona pintura, com base na cor selecionada na paleta, ao clicar em pixels

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let paitingPixels = document.querySelector('#pixel-board');
 let colors = document.getElementsByClassName('color');
+let colorSelected = document.querySelectorAll('.color')
+let selected = document.querySelector('.selected');
 
 function makeTable(number) {
   for (let i = 0; i < number; i += 1) {
@@ -17,10 +19,34 @@ function makeTable(number) {
 
 makeTable(5);
 
+let code = 0;
+
 for (let i = 0; i < colors.length; i += 1) {
   colors[i].addEventListener("click", function() {
     document.getElementsByClassName('selected')[0].className = 'color';
     colors[i].className = 'color selected';
+    code = i;
   })
 }
 
+function selectColor(cor) {
+  document.querySelector('.selected').classList.remove("selected");
+  cor.target.classList.add('selected');
+}
+
+let color = '';
+
+for (let i = 0; i < document.querySelectorAll('.pixel').length; i += 1) {
+  document.querySelectorAll('.pixel')[i].addEventListener('click', function() {
+    if (code === 0) {
+      color = 'black';
+    } else if (code === 1) {
+      color = 'blue';
+    } else if (code === 2) {
+      color = 'yellow';
+    } else if (code === 3) {
+      color = 'red';
+    }
+    event.target.style.backgroundColor = color;
+  });
+}
