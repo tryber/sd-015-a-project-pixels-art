39d96c63ec9f2b75821518d10a3ae8d7c3c22893
Bruno Miranda Pereira <brunompe@gmail.com>
2021-08-15 15:36:23 -0400

    Requirement 06 - done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function () {
 
+  const pixelBoard = document.querySelector("#pixel-board");
+
   function createColorPalette () {
     const colorPaletteSection = document.querySelector('#color-palette');
     for (let indexColorPalette = 1; indexColorPalette < 5; indexColorPalette += 1) {
@@ -7,23 +9,31 @@ window.onload = function () {
       colorPaletteDiv.className = 'color';
       colorPaletteDiv.id = 'color' + indexColorPalette;
       colorPaletteSection.appendChild(colorPaletteDiv);
+      if (colorPaletteDiv.id === 'color1') {
+        colorPaletteDiv.classList.add('selected');
+      }
     } 
   }
   createColorPalette();
-  
-  function createPixelGrid () {
-    let line = 5;
-    let column = 5;
-
-    for (let i = 0; i < line; i += 1) {
-      for (let j = 0; j < column; j += 1) {
-        const pixel = document.createElement('div');
-        pixel.className = "pixel";
-        document.getElementById('pixel-board').appendChild(pixel);
-      }
+    function createGridLine (size, heightWidth) {
+    for (let i = 0; i < size * size; i += 1) {
+      const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixel.style.backgroundColor = 'white';
+    pixel.style.width = `${heightWidth}px`;
+    pixel.style.height = `${heightWidth}px`;
+    pixel.style.border = '1px solid black';
+    pixel.style.display = 'inline-block';
+    document.querySelector('#pixel-board').appendChild(pixel);
     }
   }
-  createPixelGrid();
+
+  function createGrid(size, heightWidth) {
+      createGridLine (size, heightWidth);
+      pixelBoard.style.width = '0px';
+      pixelBoard.style.width = `${((heightWidth + 2) * size)}px`;
+  }
+  createGrid(5, 40);
 }
 
 
