a547ce3328558a1b6109fa22e3cd8d748775d0f6
Monte <you@example.com>
2021-08-15 09:45:13 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 let selected = document.querySelector('.color');
 selected.classList.add('selected');
+
+let section1 = document.querySelectorAll('.color');
+ for ( let index =1; index < section1.length; index +=1 )
+  section1[index].addEventListener('click',  function(){
+    let retira = document.querySelector('.selected')
+    retira.classList.remove('selected')  
+    section1[index].classList.add('selected')  
+ } )
+
+
+  //function changeSelected(){
+    //let retira = document.querySelector('.selected')
+    //retira.classList.remove('selected')    
+ //}
+
+ //requisito 8
+
