a750c0b3f9634fdc3be1812bc80bfa7e153d3547
Thiago Lordello <thiagolordello@hotmail.com>
2021-08-15 20:44:53 -0300

    3a questao

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function corDinamPaleta() {
-    let arrCoresPaleta = ['green', 'blue', 'yellow', 'red'];
+    let arrCoresPaleta = ['black', 'blue', 'yellow', 'red'];
 
     for (let i = 0; i < arrCoresPaleta.length; i += 1) {
         let div = document.getElementsByTagName('div')[i];
