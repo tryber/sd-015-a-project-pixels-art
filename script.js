0008f22862f6cbeb5515c4bd41892ea3600f3b77
carla <carlabazevedoa@gmail.com>
2021-08-15 00:03:18 -0300

    changeColor pronto

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ function selectColor(event) {
   event.target.classList.add('selected');
 }
 
+function changeColor(event) {
+  const color = getComputedStyle(document.querySelector('.selected')).backgroundColor;
+  event.target.style.background = color;
+}
+
 window.onload = function () {
   for (const color of document.querySelectorAll('.color')) {
     color.addEventListener('click', selectColor);
+  }
+  for (const pixel of document.querySelectorAll('.pixel')) {
+    pixel.addEventListener('click', changeColor);
   }
 };
