7554de9405ab11f6f5527a672787b98d3f17c0f1
Ismael Peccin <ismaelpeccin@gmail.com>
2021-08-15 14:25:04 -0300

    Requisto 7git add .

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-  let boardSection = document.getElementById('pixel-board'); 
-  let n = 5; 
+  const boardSection = document.getElementById('pixel-board');
+  const n = 5;
 
-  
-  
-  function createLines () {
-    for (index = 0; index < n; index += 1) {
-      let pixelBox = document.createElement('div'); 
-      boardSection.appendChild (pixelBox);
+  function createLines() {
+    for (let index = 0; index < n; index += 1) {
+      const pixelBox = document.createElement('div');
+      boardSection.appendChild(pixelBox);
       pixelBox.className = 'pixel';
       pixelBox.style.backgroundColor = 'white';
     }
   }
-   function createColumns () {
+   function createColumns() {
      for (let index = 0; index < n; index += 1) {
-       createLines(); 
+       createLines();
      }
    }
-  createColumns(); 
+  createColumns();
+  
+  function clickColor (){
+    let palette = document.querySelectorAll('.color');
+
+    for (let index2 = 0; index2 < palette.length; index2 += 1) {
+      palette[index2].addEventListener('click', function (event){
+        document.querySelector('.selected').classList.remove('selected');
+        event.target.classList.add('selected');
+      });
+    }
+  }
+  clickColor(); 
 
   
-
+  
 
