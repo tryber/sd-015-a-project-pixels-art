b930d76013adc937e74c21dfa4d8a9f3f85c4e4b
Leandro Basilio <leandroab07@gmail.com>
2021-08-15 01:23:58 -0300

    Adiciona percepção de evento de clique e altera a classe "selected" para a cor selecionada

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let paitingPixels = document.querySelector('#pixel-board');
+let colors = document.getElementsByClassName('color');
 
-function makeTable() {
-  for (let i = 0; i < 5; i += 1) {
+function makeTable(number) {
+  for (let i = 0; i < number; i += 1) {
     let ul = document.createElement('ul');
     paitingPixels.appendChild(ul).className = 'lines';
   }
 
-  for (let e = 0; e < 5; e += 1) {
-    for (let i = 0; i < 5; i += 1) {
+  for (let e = 0; e < number; e += 1) {
+    for (let i = 0; i < number; i += 1) {
       let li = document.createElement('li');
       paitingPixels.children[e].appendChild(li).className = 'pixel';
     }
   }
 }
 
-makeTable();
+makeTable(5);
+
+for (let i = 0; i < colors.length; i += 1) {
+  colors[i].addEventListener("click", function() {
+    document.getElementsByClassName('selected')[0].className = 'color';
+    colors[i].className = 'color selected';
+  })
+}
+
