74c8424efd7cd2301dad0f4c8ff810ee071bf974
Pedro Henrique <pedrohrds05@hotmail.com>
2021-08-15 10:08:09 -0300

    Adicionando os requisitos 4 5 6

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-let acess = document.getElementById('color-palette')
+document.getElementById('black-color').style.backgroundColor = 'black'
+document.getElementById('red-color').style.backgroundColor = 'red'
+document.getElementById('blue-color').style.backgroundColor = 'blue'
+document.getElementById('green-color').style.backgroundColor = 'green'
+
+
