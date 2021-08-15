4d0ccf8cad806da876080a8788783990e129ad33
Adadezer <adadezer@gmail.com>
2021-08-14 21:49:45 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// CRIAÇÃO DA PALETA DE CORES
+
+// crio a variavel que pega a div color-palette (paleta de cores) no html
 const paletaCores = document.querySelector('#color-palette');
 
+// função que cria as cores da paleta de cores
 function cores() {
   for (let index = 0; index < 4; index += 1) {
     const cores = document.createElement('div');
-    cores.className = 'color';
+    cores.className = 'color'; // atribui a classe color a todas as cores
 
     if (index === 0) {
       cores.id = 'black';
-      cores.className = 'color selected';
+      cores.className = 'color selected'; // atribui a classe selected
       cores.style.backgroundColor = 'black';
     }
     if (index === 1) {
@@ -27,63 +31,39 @@ function cores() {
 }
 cores();
 
+// CRIAÇÃO DO QUADRO DE PIXELS
+
+// crio a variavel que pega a div pixel-board (quadro de pixels) no html
 const quadroPixels = document.querySelector('#pixel-board');
 
-function criarpixels() {
-  // cria os 25 quadrados, aqui o numero de quadrados em cada linha é controlado pelo css
-  for (let i = 1; i <= 25; i += 1) {
+let tamanho = 5; // variavel que é usada para criar linhas e colunas do quadro de pixels
+
+function criarlinhaPixel() {
+  // crio a 1 linha que vai ter os pixels (essa linha pode ser entendido como sendo a coluna)
+  const linhaPixel = document.createElement('div');
+  document.body.appendChild(linhaPixel);
+
+  for (let i = 0; i < tamanho; i += 1) {
+    // crio cada pixel que vai dentro de 1 linha (o primeiro quadrado em cada coluna)
     const pixel = document.createElement('div');
     pixel.className = 'pixel';
-    // pixel.id = `pixel${[i]}`;
-    quadroPixels.appendChild(pixel);
+    linhaPixel.appendChild(pixel);
+    quadroPixels.appendChild(linhaPixel);
   }
 }
-criarpixels();
 
-// function pixels() {
-//   for (let index = 0; index < 5; index += 1) {
-//     const colunaPixels = document.createElement('div');
-//     colunaPixels.className = 'pixel';
-//     colunaPixels.id = [index];
-//     quadroPixels.appendChild(colunaPixels);
-//     for (let i = 0; i < 4; i += 1) {
-//       const linhaPixels = document.createElement('div');
-//       linhaPixels.className = 'pixel';
-//       linhaPixels.id = [i];
-//       quadroPixels.appendChild(linhaPixels);
-//     }
-//   }
-// }
-// pixels();
-
-/*
-  function criarlinhaPixel() {
-    // crio a linha que vai ter os pixels
-    const linhaPixel = document.createElement('div');
-    document.body.appendChild(linhaPixel);
-
-    for (let i = 0; i < 5; i += 1) {
-      // crio cada pixel que vai dentro de 1 linha
-      const pixel = document.createElement('div');
-      pixel.className = 'pixel';
-      pixel.id = [i];
-      linhaPixel.appendChild(pixel);
-      quadroPixels.appendChild(linhaPixel);
-    }
+function repetelinhaPixel() {
+  for (let i = 0; i < tamanho; i += 1) {
+    // crio a linha várias vezes
+    criarlinhaPixel();
   }
+}
+repetelinhaPixel();
 
-  function repetelinhaPixel() {
-    for (let i = 0; i < 5; i += 1) {
-      // crio a linha várias vezes
-      criarlinhaPixel();
-    }
-  }
-  repetelinhaPixel();
-*/
-
+// CRIAÇÃO DA LOGICA DA CLASSE SELECTED
 function selecionado(event) {
   const selected = document.querySelector('.selected'); // pego a classe selected
-  selected.classList.remove('selected'); // removo de onde ela está
+  selected.classList.remove('selected'); // removo de onde ela está (foi criada na cor preta)
   event.target.classList.add('selected'); // coloco em quem chamou o evento
 }
 
@@ -97,21 +77,56 @@ colorGreen.addEventListener('click', selecionado);
 colorRed.addEventListener('click', selecionado);
 colorBlue.addEventListener('click', selecionado);
 
+// CRIAÇÃO DA LOGICA DE PINTAR OS PIXELS
 function pintaPixel(event) {
-  const pixelS = event.target; // const pixelS pega quem está chamando o evento
-  pixelS.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-  // o bgColor de quem chama o evento é igual ao bgColor do elemento que tem a classe selected
+  const pxl = event.target; // const pxl pega quem está chamando o evento
+  pxl.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+  // o bgColor de quem chama o evento (cada pixel clicado), é igual ao bgColor do elemento que tem a classe selected
 }
-const pixel = document.querySelectorAll('.pixel'); // pega os pixel
+const pixel = document.querySelectorAll('.pixel'); // pega os pixel (a variavel precisa ser let pois ela será modificada mais em baixo)
 for (let i = 0; i < pixel.length; i += 1) {
-  pixel[i].addEventListener('click', pintaPixel); // add em cada um o addEventListener com a função pintaPixel
+  pixel[i].addEventListener('click', pintaPixel); // add em cada pixels o addEventListener com a função pintaPixel
 }
 
+// CRIAÇÃO LOGICA DE LIMPAR O QUADRO DE PIXELS
 function limpaQuadro() {
+  // console.log(pixel); // 25
+  const pixel = document.querySelectorAll('.pixel'); // pixel é declarada aqui pra atualizar o numero de pixels na pagina, e atribuir a cor branca nos pixels antigos, e novos, criados com a função novoQuadroPixel
+  // console.log(pixel); // 125
   for (let i = 0; i < pixel.length; i += 1) {
-    pixel[i].style.backgroundColor = 'white';
+    pixel[i].style.backgroundColor = 'white'; // pinta cada pixel com a cor branca
   }
 }
-
-const btnLimpar = document.querySelector('#clear-board');
+const btnLimpar = document.querySelector('#clear-board'); // pega o botão limpar no html
 btnLimpar.addEventListener('click', limpaQuadro);
+
+// CRIAÇÃO LÓGICA DE ADD NOVAS LINHAS E COLUNAS DE ACORDO COM O INPUT
+
+// aqui como pode existir somente um quadro na tela, vou ter q deletar o quadro antigo, e criar um novo com as linhas e colunas adicionadas
+
+function novoQuadroPixel() {
+  const inputVQV = document.querySelector('#board-size'); // pego o input no html
+  const deletaPixel = document.querySelector('#pixel-board'); // pego o quadro de pixel no html
+
+  if(inputVQV.value <= 0){
+    alert ('Board inválido!');
+  } else {
+    while (deletaPixel.firstChild) { // enquanto existir o primeiro filho no quadro, deleta ele
+      deletaPixel.removeChild(deletaPixel.firstChild);
+      // quando eu deleto o primeiro filho do quadro, o segundo se torna o primeiro, esse segundo é deletado, o terceiro se torna o primeiro, o terceiro é deletado... assim sucessivamente até não haver mais nenhum filho no quadro
+    }
+  
+    // quadro antigo deletado, aqui começa o processo de criação do novo quadro, onde a variavel tamanho recebe o valor do input, e cria as funções de coluna e linha
+  
+    tamanho = inputVQV.value; // a variavel tamanho vai receber o valor do input
+    repetelinhaPixel(); // aqui só preciso do repete pq ele já chama o criarLinhaPixel, q cria as colunas
+  
+    // logica de pintar os pixels: aqui como a função ja foi criada, só preciso colocar o click em cada pixel novamente
+    const pixel = document.querySelectorAll('.pixel'); // pega todos os pixel
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].addEventListener('click', pintaPixel); // add em cada um o addEventListener com a função pintaPixel
+    }
+  }
+}
+const btnVQV = document.querySelector('#generate-board');
+btnVQV.addEventListener('click', novoQuadroPixel);
