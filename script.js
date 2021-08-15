2a92797e4185a15d48a7255a0338e99f4cc78909
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-15 13:15:42 -0400

    Resolve EsLint dos requisitos 2 e 3

--- script.js
+++ script.js
@@ -5,27 +5,26 @@ function createColorPalette() {
   const getSection = document.querySelector('#section-colors');
   colorPalette.id = 'color-palette';
   getSection.appendChild(colorPalette);
-};
+}
 createColorPalette();
 
 // Cria classe color para cada cor individual da paleta de cores
 function createClassColor() {
   for (let index = 1; index <= 4; index += 1) {
-    let getColor = document.getElementById('color-palette');
-    let newColor = document.createElement('div');
-    getColor.appendChild(newColor);
-    newColor.className = 'color';
+    const getColor = document.getElementById('color-palette');
+    const newColorDiv = document.createElement('div');
+    getColor.appendChild(newColorDiv);
+    newColorDiv.className = 'color';
   }
-};
+}
 createClassColor();
 
 // Adicionando cor de fundo à cada elemento da paleta. RESOLVE TAMBÈM REQUISITO 3 (Que pede que a primeira cor da paleta de cores seja PRETA).
-let background = ['black', 'red', 'blue', 'green'];
+const background = ['black'];
 
-function addBackgroundColor(backgroundColors) {  
-  let getClassColor = document.getElementsByClassName('color');
+function addBackgroundColor(background) {
+  const getClassColor = document.getElementsByClassName('color');
   for (let index = 0; index < getClassColor.length; index += 1) {
-    console.log(getClassColor[index])
     if (index !== 0) {
       getClassColor[index].style.backgroundColor = '#' + Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0');
     } else {
@@ -33,12 +32,13 @@ function addBackgroundColor(backgroundColors) {
     }
   }
 };
-addBackgroundColor(background);
+addBackgroundColor(background)
 
 // Requisito 4 - Adicione à página um quadro de pixels, com 25 pixels. Requisito 5 - Resolve também a primeira especificação do requisito 5(Verifica se o quadro de pixels tem altura e comprimento de 5 elementos).
 const newSection = document.createElement('section');
 newSection.id = 'pixel-board';
-principal.appendChild(newSection);
+const getMain = document.querySelector('#principal')
+getMain.appendChild(newSection);
 
 function createFramePixels(param1) {
   if (param1 < 5) {
