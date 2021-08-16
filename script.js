a9a3bd9dfc51311c69f4cb4cdbf69066141960e8
Felipe <felllypes@hotmail.com>
2021-08-15 21:18:41 -0300

    Cumprido até o requisito 4

---
+++ script.js
@@ -0,0 +1,45 @@
+const linhaLousa = 5;
+const colunaLousa = 5;
+
+function createLousa(element, desiredClass, father) { 
+  let box = document.createElement(element);
+  document.getElementById(father).appendChild(box);  
+  box.className = desiredClass;
+}
+
+/* function preencheLousa() {
+  for (let preencheColuna = 0; preencheColuna < colunaLousa; preencheColuna += 1)
+} */
+function giveClass(element, desiredClass) {
+  element.className = desiredClass;
+}
+
+/* createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('br', 'firstPixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('br', 'firstPixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel'); */
+
+
+
+
+ for (let preencheColuna = 0; preencheColuna < colunaLousa; preencheColuna += 1) {
+   
+
+  for (let preencheLinha = 1; preencheLinha <= linhaLousa; preencheLinha += 1) {
+    createLousa('div', 'pixel', 'pixel-board'); 
+  }
+  createLousa('br', 'firstPixelScreen', 'pixel-board');
+}  
