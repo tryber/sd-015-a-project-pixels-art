d4aee3826610439973d47929903e03ed9d9e50cf
Rodrigo Moreira <rodrigossmoreira@hotmail.com>
2021-08-16 14:01:13 -0300

    correçao de escrita

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const quadro = document.getElementById('pixel-board');
 
 function criarquadro() {
-  for (let index = 0; index < 25; index += 1) {
-    let quadroTamanho = document.createElement('div');
-    quadroTamanho.className = 'pixel';
-    quadro.appendChild(quadroTamanho);
-    }; 
-};
+ for (let index = 0; index < 25; index += 1) {
+  const quadroTamanho = document.createElement('div');
+  quadroTamanho.className = 'pixel';
+  quadro.appendChild(quadroTamanho);
+  }; 
+}
 criarquadro();
 
-let corInicial = document.querySelector('#preta');
-function classPreta(){
-    corInicial.classList.add('selected');
+const corInicial = document.querySelector('#preta');
+function classPreta() {
+  corInicial.classList.add('selected');
 }
 classPreta();
 
-let deleteSelected = document.getElementById('color-palette');
-deleteSelected.addEventListener('click', changeSelected);
+const deleteSelected = document.querySelector('#color-palette');
 
-function changeSelected(event){
-    let classDelet = document.querySelectorAll('.color');
-    for (let index = 0; index < classDelet.length; index +=1) {
-        classDelet[index].classList.remove('selected');
-        event.target.classList.add('selected');
-    }
+function changeSelected(event) {
+ const classDelet = document.getElementsByClassName('color');
+ if (event.target.className.includes('color')) {
+  for (let index = 0; index < classDelet.length; index += 1) {
+   classDelet[index].classList.remove('selected');
+   event.target.classList.add('selected');
+   }
+ }
 }
+deleteSelected.addEventListener('click', changeSelected);
 
 let mudaCor = document.getElementsByClassName('pixel');
 for (let i = 0; i < mudaCor.length; i += 1){
