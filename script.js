e422a9f8172d9a769291c59445a2650413e57cbc
Emerson Oliveira <emerson.contato@gmail.com>
2021-08-16 12:04:53 -0300

    Requisito 7 e 8 em andamento

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function selecionaCor(cor) {
     cor.target.classList.add('selected'); 
 }
 
-// for (let cor = 0; cor < document.querySelectorAll('.color').length; cor += 1) {
-//     document.querySelectorAll('.color')[cor].addEventListener('click', selecionaCor);
-// }
-
-document.querySelector('#color-palette').addEventListener('click', selecionaCor);
+for (let cor = 0; cor < document.querySelectorAll('.color').length; cor += 1) {
+    document.querySelectorAll('.color')[cor].addEventListener('click', selecionaCor);
+}
