33b28b6c4549782e20abd7ecd52253b2060d995c
Fernando Sena <fernandosenacruz@gmail.com>
2021-08-16 09:21:01 -0300

    requisito 8 ok

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ let colorSelected = document.getElementsByTagName('div')[0];
 let columnDiv = 5;
 let lineDiv = 5;
 
-
 function randomColor() {
-   let r = Math.floor(Math.random() * 256);
-   let g = Math.floor(Math.random() * 256);
-   let b = Math.floor(Math.random() * 256);
-   
-   return `rgb(${r}, ${g}, ${b})`; //https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`; //https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
 }
 
-
 function changeColorDiv() {
-  for (let index = 1; index < colorDiv.length; index += 1) {
+  for (let index = 0; index < colorDiv.length; index += 1) {
+    if (!colorDiv[index].className.includes('black')) {
+      colorDiv[0].style.backgroundColor = 'black';
+    }
     colorDiv[index].style.backgroundColor = randomColor();
     console.log(colorDiv[index]);
   }
@@ -46,11 +47,22 @@ function selectColor() {
 }
 selectColor(); // 7 ok
 
-// let palleteColor = document.getElementById('color-palette');
-
-// palleteColor.addEventListener('click', function (evt) {
+//  colorDiv.addEventListener('click', function (evt) {
 //   if(evt.target.className.includes('color')){
 //     document.querySelector('.selected').classList.remove('selected');
 //     evt.target.classList.add('selected');
 //   }
 // }); outra forma de fazer o requisto 7 .
+
+pixelBord.addEventListener('click', function (event) {
+  if (event.target.classList.contains('pixel')) {
+    let corSelecionada =
+      document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = corSelecionada;
+  }
+});
+
+// for (let index = 0; index < pixelBordChildren.length; index += 1) {
+//   pixelBordChildren[index]
+
+// }
