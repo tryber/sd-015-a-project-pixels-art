f26cb2f5e90f5431e67da661e6a38c2d4b3233e2
Pedro Henrique <pedrohrds05@hotmail.com>
2021-08-16 17:35:17 -0300

    Cumprindo requisito 8

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-document.getElementById('black-color').style.backgroundColor = 'black'
-document.getElementById('red-color').style.backgroundColor = 'red'
-document.getElementById('blue-color').style.backgroundColor = 'blue'
-document.getElementById('green-color').style.backgroundColor = 'green'
+const accessBlack = document.getElementById('black-color');
+const accessRed = document.getElementById('red-color');
+const acessBlue = document.getElementById('blue-color');
+const accessGreen = document.getElementById('green-color');
+const accessDivBlock = document.querySelectorAll('section.block');
 
-const accessBlack = document.getElementById('black-color')
-const accessRed = document.getElementById('red-color')
-const acessBlue = document.getElementById('blue-color')
-const accessGreen = document.getElementById('green-color')
+document.getElementById('black-color').style.backgroundColor = 'black';
+document.getElementById('red-color').style.backgroundColor = 'red';
+document.getElementById('blue-color').style.backgroundColor = 'blue';
+document.getElementById('green-color').style.backgroundColor = 'green';
 
-function changeClass (change) {
-  const accessClassSelected = document.querySelector('.selected') 
-  accessClassSelected.classList.remove('selected')
-  change.target.classList.add('selected')
+function changeClass(change) {
+  const accessClassSelected = document.querySelector('.selected');
+  accessClassSelected.classList.remove('selected');
+  change.target.classList.add('selected');
 }
 
-accessBlack.addEventListener('click', changeClass)
-accessRed.addEventListener('click', changeClass)
-acessBlue.addEventListener('click', changeClass)
-accessGreen.addEventListener('click', changeClass)
+accessBlack.addEventListener('click', changeClass);
+accessRed.addEventListener('click', changeClass);
+acessBlue.addEventListener('click', changeClass);
+accessGreen.addEventListener('click', changeClass);
+
+function createBlock() {
+  for (let i = 0; i < accessDivBlock.length; i += 1) {
+    const list = accessDivBlock[i];
+    for (let i2 = 0; i2 < accessDivBlock.length; i2 += 1) {
+      const createDiv = document.createElement('div');
+      createDiv.classList.add('pixel');
+      list.appendChild(createDiv);
+    }
+  }
+}
+createBlock();
+
+function changeColor(evento) {
+  const newBackGroundColor = document.querySelector('.selected');
+  const way = evento.target;
+  const change = window.getComputedStyle(newBackGroundColor).getPropertyValue('background-color');
+  way.style.backgroundColor = change;
+}
+
+function selectedBlock() {
+  const accessAlBlocks = document.querySelectorAll('.pixel');
+  for (let i = 0; i < accessAlBlocks.length; i += 1) {
+    const listBlock = accessAlBlocks[i];
+    listBlock.addEventListener('click', changeColor);
+  }
+}
+
+selectedBlock();
