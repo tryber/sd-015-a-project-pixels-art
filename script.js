88902fa1da3094dcc774eba265291ed8f30c9fcb
Lucas Macedo <lucasrrm22@gmail.com>
2021-08-15 23:56:59 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const quadro = document.querySelector('#pixel-board');
-
 for (let index = 0; index < 5; index += 1) {
     for (let indice = 0; indice < 5; indice += 1) {
         const pixel = document.createElement('div');
         pixel.className = 'pixel'
         quadro.appendChild(pixel)
     }
+}
+
+let clickTarget = document.getElementById('pixel-board')
+clickTarget.addEventListener('click', colore)
+function colore(event) {
+    event.target.style.backgroundColor = 'black'
 }
