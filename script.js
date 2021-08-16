d6405a07e7847351e1205dbcd58101329899f3d5
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-08-15 21:32:19 -0300

    requisito 9

--- script.js
+++ script.js
@@ -24,23 +24,23 @@ for (let i = 0; i < 25; i += 1) {
   pixelsId.appendChild(pixels);
 }
 
-// Requisito 7: clicar na cor 
+// Requisito 7: clicar na cor
+function alvoCor(event) {
+  removeSelected();
+  event.target.classList.add('selected')
+}
+
 const clicaNaCor = document.querySelectorAll('.color');
 for (let i = 0; i < clicaNaCor.length; i += 1) {
   clicaNaCor[i].addEventListener('click', alvoCor);
-}
+};
 
 const selecionarCor = document.getElementsByClassName('color');
 
 function removeSelected() {
   for (let i = 0; i < selecionarCor.length; i += 1) {
     selecionarCor[i].classList.remove('selected')
-  }
-}
-
-function alvoCor(event) {
-  removeSelected();
-  event.target.classList.add('selected')
+  };
 }
 
 // Requisito 8 : colorir pixels
@@ -52,7 +52,20 @@ for (let i = 0; i < corPixel.length; i += 1) {
 
 function pintarCor(event) {
   const boxPixel = event.target;
-  const pegarPixel = document.querySelector('.selected')
-  const estilo = getComputedStyle(pegarPixel)
+  const pegarPixel = document.querySelector('.selected');
+  const estilo = getComputedStyle(pegarPixel);
   boxPixel.style.backgroundColor = estilo.backgroundColor;
 }
+
+// Requisito 9: botao que deixa todos os pixels branco 
+
+const botaoLimpar = document.querySelector('#clear-board');
+botaoLimpar.innerText = 'Limpar';
+
+botaoLimpar.addEventListener('click', function () {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+});
+
