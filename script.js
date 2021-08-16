f8f072df20736920f0f535eb73672e5fc6b04079
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-08-16 20:09:05 -0300

    adiciona cores à paleta de cores

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const pixelBoard = document.querySelector('#pixel-board');
 for (let i = 1; i <= 25; i += 1) {
   const pixels = document.createElement('div');
   pixels.classList = 'pixel';
+  pixels.style.backgroundColor = 'white';
   pixelBoard.appendChild(pixels);
 }
