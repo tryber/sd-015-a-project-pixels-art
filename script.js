a73fc14b15b087c7c17758fb2d7a96c257e8ecc3
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-08-16 19:14:30 -0300

    refatora layout usando flexbox

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hey')
+const pixelBoard = document.querySelector('#pixel-board');
+
+for (let i = 1; i <= 25; i += 1) {
+  const pixels = document.createElement('div');
+  pixels.classList = 'pixel';
+  pixelBoard.appendChild(pixels);
+}
