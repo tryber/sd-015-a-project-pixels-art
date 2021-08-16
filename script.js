95707d6c9a8e9237b2f10249c88a029e4114da93
fabiana <fabianamfranco@outlook.com>
2021-08-16 11:06:00 -0300

    pequenas correções

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let todasCores = document.getElementsByClassName('color');
-for (index = 0; index < todasCores.length; index += 1) {
-  todasCores[index].addEventListener('click', selecionadorCor)
-}
-function selecionadorCor(event) {
-  for (index = 0; index < todasCores.length; index += 1) {
+const todasCores = document.getElementsByClassName('color');
+function selecionadorCor() {
+  for (let index = 0; index < todasCores.length; index += 1) {
     todasCores[index].classList.remove('selected');
   }
   event.target.className = 'color selected';
 }
+for (let index = 0; index < todasCores.length; index += 1) {
+  todasCores[index].addEventListener('click', selecionadorCor);
+}
 let cadaQuadrado = document.getElementsByClassName('pixel')
-for (index = 0; index < cadaQuadrado.length; index += 1) {
-  cadaQuadrado[index].addEventListener('click', pintando)
+function pintando() {
+  const selecionado = document.getElementsByClassName('selected')[0];
+  event.target.style.backgroundColor = selecionado.id;
 }
-function pintando(event) {
-  let selecionado = document.getElementsByClassName('selected')[0];
-  event.target.style.backgroundColor = selecionado.id
+for (let index = 0; index < cadaQuadrado.length; index += 1) {
+  cadaQuadrado[index].addEventListener('click', pintando);
 }
-let botaoLimpar = document.getElementById('clear-board')
-botaoLimpar.addEventListener('click', branco)
+const botaoLimpar = document.getElementById('clear-board');
 function branco() {
-  for (index = 0; index < cadaQuadrado.length; index += 1) {
-    cadaQuadrado[index].style.backgroundColor = 'white'
+  for (let index = 0; index < cadaQuadrado.length; index += 1) {
+    cadaQuadrado[index].style.backgroundColor = 'white';
   }
 }
+botaoLimpar.addEventListener('click', branco);
