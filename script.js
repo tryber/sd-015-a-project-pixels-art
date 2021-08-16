01838f47f8c5dd649bc525976fed2850b4a6bc50
herzogcassia <cassiahherzog@gmail.com>
2021-08-16 14:07:23 -0300

    requisito 7 parcial

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-let color;
+
 function getColor(event) {
-  color = event.target.id;
+  let array = document.getElementsByClassName("color");
+  for (let index = 0; index < array.length; index +=1){
+    console.log(array[index]);
+      
+      array[index].classList.remove('selected')
+    
+  }
+  event.target.classList.add("selected")
 }
-
+ //const color = 0;
 function reciveColor(event) {
-  event.target.style.backgroundColor = color;
+  let styleColor = document.getElementsByClassName("selected")
+  const cor = window.getComputedStyle(styleColor).getPropertyValue("background-color");
+  event.target.style.backgroundColor = cor;
 }
 
-function Apagarcor() {
-  document.getElementById("button").style.backgroundColor = "white";
-}
+// function clearColorButton() {
+  // document.getElementById("button").style.backgroundColor = "white";
+// }
+
+// const clickColor = document.getElementById("color-palette");
+
+// const blackColor = document.getElementById("black")
+
+// function removeClassSelected(event){
+ 
+//   const colorSelected = document.getElementsByClassName("color")
+//    colorSelected.classList.remove('selected')
+//     event.target.classList.add("selected")
+
+
+// }
+// let arrayColor = document.getElementsByClassName("color");
+//  for(let index = 0; index <= arrayColor; index =+1) {
+//   arrayColor[index].addEventListener('click', removeClassSelected)
+
+// }
