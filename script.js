bf9c5b51498f65b55ff3bef54f10bbd5990a74de
fabiana <fabianamfranco@outlook.com>
2021-08-16 10:23:15 -0300

    requisitos 5 e 7

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function corSelecionada(){
-  let quadrado = document.getElementsByClassName('color')
-
+let todasCores = document.getElementsByClassName('color');
+for (index = 0; index < todasCores.length; index += 1) {
+  todasCores[index].addEventListener('click', selecionadorCor)
+}
+function selecionadorCor(event) {
+  for (index = 0; index < todasCores.length; index += 1) {
+    todasCores[index].classList.remove('selected');
+  }
+  event.target.className = 'color selected';
 }
