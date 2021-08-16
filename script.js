c2584a18620f973775484b14b6b86919b06d53e1
renatozr <renatozr07@gmail.com>
2021-08-16 12:24:46 -0300

    requisitos obrigatórios feitos

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
-const pixelBoard = document.getElementById('pixel-board');
-const generateBoard = document.getElementById('generate-board');
-const valueSize = document.getElementById('board-size').value;
-const pixelSize = '40px';
-const boardSize = 5;
-const colorPalette = document.getElementById('color-palette');
-const colorsOfPalette = colorPalette.children;
-const clearBoard = document.getElementById('clear-board');
+// requisito 4
+const lines = document.getElementsByClassName('line');
+
+function addPixelsToLines() {
+  for (let i = 0; i < lines.length; i += 1) {
+    for (let i2 = 0; i2 < lines.length; i2 += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      lines[i].appendChild(pixel);
+    }
+  }
+}
+addPixelsToLines();
+
+// requisito 6
+window.onload = function initPage() {
+  const paletteBlack = document.getElementById('black');
+  paletteBlack.className += ' selected';
+};
+
+// requisito 7
+const paletteColors = document.getElementsByClassName('color');
+
+function selectColor(event) {
+  document.querySelector('.selected').className = 'color';
+  const clickedColor = event.target;
+  clickedColor.className += ' selected';
+}
+function addClickPalette() {
+  for (let i = 0; i < paletteColors.length; i += 1) {
+    paletteColors[i].addEventListener('click', selectColor);
+  }
+}
+addClickPalette();
+
+// requisito 8
 const pixels = document.getElementsByClassName('pixel');
 
 function paintPixel(event) {
-  const clickPixel = event.target;
-  const selecColor = document.querySelector('.selected');
-  const paintColor = window.getComputedStyle(selecColor, null).getPropertyValue('background-color');
-  clickPixel.style.backgroundColor = paintColor;
+  const selectedColor = document.querySelector('.selected');
+  const color = window.getComputedStyle(selectedColor, null).getPropertyValue('background-color');
+
+  const clickedPixel = event.target;
+
+  clickedPixel.style.backgroundColor = color;
 }
-function createPixels() {
-  const pixel = document.createElement('div');
-  pixel.className = 'pixel';
-  pixel.style.width = pixelSize;
-  pixel.style.height = pixelSize;
-  pixel.addEventListener('click', paintPixel);
-  return pixel;
-}
-function addPixelsToLine(numberOfPixels, line) {
-  for (let i = 0; i < numberOfPixels; i += 1) {
-    line.appendChild(createPixels());
+function addClickPixels() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', paintPixel);
   }
 }
-function createBoard(numberOfLines) {
-  for (let i = 0; i < numberOfLines; i += 1) {
-    const line = document.createElement('div');
-    line.className = 'line';
-    line.style.height = pixelSize;
-    addPixelsToLine(numberOfLines, line);
-    pixelBoard.appendChild(line);
-  }
-}
-function addClassSelected(event) {
-  document.querySelector('.selected').className = 'color';
-  const color = event.target;
-  color.className += ' selected';
-}
-function clickPalette() {
-  for (let i = 0; i < colorsOfPalette.length; i += 1) {
-    colorsOfPalette[i].addEventListener('click', addClassSelected);
-  }
-}
-function cleanBoard() {
+addClickPixels();
+
+// requisito 9
+const clearButton = document.getElementById('clear-board');
+
+function clearTheBoard() {
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
   }
 }
-function changeSizeBoard() {
-  createBoard(valueSize);
+function addClickClearBtn() {
+  clearButton.addEventListener('click', clearTheBoard);
 }
-
-function windowInit() {
-  createBoard(boardSize);
-  clickPalette();
-  clearBoard.addEventListener('click', cleanBoard);
-  generateBoard.addEventListener('click', changeSizeBoard);
-}
-window.onload = windowInit;
+addClickClearBtn();
