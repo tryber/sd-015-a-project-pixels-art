9a007a20379f618366c5ffc082668402c1cd8a9d
Arthur Lisboa <lisboaarthur@hotmail.com>
2021-08-16 14:06:46 -0300

    adiciona o requisito 8

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function criaCoresPaleta() {
     const cores = coresCor[index];
     const coresItem = document.createElement('div');
     coresItem.className = 'color';
-    coresItem.style.background = cores;
+    coresItem.style.backgroundColor = cores;
     coreslista.appendChild(coresItem);
   }
 }
@@ -31,12 +31,27 @@ function selecionaCorPreencher(event) {
 window.onload = function colorClassSelected() {
   const selecionaColor = document.querySelector('.color');
   selecionaColor.className = 'selected color';
-  const coresSeleção = document.querySelectorAll('.color')[0];
-  const coresSeleção2 = document.querySelectorAll('.color')[1];
-  const coresSeleção3 = document.querySelectorAll('.color')[2];
-  const coresSeleção4 = document.querySelectorAll('.color')[3];
-  coresSeleção.addEventListener('click', selecionaCorPreencher);
-  coresSeleção2.addEventListener('click', selecionaCorPreencher);
-  coresSeleção3.addEventListener('click', selecionaCorPreencher);
-  coresSeleção4.addEventListener('click', selecionaCorPreencher);
 };
+
+const coresSeleção = document.querySelectorAll('.color')[0];
+const coresSeleção2 = document.querySelectorAll('.color')[1];
+const coresSeleção3 = document.querySelectorAll('.color')[2];
+const coresSeleção4 = document.querySelectorAll('.color')[3];
+coresSeleção.addEventListener('click', selecionaCorPreencher);
+coresSeleção2.addEventListener('click', selecionaCorPreencher);
+coresSeleção3.addEventListener('click', selecionaCorPreencher);
+coresSeleção4.addEventListener('click', selecionaCorPreencher);
+
+function pintaCor(event) {
+  const corSelecionada = document.querySelector('.selected');
+  const valorCor = window.getComputedStyle(corSelecionada);
+  event.target.style.backgroundColor = valorCor.getPropertyValue('background-color');
+}
+
+function pinturaPixel() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    const pixelsIndex = pixels[index];
+    pixelsIndex.addEventListener('click', pintaCor);
+  }
+} pinturaPixel();
