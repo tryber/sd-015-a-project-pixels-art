44784c85cbaaace53f8ad05649702b6977b5d75a
João Cassanji <joaocassanji@gmail.com>
2021-08-16 14:43:54 -0300

    novo envio

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
-window.onload = function() { // https://developer.mozilla.org/pt-BR/docs/Web/API/GlobalEventHandlers/onload
-    let colorNumbers = document.getElementsByClassName('color').length
-    
-    let pixelBoardConfig = {
-        colums: 5,
-        lines: 5
+let pixelBoardConfig = {
+    colums: 5,
+    lines: 5
+}
+
+function reloadPage() {
+
+let colorNumbers = document.getElementsByClassName('color').length
+
+let pixelBoard = document.querySelector('#pixel-board')
+
+document.getElementById("selected-color-box").style.background = 'black';
+
+document.getElementById('black').classList.add('selected'); // https://stackoverflow.com/questions/507138/how-to-add-a-class-to-a-given-element
+
+for (let i = 0; i < colorNumbers; i++) {
+document.getElementsByClassName('color')[i].addEventListener('click', selectColor);
+}
+
+for (let i = 0; i < pixelBoardConfig.lines; i += 1) {
+    let newLine= document.createElement('div')
+    let lineId = 'line' + (i+1).toString()
+    pixelBoard.appendChild(newLine).className = 'line'
+    document.querySelectorAll('.line')[i].id = lineId
+
+    for (let n = 0; n < pixelBoardConfig.colums; n += 1) {
+        let newColum = document.createElement('div')
+        let columId = 'pixel-board-c' + (n+1).toString()
+        document.querySelectorAll('.line')[i].appendChild(newColum).className = 'pixel'
     }
 
-    let pixelBoard = document.querySelector('#pixel-board')
+}
 
-    document.getElementById("selected-color-box").style.background = 'black';
+for (let i = 0; i < (pixelBoardConfig.lines * pixelBoardConfig.colums); i += 1) {
+    document.querySelectorAll('.pixel')[i].addEventListener('click', paintPixel);
+   // document.querySelectorAll('.pixel')[i].addEventListener('click', selectColor);
+} }
 
-    document.getElementById('black').classList.add('selected'); // https://stackoverflow.com/questions/507138/how-to-add-a-class-to-a-given-element
-
-    for (let i = 0; i < colorNumbers; i++) {
-    document.getElementsByClassName('color')[i].addEventListener('click', selectColor);
-    }
-
-
-
-
-
-    for (let i = 0; i < pixelBoardConfig.lines; i += 1) {
-        let newLine= document.createElement('div')
-        let lineId = 'line' + (i+1).toString()
-        pixelBoard.appendChild(newLine).className = 'line'
-        document.querySelectorAll('.line')[i].id = lineId
-
-        for (let n = 0; n < pixelBoardConfig.colums; n += 1) {
-            let newColum = document.createElement('div')
-            let columId = 'pixel-board-c' + (n+1).toString()
-            document.querySelectorAll('.line')[i].appendChild(newColum).className = 'pixel'
-        }
-    
-    }
-
-    for (let i = 0; i < (pixelBoardConfig.lines * pixelBoardConfig.colums); i += 1) {
-        document.querySelectorAll('.pixel')[i].addEventListener('click', paintPixel);
-       // document.querySelectorAll('.pixel')[i].addEventListener('click', selectColor);
-    }
-
-
-
-
+window.onload = function() {
+    reloadPage()
 }
 
 
@@ -59,5 +55,34 @@ function paintPixel(color) {
 function clearBoard() {
     for (let i = 0; i < (document.querySelectorAll('.pixel').length); i += 1) {
         document.querySelectorAll('.pixel')[i].style.background = 'white';
+    }
+}
+
+function resizeBoard() {
+    if ( document.getElementById('board-size').value < 1 ) {
+        alert('Board inválido!')
+    }
+    else if ( document.getElementById('board-size').value < 5 ) {
+        document.querySelectorAll('.pixel').forEach(function(a){ //https://stackoverflow.com/questions/44984867/javascript-remove-elements-by-class-name/44984940
+        a.remove()})
+        pixelBoardConfig.colums = 5
+        pixelBoardConfig.lines = 5
+        reloadPage()
+    }
+
+    else if ( document.getElementById('board-size').value > 50 ) {
+        document.querySelectorAll('.pixel').forEach(function(a){ //https://stackoverflow.com/questions/44984867/javascript-remove-elements-by-class-name/44984940
+        a.remove()})
+        pixelBoardConfig.colums = 50
+        pixelBoardConfig.lines = 50
+        reloadPage()
+    }
+
+    else {
+        document.querySelectorAll('.pixel').forEach(function(a){ //https://stackoverflow.com/questions/44984867/javascript-remove-elements-by-class-name/44984940
+        a.remove()})
+        pixelBoardConfig.colums = document.getElementById('board-size').value
+        pixelBoardConfig.lines = document.getElementById('board-size').value
+        reloadPage()
     }
 }
