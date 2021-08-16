a75cc5315762675b7d7c5a75a3d6f1e48db0b6cd
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-08-16 14:56:47 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
 
 window.onload = function(){
-  let color = document.querySelectorAll('.color')
-  let initialColor = color[0].classList.add('selected')
-  let paletteColors = ['black', 'red', 'green', 'blue'];
+  let colorClass = document.querySelectorAll('.color')
+  let arrayColorsPalletes = ['black', 'red', 'green', 'blue'];
+  colorClass[0].classList.add('selected')
 
   function createPaletteColors() {
-    for (let i = 0; i <  4; i += 1) {
-    color[i].style.backgroundColor = paletteColors[i];
+    for (let i = 0; i <  arrayColorsPalletes.length; i += 1) {
+    colorClass[i].style.backgroundColor = arrayColorsPalletes[i];
    }
-}
+  }
 createPaletteColors();
-const section = document.querySelector('section')
+
+
 
 function createBoard() {
-  let boardPixel = document.createElement('section');
-  boardPixel.id = 'pixel-board';
-  document.body.appendChild(boardPixel);
+  let pixelBoardSection = document.createElement('section');
+  pixelBoardSection.id = 'pixel-board';
+  document.body.appendChild(pixelBoardSection);
 
+  let lines = 5;
+  let column = 5;
 
-  for (i = 0 ; i < 5; i += 1) {
-      let line = document.createElement('div');
-      boardPixel.appendChild(line);
-      for (x = 0 ; x < 5; x += 1){
-        let pixelBlock = document.createElement('div');
-        pixelBlock.className = 'pixel';
-        pixelBlock.style.backgroundColor = 'white';
-        line.appendChild(pixelBlock);
+  for (i = 0 ; i < lines; i += 1) {
+    let line = document.createElement('div');
+    pixelBoardSection.appendChild(line);
+    for (x = 0 ; x < column; x += 1){
+      let pixelBlockDiv = document.createElement('div');
+      pixelBlockDiv.className = 'pixel';
+      pixelBlockDiv.style.backgroundColor = 'white';
+      line.appendChild(pixelBlockDiv);
       }
-  }
-}
-createBoard();
+    }
+  }createBoard();
+
+function selectColorClick(){
+      for (let x = 0; x < colorClass.length; x += 1){
+      colorClass[x].addEventListener('click', selectedColorClass);
+      }
+    }
+function selectedColorClass(event) {
+  let clicked = event.target
+  let selectedClass = document.querySelector('.selected');
+    selectedClass.classList.remove('selected');
+    clicked.classList.add('selected');
+    
+}selectColorClick();
 
 
-// let colorBlack = paletteColor[0]
-// colorBlack.className = 'selected'
+function selectedColorPixelClick(){
+  let pixel = document.querySelectorAll('.pixel')
+  for(let x = 0; x < pixel.length; x += 1){
+    pixel[x].addEventListener('click', changeBackgroundPixel)
+  }}
+
+function changeBackgroundPixel(event){
+  let selectedClass2 = document.querySelector('.selected').style.backgroundColor;
+  let clicked2 = event.target
+  clicked2.style.backgroundColor = selectedClass2
+ 
 }
+selectedColorPixelClick()
+}
+
