59fa4ee59fb0cced2b9263e5891e0123956c454f
ZekaEu <zeka.surf@hotmail.com>
2021-08-17 18:14:06 -0300

    corrige requisito 3

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-document.getElementById('black').style.backgroundColor = 'black';
-
 const color = document.getElementsByClassName('color');
 const pixel = document.getElementsByClassName('pixel');
 const clearB = document.getElementById('clear-board');
 const boardSize = document.getElementById('board-size');
 const genBoard = document.getElementById('generate-board');
 const pixelBoard = document.getElementById('pixel-board');
+
+function randomColor() {
+  const genColor = '#' + Math.floor(Math.random()*16777216).toString(16).slice(-6);
+  return genColor;
+} // Copyright: function made using sugestions and ideas from the content and comments posted at this link: https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
+
+document.getElementById('black').style.backgroundColor = 'black';
+document.getElementById('color1').style.backgroundColor = randomColor();
+document.getElementById('color2').style.backgroundColor = randomColor();
+document.getElementById('color3').style.backgroundColor = randomColor();
 
 function selectColor() {
   for (let i = 0; i < color.length; i += 1) {
@@ -40,15 +48,6 @@ function clearBoard() {
 }
 
 clearB.addEventListener('click', clearBoard);
-
-function randomColor() {
-  const genColor = '#' + Math.floor(Math.random()*16777216).toString(16).slice(-6);
-  return genColor;
-} // Copyright: function made using sugestions and ideas from the content and comments posted at this link: https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
-
-document.getElementById('color1').style.backgroundColor = randomColor();
-document.getElementById('color2').style.backgroundColor = randomColor();
-document.getElementById('color3').style.backgroundColor = randomColor();
 
 function entireBoard(wxh) {
   while (pixelBoard.firstChild) {
