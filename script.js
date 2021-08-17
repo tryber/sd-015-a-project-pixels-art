511052419b14eb5f42fb93c6fbec06b9f42a291c
Thiago Lordello <thiagolordello@hotmail.com>
2021-08-17 08:28:46 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -10,3 +10,45 @@ function corDinamPaleta() {
 
 }
 corDinamPaleta();
+
+function criaPixels() {
+    let getBody = document.querySelector('body');
+    let divPaiPixels = document.createElement('div');
+    divPaiPixels.className = 'estiloPixelPai';
+    divPaiPixels.id = 'pixel-board';
+    divPaiPixels.innerHTML;
+    getBody.appendChild(divPaiPixels);
+
+    for (let i = 0; i < 5; i += 1) {
+        let getDivPai = document.querySelector('#pixel-board');
+        let linha = document.createElement('div');
+        linha.innerHTML;
+        getDivPai.appendChild(linha);
+
+        for (let j = 0; j < 5; j += 1) {
+            let getLinha = document.getElementsByClassName('pixel');
+            let blocoDe5 = document.createElement('div');
+            blocoDe5.className = 'pixel';
+            blocoDe5.innerHTML;
+            linha.appendChild(blocoDe5);
+        }
+
+    }
+
+}
+criaPixels();
+
+
+
+/*window.onload = function() {
+    let corSelecionada = document.querySelector('#divBlack');
+    corSelecionada.className = 'selected';
+}*/
+
+function selected() {
+
+}
+
+function selecionaCor() {
+
+}
