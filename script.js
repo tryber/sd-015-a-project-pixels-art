dbfc825db49425d8a40dba46da5c3323fa47b810
rafaelmedeiross <rafael.medeiros@msn.com>
2021-08-17 20:13:21 -0300

    Requisitos 10 e 11: tamanho do board definido pelo user, com tam. max e min

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
-const linesPixel = document.querySelectorAll('.line');
+const boardPixel = document.querySelector('#pixel-board');
 
 function trocaCor(event) {
-  let elementoCor = document.querySelector('.selected');
+  const elementoCor = document.querySelector('.selected');
   let corSelecionada = window
     .getComputedStyle(elementoCor, null)
     .getPropertyValue('background-color');
   event.target.style.backgroundColor = corSelecionada;
 }
 
-for (let i = 0; i < 5; i += 1) {
-  let divLinha = linesPixel[i];
-  for (let k = 1; k <= 5; k += 1) {
-    let divPixel = document.createElement('div');
-    divPixel.className = 'pixel';
-    divLinha.appendChild(divPixel);
-    divPixel.addEventListener('click', trocaCor);
+const botaoVQV = document.getElementById('generate-board');
+botaoVQV.addEventListener('click', subCriaBoard);
+const inputVQV = document.getElementById('board-size');
+
+function subCriaBoard() {
+  if (inputVQV.value === '') {
+    return alert('Board inválido!');
+  } else {
+    criaBoard();
   }
 }
+function criaBoard() {
+  inputVQV.type = 'number';
+  inputVQV.min = '1';
+  if (document.querySelector('.line') !== null) {
+    let linhas = document.querySelectorAll('.line');
+    for (let k of linhas) {
+      linhaRemovida = k;
+      boardPixel.removeChild(linhaRemovida);
+    }
+  }
+  inputVQV.min;
+
+  let tamanhoVQV = inputVQV.value;
+  if (tamanhoVQV === '' || (tamanhoVQV < 5 && tamanhoVQV > 0)) {
+    tamanhoVQV = 5;
+  }
+  if (tamanhoVQV > 50) {
+    tamanhoVQV = 50;
+  }
+
+  for (let i = 0; i < tamanhoVQV; i += 1) {
+    let linesPixel = document.createElement('div');
+    linesPixel.className = 'line';
+    boardPixel.appendChild(linesPixel);
+    for (let k = 1; k <= tamanhoVQV; k += 1) {
+      let divPixel = document.createElement('div');
+      divPixel.className = 'pixel';
+      linesPixel.appendChild(divPixel);
+      divPixel.addEventListener('click', trocaCor);
+    }
+  }
+}
+criaBoard();
 
 function selecionaCor(event) {
   document.querySelector('.selected').classList.remove('selected');
