8328606b5e01be59e6624af3d38a81a6ffe12146
djsilva <coxcorp@gmail.com>
2021-08-17 16:35:16 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 let numberOfBlocks = 5;
 
-for (let i = 0, i < numberOfBlocks, i += 1) {
-    for (let j = 0, j < numberOfBlocks, j += 1) {
-
-    }
-}
-
 // Cria uma caixa com base nas diferentes classes
 function createBlock(className) {
     let block = document.createElement("div");
@@ -20,3 +14,5 @@ function createBlock(className) {
         divs.appendChild(block);
     }
   }
+
+  
