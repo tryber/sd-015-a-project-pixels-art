6e2005870c7f45bf49fdc0563199eaf3ebc3d850
Luan Vitor <luan_vittor@hotmail.com>
2021-08-16 22:30:48 -0300

    lint corrigido

--- script.js
+++ script.js
@@ -1,75 +1,64 @@
-// requisito 6 // 
+//  requisito 6   //
 
+const clear = document.getElementById('clear-board');
+const tdColor = document.querySelectorAll('.color');
 const corInicial = document.querySelector('.color');
 corInicial.classList += ' selected';
+const box = document.getElementById('board-size');
+const botao = document.getElementById('generate-board');
+const pixelBoard = document.getElementById('pixel-board');
 
-//requisito 7 //
+//  requisito 7   //
 
-const tdColor = document.querySelectorAll('.color');
-for (const element of tdColor) {
-  element.addEventListener('click', changeSelected);
-}
-function changeSelected(event) {
-  const removeClass = document.querySelector('.selected')
+function changeSelected(e) {
+  const event = e;
+  const removeClass = document.querySelector('.selected');
   removeClass.classList.remove('selected');
   event.target.className = 'color selected';
 }
 
-//requisito 8 //
-
-function changeColor2() {
-  let paint = document.querySelectorAll('.pixel');
-  for (const element of paint) {
-    element.addEventListener('click', changeColor);
-  }
+for (let i = 0; i < tdColor.length; i += 1) {
+  tdColor[i].addEventListener('click', changeSelected);
 }
-changeColor2()
 
-function changeColor(event) {
+//  requisito 8  //
+
+function changeColor(e) {
+  const event = e;
   const colorSelected = document.getElementsByClassName('selected')[0];
   const colorPaint = getComputedStyle(colorSelected);
   event.target.style.backgroundColor = colorPaint.backgroundColor;
 }
 
-//requisito 9 //
+function changeColor2() {
+  const paint = document.querySelectorAll('.pixel');
+  for (let i = 0; i < paint.length; i += 1) {
+    paint[i].addEventListener('click', changeColor);
+  }
+}
+changeColor2();
 
-const clear = document.getElementById('clear-board');
-clear.addEventListener('click', limparQuadro);
+//  requisito 9  //
 
 function limparQuadro() {
-  let paint = document.querySelectorAll('.pixel');
-  for (element of paint) {
-    element.style.backgroundColor = 'white';
+  const paint = document.querySelectorAll('.pixel');
+  for (let i = 0; i < paint.length; i += 1) {
+    paint[i].style.backgroundColor = 'white';
   }
 }
 
-// requisito10 e 11//
+clear.addEventListener('click', limparQuadro);
 
-let box = document.getElementById('board-size');
-let botao = document.getElementById('generate-board');
-let pixelBoard = document.getElementById('pixel-board');
-botao.addEventListener('click', callFunction);
-
-function callFunction() {
-  if (box.value === '') {
-    alert('Board inválido!')
-  } else if (box.value < 5) {
-    funcaoLessFive();
-  } else if (box.value > 50) {
-    funcaoMoreFifity();
-  } else {
-    valorValido();
-  }
-}
+//   requisito10 e 11 //
 
 function funcaoLessFive() {
   pixelBoard.innerHTML = '';
   for (let linha = 1; linha <= 5; linha += 1) {
-    let tr = document.createElement('tr');
+    const tr = document.createElement('tr');
     pixelBoard.appendChild(tr);
     for (let coluna = 0; coluna < 5; coluna += 1) {
-      let td = document.createElement('td');
-      td.className = 'pixel'
+      const td = document.createElement('td');
+      td.className = 'pixel';
       tr.appendChild(td);
       td.addEventListener('click', changeColor2);
     }
@@ -78,9 +67,9 @@ function funcaoLessFive() {
 
 function valorValido() {
   pixelBoard.innerHTML = '';
-  for (let linha = 1; linha <= parseInt(box.value); linha += 1) {
+  for (let linha = 1; linha <= parseInt(box.value, 10); linha += 1) {
     const tr = document.createElement('tr');
-    console.log(tr)
+    console.log(tr);
     pixelBoard.appendChild(tr);
     for (let coluna = 0; coluna < box.value; coluna += 1) {
       const td = document.createElement('td');
@@ -105,19 +94,32 @@ function funcaoMoreFifity() {
   }
 }
 
-// requisito 13 //
-
-function changeRandomColor() {
-  for (let i = 1; i < tdColor.length; i += 1) {
-    tdColor[i].style.backgroundColor = randomColors();
+function callFunction() {
+  if (box.value === '') {
+    alert('Board inválido!');
+  } else if (box.value < 5) {
+    funcaoLessFive();
+  } else if (box.value > 50) {
+    funcaoMoreFifity();
+  } else {
+    valorValido();
   }
 }
 
+botao.addEventListener('click', callFunction);
+
+//   requisito 13   //
+
 function randomColors() {
-  let blue = Math.floor(Math.random() * 255);
-  let red = Math.floor(Math.random() * 255);
-  let green = Math.floor(Math.random() * 255);
-  let random = `rgb(${blue}, ${red}, ${green})`;
+  const blue = Math.floor(Math.random() * 255);
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const random = `rgb(${blue}, ${red}, ${green})`;
   return (random);
 }
-window.onload = changeRandomColor();
+
+window.onload = function changeRandomColor() {
+  for (let i = 1; i < tdColor.length; i += 1) {
+    tdColor[i].style.backgroundColor = randomColors();
+  }
+};
