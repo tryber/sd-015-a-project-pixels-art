b5c117458faf31bbfb165e795812aa88ff42604e
biizerrah <biizerrah@outlook.com>
2021-08-18 11:04:09 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 
-let listOfColors = document.querySelectorAll('.color');
+
+  const listOfColors = document.querySelectorAll('.color');
 for(let index=0;index<listOfColors.length;index+=1){
   listOfColors[index].addEventListener('click', function(event){
   
     if(!event.target.classList.contains('selected')){
       let colorSelected = document.querySelector('.selected')
-      colorSelected.classList.remove('selected')
       event.target.classList.add('selected')
+      colorSelected.classList.remove('selected')
    }
 }
   )}
+    
+  window.onload = function(){ 
+    const pixels = document.querySelectorAll('.pixel');
+    for(let index=0;index<pixels.length;index+=1){
+    pixels[index].addEventListener('click', setColor);
 
+    function setColor (event){
+    const getColor = getComputedStyle(document.querySelector('.selected')).backgroundColor;
+    event.target.style.backgroundColor = getColor
+    }
+    } 
+  }
 
 
  
