1a00b2c6eacba3f68d51d37498cf78849fd6ace7
douglas oliveira <douglasgama0@gmail.com>
2021-08-17 22:27:05 -0300

    Finalizaçao do requisito 7 do projeto

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const black = document.querySelector('.color');
 black.className = 'color selected';
 black.style.backgroundColor = '#000000';
+let allColors = document.querySelectorAll('.color');
+const paletteColor = document.querySelector('#color-palette');
 
 function palletColor() {
   const color = document.querySelectorAll('.color');
@@ -17,10 +19,46 @@ function palletColor() {
 palletColor();
 
 const boxPixel = document.getElementById('pixel-board');
-
-for (let i = 1; i <= 25; i += 1) {
-  const elementBox = document.createElement('li');
-  elementBox.style.backgroundColor = 'white';
-  elementBox.classList = 'pixel';
-  boxPixel.appendChild(elementBox);
+function creatPixelBox() {
+  for (let i = 1; i <= 25; i += 1) {
+    const elementBox = document.createElement('li');
+    elementBox.style.backgroundColor = 'white';
+    elementBox.classList = 'pixel';
+    boxPixel.appendChild(elementBox);
+  }
 }
+creatPixelBox();
+
+function selectedColor() {
+  for (let index = 0; index < allColors.length; index += 1) {
+    allColors[index].addEventListener('click', function eventClick() {
+      if (allColors[index].className === 'color selected') {
+        allColors[index].className = 'color';
+      } else if (allColors[index].className === 'color') {
+        allColors[index].className = 'color selected';
+      }
+    });
+  }
+}
+selectedColor();
+
+//7
+//Codigo utilizado da PR de Emily Nilsen Carvalho - Turma 15 Tribo A
+function alvoCor(event) {
+  removeSelected();
+  event.target.classList.add('selected');
+}
+
+const clicaNaCor = document.querySelectorAll('.color');
+for (let i = 0; i < clicaNaCor.length; i += 1) {
+  clicaNaCor[i].addEventListener('click', alvoCor);
+}
+
+const selecionarCor = document.getElementsByClassName('color');
+
+function removeSelected() {
+  for (let i = 0; i < selecionarCor.length; i += 1) {
+    selecionarCor[i].classList.remove('selected');
+  }
+}
+//Codigo utilizado da PR de Emily Nilsen Carvalho - Turma 15 Tribo A
