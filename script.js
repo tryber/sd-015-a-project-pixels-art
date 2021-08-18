8cbd857235d76a5241f4476567276d6acf99b672
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-18 10:28:56 -0300

    finish styles

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function blockMaker(sideSize) {
   let board = document.getElementById('pixel-board');
   for (index = 1; index <= sideSize; index += 1) {
     let line = document.createElement('div');
+    line.style.height = '42px';
     for (index_2 = 1; index_2 <= sideSize; index_2 += 1) {
       let block = document.createElement('div');
       block.className = 'pixel';
@@ -42,14 +43,14 @@ getColor();
 //PAINT THE BOARD
 
 function painter() {
-  let pixelBoard = document.getElementsByClassName('pixel');
-  for (pixel of pixelBoard) {
-    pixel.addEventListener('click', function(event) {
-      let selectedColor = document.getElementsByClassName('color selected')[0];
-      let color = selectedColor.style.backgroundColor;
-      event.target.style.backgroundColor = selectedColor.innerHTML;
-    })
-  }
+  let pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.addEventListener('click', function(event) {
+      if (event.target.className === 'pixel') {
+        let selectedColor = document.getElementsByClassName('color selected')[0];
+        event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+      }
+    // }
+  })
 }
 
 painter();
@@ -67,3 +68,46 @@ function clear() {
 }
 
 clear();
+
+// INPUT TO CHOOSE A BOARD SIZE
+
+function chooseTheSize() {
+  const pixelsBoard = document.getElementById('pixel-board');
+  if (pixelsBoard.children.length > 0) {
+    for (let index = pixelsBoard.children.length - 1; index >= 0; index -= 1){
+      pixelsBoard.removeChild(pixelsBoard.lastChild);
+    }
+  }
+  const chosenSize = document.getElementById('board-size').value;
+  if (chosenSize === '') {
+    window.alert('Board inválido!');
+  } else if (chosenSize < 5) {
+    blockMaker(5);
+  } else if (chosenSize > 50) {
+  blockMaker(50);
+  } else {
+    blockMaker(chosenSize);
+  }
+}
+
+function chooseTheSizeButton() {
+  const sizeButton = document.getElementById('generate-board');
+  sizeButton.addEventListener('click', chooseTheSize);
+}
+
+chooseTheSizeButton();
+
+// RANDOM COLORS
+
+function ramdomColors() {
+  const color1 = document.getElementById('c1');
+  color1.style.backgroundColor = 'black';
+  const color2 = document.getElementById('c2');
+  color2.style.backgroundColor = 'rgb(' + (Math.random() * 255) + ', ' + (Math.random() * 255) +', ' + (Math.random() * 255) + ')';
+  const color3 = document.getElementById('c3');
+  color3.style.backgroundColor = 'rgb(' + (Math.random() * 255) + ', ' + (Math.random() * 255) +', ' + (Math.random() * 255) + ')';
+  const color4 = document.getElementById('c4');
+  color4.style.backgroundColor = 'rgb(' + (Math.random() * 255) + ', ' + (Math.random() * 255) +', ' + (Math.random() * 255) + ')';
+}
+
+ramdomColors();
