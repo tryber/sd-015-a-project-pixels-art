518f242858fdb565ee8da4370e761165b66d47f2
Vitor <tutornews2@gmail.com>
2021-08-18 07:23:55 -0300

    Adiciona Requesito 7.

--- script.js
+++ script.js
@@ -10,6 +10,48 @@ element.classList.add("selected");
 return colorblack
 }
 
+//Source: https://www.mundojs.com.br/2018/01/11/diferenca-de-classname-classlist-e-setattribute/
+
+let buttonBlack = document.getElementsByClassName ('color')[0];
+let buttonRed = document.getElementsByClassName ('color')[1];
+let buttonGreen = document.getElementsByClassName ('color')[2];
+let buttonBrown = document.getElementsByClassName ('color')[3];
+
+buttonBlack.addEventListener('click', clickBlack);
+buttonRed.addEventListener('click', clickRed);
+buttonGreen.addEventListener('click', clickGreen);
+buttonBrown.addEventListener('click', clickBrown);
+
+
+function clickBlack() {
+    buttonBlack.className = 'color selected';
+    buttonRed.className = 'color';
+    buttonGreen.className = 'color';
+    buttonBrown.className = 'color';
+
+}
+function clickRed() {
+    buttonBlack.className = 'color';
+    buttonRed.className = 'color selected';
+    buttonGreen.className = 'color';
+    buttonBrown.className = 'color';
+
+}
+function clickGreen() {
+    buttonBlack.className = 'color';
+    buttonRed.className = 'color';
+    buttonGreen.className = 'color selected';
+    buttonBrown.className = 'color';
+
+}
+function clickBrown() {
+    buttonBlack.className = 'color';
+    buttonRed.className = 'color';
+    buttonGreen.className = 'color';
+    buttonBrown.className = 'color selected';
+}
+
+
 
 function createTable() {
   const pixelBoard = document.querySelector('#pixel-board');
