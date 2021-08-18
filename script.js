708a751444bb467f1c4449459b3702f7d6689437
Davio <davioplp@gmail.com>
2021-08-18 02:41:04 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function criarCoresAleatorias() { 
+function criarCoresAleatorias() {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
@@ -6,3 +6,15 @@ function criarCoresAleatorias() {
   return corCompleta;
 }
 
+for (let i = 0; i < 25; i += 1) {
+  const wh = document.createElement('cor');
+  wh.className = 'pixel';
+  document.getElementById('pixel-board').appendChild(wh);
+}
+
+window.onload = function () {
+  const quadsBrancos = document.getElementsByClassName('pixel');
+  quadsBrancos.addEventListener('click', (pintaPreto) {
+  
+  
+};
