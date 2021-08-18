79c1696d85b8c7e5e3fdd2945ebe8e965bc1f235
lipegois <luizz3.14@hotmail.com>
2021-08-18 04:03:16 -0500

    Requisito 8

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-window.onload = selectBlackColor
-
-function selectBlackColor() {
+window.onload = function selectBlackColor() {
   document.getElementsByClassName('color')[0].className += ' selected';
-}
+};
 
 const colorOne = document.getElementsByClassName('color')[0];
 const colorTwo = document.getElementsByClassName('color')[1];
 const colorTheree = document.getElementsByClassName('color')[2];
 const colorFour = document.getElementsByClassName('color')[3];
 
+document.getElementById('color-1').style.backgroundColor = 'black'; 
+document.getElementById('color-2').style.backgroundColor = '#023047'; 
+document.getElementById('color-3').style.backgroundColor = '#cb997e'; 
+document.getElementById('color-4').style.backgroundColor = '#d00000'; 
+
 function clickColorOne() {
-  colorOne.className = 'color selected';
+  colorOne.className += ' selected';
   colorTwo.className = 'color';
   colorTheree.className = 'color';
   colorFour.className = 'color';
@@ -18,7 +21,7 @@ function clickColorOne() {
 
 function clickColorTwo() {
   colorOne.className = 'color';
-  colorTwo.className = 'color selected';
+  colorTwo.className += ' selected';
   colorTheree.className = 'color';
   colorFour.className = 'color';
 }
@@ -41,3 +44,13 @@ colorOne.addEventListener('click', clickColorOne);
 colorTwo.addEventListener('click', clickColorTwo);
 colorTheree.addEventListener('click', clickColorTheree);
 colorFour.addEventListener('click', clickColorFour);
+
+const pixelFrame = document.getElementById('pixel-board');
+
+function paintPixels(event) { 
+  const selectedPixel = event.target;
+  const selectorColor = document.querySelector('.selected').style.backgroundColor;
+  selectedPixel.style.backgroundColor = selectorColor;
+}
+
+pixelFrame.addEventListener('click', paintPixels);
