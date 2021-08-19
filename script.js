445e082144821ddf8f5e9dcf07f675ff5d316658
pauloruan <batista94pr@gmail.com>
2021-08-19 13:29:35 -0300

    Requisito 7 concluido

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// REQUISITO 7
+window.onload = function iniciar() {
+  const corPreta = document.getElementById('black');
+  corPreta.classList.add('selected');
+};
+
 // REQUISITO 4
 function quadroDePixels() {
   const altura = 5;
@@ -10,3 +16,39 @@ function quadroDePixels() {
   }
 }
 quadroDePixels();
+
+// REQUISITO 7
+function selecionar() {
+  const black = document.getElementById('black');
+  const red = document.getElementById('red');
+  const green = document.getElementById('green');
+  const blue = document.getElementById('blue');
+  black.addEventListener('click', function() {
+    black.classList.add('selected')
+    red.classList.remove('selected')
+    green.classList.remove('selected')
+    blue.classList.remove('selected')
+  })
+
+  red.addEventListener('click', function() {
+    black.classList.remove('selected')
+    red.classList.add('selected')
+    green.classList.remove('selected')
+    blue.classList.remove('selected')
+  })
+
+  green.addEventListener('click', function() {
+    black.classList.remove('selected')
+    red.classList.remove('selected')
+    green.classList.add('selected')
+    blue.classList.remove('selected')
+  })
+
+  blue.addEventListener('click', function() {
+    black.classList.remove('selected')
+    red.classList.remove('selected')
+    green.classList.remove('selected')
+    blue.classList.add('selected')
+  })
+}
+selecionar();
