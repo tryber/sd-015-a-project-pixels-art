c4ada1e084e938eabf8c09c2272a62ec1735b9d0
Felipe <felllypes@hotmail.com>
2021-08-19 04:13:47 -0300

    tentanto pintar

--- script.js
+++ script.js
@@ -1,2 +1,123 @@
-/* Concultado Repositório de italosergio link:https://github.com/tryber/sd-015-a-project-pixels-art/blob/ee8bebadbe0f903ec1c4482f1ce454ad51aa9f7a/script.js */
+/* Consultado Repositório de italosergio link:https://github.com/tryber/sd-015-a-project-pixels-art/blob/ee8bebadbe0f903ec1c4482f1ce454ad51aa9f7a/script.js */
+
+
+let botãoAmarelo = document.getElementById("yellow");
+let botãoVermelho = document.getElementById("red");
+let botãoPreto = document.getElementById("black");
+let botãoVerde = document.getElementById("green");
+
+let absorverCor = document.getElementsByClassName("selected");
+
+botãoAmarelo.addEventListener("click", function( ) {absorverCor.style.backgroundColor = "yellow"}  );
+botãoPreto.addEventListener("click", function( ) {absorverCor.style.backgroundColor = "black"}  );
+botãoVermelho.addEventListener("click", function( ) {absorverCor.style.backgroundColor = "red"}  );
+botãoVerde.addEventListener("click", function( ) {absorverCor.style.backgroundColor = "green"}  );
+
+
+
+let a = document.getElementById("primeiro");
+let b = document.getElementById("segundo");
+let c = document.getElementById("terceiro");
+let d = document.getElementById("quarto");
+let e = document.getElementById("quinto");
+let f = document.getElementById("sexto");
+let g = document.getElementById("setimo");
+let h = document.getElementById("oitavo");
+let i = document.getElementById("nove");
+let j = document.getElementById("dez");
+let k = document.getElementById("onze");
+let l = document.getElementById("doze");
+let m = document.getElementById("treze");
+let n = document.getElementById("quatorze");
+let o = document.getElementById("quinze");
+let p = document.getElementById("dezesseis");
+let q = document.getElementById("dezessete");
+let r = document.getElementById("dezoito");
+let s = document.getElementById("dezenove");
+let t = document.getElementById("vinte");
+let u = document.getElementById("vinteUm");
+let v = document.getElementById("vinteDois");
+let x = document.getElementById("vinteEtres");
+let y = document.getElementById("vinteEquatro");
+let z = document.getElementById("vinteECinco");
+
+
+a.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {a.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {a.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {a.style.background = "green"} else {a.style.backgrond = "black"}});
+
+b.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {b.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {b.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {b.style.background = "green"} else {b.style.backgrond = "black"}});
+
+c.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {c.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {c.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {c.style.background = "green"} else {c.style.backgrond = "black"}});
+
+d.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {d.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {d.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {d.style.background = "green"} else {d.style.backgrond = "black"}});
+
+e.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {e.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {e.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {e.style.background = "green"} else {e.style.backgrond = "black"}});
+
+f.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {f.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {f.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {f.style.background = "green"} else {f.style.backgrond = "black"}});
+
+g.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {g.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {g.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {g.style.background = "green"} else {g.style.backgrond = "black"}});
+
+h.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {h.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {h.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {h.style.background = "green"} else {h.style.backgrond = "black"}});
+
+i.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {i.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {i.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {i.style.background = "green"} else {a.style.backgrond = "black"}});
+
+j.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {j.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {j.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {j.style.background = "green"} else {j.style.backgrond = "black"}});
+
+k.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {k.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {k.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {k.style.background = "green"} else {k.style.backgrond = "black"}});
+
+l.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {l.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {l.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {l.style.background = "green"} else {l.style.backgrond = "black"}});
+
+m.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {m.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {m.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {m.style.background = "green"} else {m.style.backgrond = "black"}});
+
+n.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {n.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {n.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {n.style.background = "green"} else {n.style.backgrond = "black"}});
+
+o.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {o.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {o.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {o.style.background = "green"} else {o.style.backgrond = "black"}});
+
+p.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {p.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {p.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {p.style.background = "green"} else {p.style.backgrond = "black"}});
+
+q.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {q.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {q.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {q.style.background = "green"} else {q.style.backgrond = "black"}});
+
+r.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {r.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {r.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {r.style.background = "green"} else {r.style.backgrond = "black"}});
+
+s.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {s.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {s.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {s.style.background = "green"} else {s.style.backgrond = "black"}});
+
+t.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {t.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {t.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {t.style.background = "green"} else {t.style.backgrond = "black"}});
+
+u.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {a.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {a.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {a.style.background = "green"} else {a.style.backgrond = "black"}});
+
+v.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {v.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {v.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {v.style.background = "green"} else {v.style.backgrond = "black"}});
+
+x.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {x.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {x.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {x.style.background = "green"} else {x.style.backgrond = "black"}});
+
+y.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {y.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {y.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {y.style.background = "green"} else {y.style.backgrond = "black"}});
+
+z.addEventListener("click",function() {if (absorverCor.style.backgroundColor === "red")  {z.style.backgroundColor = "red"} else if (absorverCor.style.backgroundColor === "yellow") {z.style.backgroundColor = "yellow"} 
+else if(absorverCor.style.backgroundColor === "green") {z.style.background = "green"} else {z.style.backgrond = "black"}});
+
+
+
+
 
