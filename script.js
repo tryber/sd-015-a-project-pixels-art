c9f949e932e99058b6af024d1d9b5433b2a66b67
GTmudin <testederealismo@gmail.com>
2021-08-19 15:34:08 -0300

    tentativa requisitos de classe

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let cores = (document.getElementsByClassName('color'));
 
 let tabela = (document.getElementsByClassName('pixel'));
 
-let selected = window.getComputedStyle(cores[0]).getPropertyValue('background-color')
 
 //codigo BRUTO
 
@@ -87,6 +86,9 @@ cores[0].onclick = function(){
     tabela[24].onclick = function(){
         tabela[24].style.backgroundColor = selected
     }
+    //adicionar classe 'selected'
+    cores[0].classList.add('selected')
+
 }
 cores[1].onclick = function(){
     let selected = window.getComputedStyle(cores[1]).getPropertyValue('background-color')
@@ -165,6 +167,9 @@ cores[1].onclick = function(){
     tabela[24].onclick = function(){
         tabela[24].style.backgroundColor = selected
     }
+    //adicionar a classe 'selected
+    cores[1].classList.add('selected')
+
 }
 cores[2].onclick = function(){
     let selected = window.getComputedStyle(cores[2]).getPropertyValue('background-color')
@@ -243,6 +248,9 @@ cores[2].onclick = function(){
     tabela[24].onclick = function(){
         tabela[24].style.backgroundColor = selected
     }
+    //adicionar a classe 'selected'
+    cores[2].classList.add('selected')
+
 }
 cores[3].onclick = function(){
     let selected = window.getComputedStyle(cores[3]).getPropertyValue('background-color')
@@ -321,7 +329,87 @@ cores[3].onclick = function(){
     tabela[24].onclick = function(){
         tabela[24].style.backgroundColor = selected
     }
+    //adicionar a classe 'selected'
+    cores[3].classList.add('selected')
+
 }
 
+let selected = window.getComputedStyle(cores[0]).getPropertyValue('background-color')
+//adicionar a classe 'selected'
+cores[0].classList.add('selected')
 
-
+tabela[0].onclick = function(){
+    tabela[0].style.backgroundColor = selected
+}
+tabela[1].onclick = function(){
+    tabela[1].style.backgroundColor = selected
+}
+tabela[2].onclick = function(){
+    tabela[2].style.backgroundColor = selected
+}
+tabela[3].onclick = function(){
+    tabela[3].style.backgroundColor = selected
+}
+tabela[4].onclick = function(){
+    tabela[4].style.backgroundColor = selected
+}
+tabela[5].onclick = function(){
+    tabela[5].style.backgroundColor = selected
+}
+tabela[6].onclick = function(){
+    tabela[6].style.backgroundColor = selected
+}
+tabela[7].onclick = function(){
+    tabela[7].style.backgroundColor = selected
+}
+tabela[8].onclick = function(){
+    tabela[8].style.backgroundColor = selected
+}
+tabela[9].onclick = function(){
+    tabela[9].style.backgroundColor = selected
+}
+tabela[10].onclick = function(){
+    tabela[10].style.backgroundColor = selected
+}
+tabela[11].onclick = function(){
+    tabela[11].style.backgroundColor = selected
+}
+tabela[12].onclick = function(){
+    tabela[12].style.backgroundColor = selected
+}
+tabela[13].onclick = function(){
+    tabela[13].style.backgroundColor = selected
+}
+tabela[14].onclick = function(){
+    tabela[14].style.backgroundColor = selected
+}
+tabela[15].onclick = function(){
+    tabela[15].style.backgroundColor = selected
+}
+tabela[16].onclick = function(){
+    tabela[16].style.backgroundColor = selected
+}
+tabela[17].onclick = function(){
+    tabela[17].style.backgroundColor = selected
+}
+tabela[18].onclick = function(){
+    tabela[18].style.backgroundColor = selected
+}
+tabela[19].onclick = function(){
+    tabela[19].style.backgroundColor = selected
+}
+tabela[20].onclick = function(){
+    tabela[20].style.backgroundColor = selected
+}
+tabela[21].onclick = function(){
+    tabela[21].style.backgroundColor = selected
+}
+tabela[22].onclick = function(){
+    tabela[22].style.backgroundColor = selected
+}
+tabela[23].onclick = function(){
+    tabela[23].style.backgroundColor = selected
+}
+tabela[24].onclick = function(){
+    tabela[24].style.backgroundColor = selected
+}
