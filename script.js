50f1df2b067bec441f7ba9cec5b51d3eb2cabe67
Arthur Simões <arthursimoes155@gmail.com>
2021-08-19 11:44:33 -0300

    Adiciona requisitos bonus

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const div = document.createElement("div");
-div.className = 'pixel';
+// Inícia a página com um quadro de pixels
+function createPixel(number) { 
 
-let divs = 5;
-let totalDivs = divs * divs
+    const div = document.createElement("div");
+    div.className = 'pixel';
 
-let fatherDiv = document.getElementById("pixel-board");
+    let fatherDiv = document.getElementById("pixel-board");
 
-for (let i = 0; i < divs; i += 1){
-    for (let index = 0; index < divs; index += 1){  
-        let div = document.createElement('div');
-        div.className = 'pixel';
-        fatherDiv.appendChild(div);
+    for (let i = 0; i < number; i += 1){
+        for (let index = 0; index < number; index += 1){  
+            let div = document.createElement('div');
+            div.className = 'pixel';
+            fatherDiv.appendChild(div);
+        }
+        let quebra = document.createElement('br');
+        fatherDiv.appendChild(quebra);
     }
-    let quebra = document.createElement('br');
-    fatherDiv.appendChild(quebra);
 }
+
+createPixel(5)
+
+//Muda a classe para a paleta de cor que eu clicar
+function paleta(){
 
 const black = document.getElementById("black")
 const blue = document.getElementById("blue")
@@ -33,25 +39,52 @@ blue.addEventListener("click", changeClass)
 red.addEventListener("click", changeClass)
 yellow.addEventListener("click", changeClass)
 
-let pixel = document.getElementById("pixel-board")
-
-pixel.addEventListener("click", changeColor)
-
-function changeColor(event) {
-    let selectedColor = document.getElementsByClassName("selected")[0]
-    event.target.style.backgroundColor = selectedColor.id
 }
 
+paleta()
+
+//Função para conseguir pintar o quadro de pixels com a cor que eu quero e também para pinta-las de branco quando eu clicar no button "limpar"
+function paintAndClean () {
+
+const pixelsNumber = document.getElementsByClassName("pixel")
 const button = document.getElementById("clear-board")
 
-function clearPixels() {
-    for (i = 0; i < totalDivs; i += 1) {
-    let teste = document.querySelectorAll(".pixel")[i]
-    teste.style.backgroundColor = "white"
+for (let i = 0; i < pixelsNumber.length; i += 1) {
+    pixelsNumber[i].addEventListener("click", function(event) {
+        let selectedColor = document.getElementsByClassName("selected")[0]
+        event.target.style.backgroundColor = selectedColor.id
+    })
+}
+
+button.addEventListener("click", function() {
+    for (let i = 0; i < pixelsNumber.length; i += 1) {
+        let clean = document.querySelectorAll(".pixel")[i]
+        clean.style.backgroundColor = "white"
+        }
+})
+}
+
+paintAndClean()     
+
+function cleanPixels () {
+    let fatherPixels = document.getElementById("pixel-board")
+    while (fatherPixels.hasChildNodes()) {  
+        fatherPixels.removeChild(fatherPixels.firstChild)
     }
 }
 
-button.addEventListener("click", clearPixels)
-
-        
-    
+const button = document.getElementById("generate-board")
+button.addEventListener("click", function() {
+    cleanPixels()
+    const input = document.getElementById("board-size")
+    if (input.value === "") {
+        alert('Board inválido!')
+    } else if (input.value >= 5 && input.value <= 50){
+        createPixel(input.value)
+    } else if (input.value < 5) {
+        createPixel(5)
+    } else if (input.value > 50) {
+        createPixel(50)
+    } 
+    paintAndClean()
+})
