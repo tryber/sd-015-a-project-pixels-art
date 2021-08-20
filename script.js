ba812fd656baac69b816d8aba27baa4114f55003
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-08-20 12:55:50 -0300

    teste

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Requisito 04 */
+function pixframe() {
+
+let pixelboard = document.getElementById("pixel-board");
+let newPixFrame = document.createAttribute("div");
+pixelboard.appendChild(newPixFrame);
+
+
+
+
+};
+
+pixframe();
