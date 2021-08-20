83f5ec1b6cdd39eba5204d028e8ca46b3dace2d3
João V. Dias <diasvictorj@gmail.com>
2021-08-20 11:53:07 -0300

    Atualizaçao de novos requisitos 2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
+/*Variavel para selecionar o elemento lista onde será feito o quadro*/
 let boardPlace = document.getElementById('boardList');
+
+/*variaveis que ditam o tamanho do quadro*/
 let altura = 5;
 let comprimento = 5;
 
+/* Função para criar o quadro*/
 for (let index = 0; index < altura; index += 1) {
     
     for (let index = 0; index < comprimento; index += 1) {
