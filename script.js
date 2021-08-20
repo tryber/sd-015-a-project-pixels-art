492da463217bf0cd04dd2960b6d9267bff99a8fa
Valdeci <moura97.valdeci@gmail.com>
2021-08-19 22:22:13 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ function firstSelection(){
   initialColor.classList.add('selected');
 };
 
-window.onload = firstSelection();
+firstSelection();
+
+function changeColorClass(event){
+  let colorSelected = document.querySelector('.selected');
+  colorSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+};
+
+  let c1 = document.querySelector('#c1');
+  c1.addEventListener('click', changeColorClass);
+  let c2 = document.querySelector('#c2');
+  c2.addEventListener('click', changeColorClass);
+  let c3 = document.querySelector('#c3');
+  c3.addEventListener('click', changeColorClass);
+  let c4 = document.querySelector('#c4');
+  c4.addEventListener('click', changeColorClass);
