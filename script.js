39093cec987bd712c11eb33e85616b461912a00f
antoniofortisneto <antoniofortis@gmail.com>
2021-08-20 08:09:58 -0300

    falta menos agora do que antes

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+window.onload
+//cria os div pixal
 for (let i = 0; i < 25; i += 1) {
   const caixa = document.createElement('cor');
   caixa.className = 'pixel';
   document.getElementById('pixel-board').appendChild(caixa);
+}
+let event = 
+// muda classe da cor para selecionado
+function mudaClasse() {
+  let corselecionada = document.getElementById("color-palette");
+  corselecionada.classList.add("selected");
+}
+
+function removeClasse(){
+  let cormudada = document.getElementById();
+  cormudada.classList.remove("Selected");
 }
