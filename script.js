fd1009a6ca3b369d0149c8a2e679aee9ee926e73
djsilva <coxcorp@gmail.com>
2021-08-19 22:58:19 -0300

    atualizaçao requisito 06

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 
 // Requisito 06 - Define a cor preta como padrão de seleção (selected)
+let selected = document.getElementById('color-palette');
+let qtdColors = selected.children.length;
+
+function clearSelected() {
+    for (let i = 0; i < qtdColors; i += 1) {
+        selected[i].classList.remove('selected');
+    }
+}
+
+function standardSelected() {
+    let standard = document.getElementById('color1');
+    standard.classList.add('selected');
+}
+
+standardSelected();
+
+// Requisito 07
+// selected.addEventListener('click', selectColor);
+
+// function selectColor(color) {
+//     let selectColor = color.target.id;
+//     return selectColor;
+// }
+
+// // Limpeza da classe selected
+// let listColors = selected.children;
+
+// for (i = 0;i < qtdColors; i += 1) {
+//     let comparedColor = listColors[i];
+//     let comparedColorId = comparedColor.id;
+//     if (selectColor !== comparedColorId) {
+//         if (comparedColor.classList.contains('selected')) {
+//             comparedColor.remove('selected');
+//         // } else {
+//         //     comparedColor.classList = 'selected';
+//         }
+//     }
+// // }
 
 
 
