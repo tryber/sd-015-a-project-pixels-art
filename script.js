15121803003459569dccfb8a694a94b8b6e34efc
Elias Paiva <elias.jpaiva@gmail.com>
2021-08-20 16:39:27 +0200

    requesito 8 completo

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-let colorBlack = document.querySelector(".black");
-let colorRed = document.querySelector(".red");
-let colorBlue = document.querySelector(".blue");
-let colorGreen = document.querySelector(".green");
+// Requesito 7
 let selectSection = document.querySelector('#color-palette');
 
 selectSection.addEventListener('click', (event) => {
-  console.log(document.querySelector('.selected').classList.remove("selected"));
-  console.log(event.target.classList.add("selected"));  
+  
+  if (event.target.className.includes ('color')){
+    console.log(document.querySelector('.selected').classList.remove("selected"));
+  console.log(event.target.classList.add("selected")); 
+  }
+  
 })
 
+// Requesito 8
+let selectMain = document.querySelector('#pixel-board');
+
+selectMain.addEventListener('click', (event) => {
+  let selected = document.querySelector('.selected');
+  let color = window.getComputedStyle(selected).getPropertyValue("background-color");
+
+  if (event.target.classList.contains('pixel')) {
+      event.target.style.backgroundColor = color;
+  }
+})
