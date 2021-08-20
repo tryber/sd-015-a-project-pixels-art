b9b37cc561c9e79a887bc6eaec673bd3d249ed49
João V. Dias <diasvictorj@gmail.com>
2021-08-20 20:14:25 -0300

    Setimo Requisito

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 /*Variavel para selecionar o elemento lista onde será feito o quadro*/
 let boardPlace = document.getElementById('boardList');
 
+/*variaveis para selecionar uma das cores disponiveis*/
+let escutaPreto = document.getElementById('black');
+let escutaBlue = document.getElementById('blue');
+let escutaPink = document.getElementById('pink');
+let escutaRed = document.getElementById('red');
+
+
+/*Inicializando a escuta de eventos no quadro*/
+boardPlace.addEventListener('click',pintarQuadro);
+
+/*Inicializando a escuta de eventos no quadro de cores*/
+escutaPreto.addEventListener('click',selecionaCor);
+escutaBlue.addEventListener('click',selecionaCor);
+escutaPink.addEventListener('click',selecionaCor);
+escutaRed.addEventListener('click',selecionaCor);
+
+
+
 /*variaveis que ditam o tamanho do quadro*/
 let altura = 5;
 let comprimento = 5;
@@ -16,4 +34,18 @@ for (let index = 0; index < altura; index += 1) {
         boardPlace.appendChild(pixel);
 
     }
+}
+
+/*Função para pintar o quadro com a cor selecionada*/
+function selecionaCor(event){
+
+    let corSelecionada = event.target;
+    let corAnterior = document.querySelector('.selected');
+    corAnterior.classList.remove('selected'); 
+    corSelecionada.classList.toggle('selected');
+}
+
+
+function pintarQuadro(){
+
 }
