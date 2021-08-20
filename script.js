c7835ed5d3116150d85bfc01d1a891f7967183b4
Elias Paiva <elias.jpaiva@gmail.com>
2021-08-20 15:12:00 +0200

    refazendo requesito 7

--- script.js
+++ script.js
@@ -5,17 +5,7 @@ let colorGreen = document.querySelector(".green");
 let selectSection = document.querySelector('#color-palette');
 
 selectSection.addEventListener('click', (event) => {
-  console.log(event.target.classList.add("selected"));
   console.log(document.querySelector('.selected').classList.remove("selected"));
-
-
-  if ( event.target.className.includes('color')) {
-      console.log('teste');
-  }
-
+  console.log(event.target.classList.add("selected"));  
 })
 
-// colorRed.addEventListener('click',() => {
-// colorRed.classList.add("selected");
-// console.log(colorRed);
-// })
