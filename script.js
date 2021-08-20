4735250b7aff7aa138acb2b7f25871808e97887d
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-08-20 15:41:05 -0300

    1 ao 5, teste

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 /* Requisito 04 */
-function pixframe() {
+/* function pixframe() {
 
 let pixelboard = document.getElementById("pixel-board");
-let newPixFrame = document.createAttribute("div");
-pixelboard.appendChild(newPixFrame);
-
-
-
+let newPixFrame = document.createElement("div");
+   pixelboard.appendChild(newPixFrame);
+   newPixFrame.className = "pixel"
 
 };
 
-pixframe();
+pixframe(); */
+
+/* função para verificar se a cor da paleta é branca */
+/* 
+function checkColor (){
+   let checkColorPallet = document.querySelectorAll(".pixel");
+
+   if (checkColorPallet  backgroundColor ="white") {
+      
+   }
+  
+  
+   console.log(checkColorPallet)
+}
+
+checkColor(); */
