fc121f11dd7e83a83234a79acbcf98c6bcab6514
guilhermeadelino <guilhermeadelino@live.com>
2021-08-19 21:41:08 -0300

    Requisito 7 e 8

---
+++ script.js
@@ -0,0 +1,34 @@
+let colocaClasse = document.getElementsByClassName('color')[0];
+colocaClasse.classList.add('selected')
+
+const clickColor = document.querySelectorAll('.color');
+for (let index = 0; index < clickColor.length; index += 1) {
+  clickColor[index].addEventListener('click', targetColor);
+}
+
+const selectedColor = document.getElementsByClassName('color');
+
+function removeSelect () {
+  for (let index = 0; index < selectedColor.length; index+= 1) {
+    selectedColor[index].classList.remove('selected')
+  }
+}
+
+function targetColor () {
+  removeSelect();
+  event.target.classList.add('selected');
+}
+
+const setBox = document.querySelectorAll('.pixel');
+
+for (let index = 0; index < setBox.length; index += 1) {
+  setBox[index].addEventListener('click', fillBoxPixel);
+}
+
+function fillBoxPixel (event) {
+  const boxPixel = event.target;
+  const selectedColor = document.querySelector('.selected')
+  const style = getComputedStyle(selectedColor)
+  boxPixel.style.backgroundColor = style.backgroundColor;
+}
+
