9a4556dff70b98bc3acebba7daea7447e63df52a
antoniofortisneto <antoniofortis@gmail.com>
2021-08-21 04:59:13 -0300

    eita

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-
-//aqui as divs nascem, e se fortalecem 
+window.onload
+//aqui as divs nascem, e se fortalecem para formar a tabela 5*5
 for (let i = 0; i < 25; i += 1) {
   const caixa = document.createElement('cor');
   caixa.className = 'pixel';
   document.getElementById('pixel-board').appendChild(caixa);
 }
 
-// muda classe da cor para selecionado
-function mudaClasse() {
-  const mudaClasse = document.querySelector('.selected');
-  mudaClasse.classList.remove('selected');
-  change.target.classList.add('selected');
-}
-
-// aqui declaro as variaveis a passo para transitar na funçao mudaclasse
-
+//a const paleta aponta a class color aqui para na funçao abaixo usar a magia da programaçao para adicionar o select e remover o antigo select
 const paleta = document.querySelectorAll('.color');
 function mudacor() {
   for (let i = 0; i < paleta.length; i += 1) {
-    paleta[i].classList.remove('selected');
+    paleta[i].classList.add('selected');
   }
 }
-
-function novacor(cor) {
-  mudacor();
-  cor.target.classList.add('selected');
-}
-
-for (let i = 0; i < paleta.length; i += 1) {
-  paleta[i].addEventListener('click', novacor);
-}
+//nessas linhas radicais aqui, temos um apontamento maravilindo para a cor preto, assim mudando a class dela :)
+ const corPreta = document.getElementById('color1');
+corPreta.classList.add('selected');
