557140839fb9c698eafccaaf3eab679b4d72fe12
JunioASilva <junioalves2005@hotmail.com>
2021-08-21 18:10:41 -0300

    att project

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.onload = function(){
 function paletaDeCores(event) {
 
     document.getElementsByClassName("color")[1].style.backgroundColor = "blue";
@@ -11,25 +12,24 @@ paletaDeCores();
 
 function classSelected() {
 
-    const elementSelected = document.querySelector(".color");
+    let elementSelected = document.querySelector(".color");
     elementSelected.classList.add("selected");
+    
+   
 }
 classSelected();
 
 function checkSelected() {
-    let selectedCheck = document.getElementsByTagName("div")[1];
-    for (let index = 0; index < selectedCheck.length; index += 1) {
-
-
-
-    }
+    let selectedCheck = document.querySelector(".selected");
+    
+    let addSelect = document.getElementsByTagName("section");
+    
+    addSelect.addEventListener("click", function(event){
+        if(document.querySelector(".selected")){
+            document.querySelector(".selected").classList.remove("selected");
+           
+           
+        }
+    })
 }
-checkSelected();
-
-
-function pixelBoard(selected, event) {
-
 }
-pixelBoard();
-
-
