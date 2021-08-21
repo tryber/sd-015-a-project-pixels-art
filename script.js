ec81d77c69aca3aa963bf3220f5b55c6647765b7
leonardo <leonardo.siepierski@outlook.com>
2021-08-21 18:17:25 -0300

    Tentativa de botao de limpar board

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let botaoApagar = document.getElementById('clear-board');
+let allPixels = document.getElementsByClassName('pixel');
+
 function clearBoard() {
-  let allPixels = document.querySelectorAll('.pixel');
   for (let index = 0; index < allPixels.length; index += 1) {
     allPixels[index].style.backgroundColor = 'white';
-  }
+  } 
 }
+
+botaoApagar.addEventListener('click', clearBoard);
