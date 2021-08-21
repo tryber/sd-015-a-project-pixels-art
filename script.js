03f4667f210c207aaf846e1f494bd018e4e12052
MoisésFigueira <moisesmichael@gmail.com>
2021-08-21 09:51:20 -0300

    exercise 8

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ const colors = document.querySelectorAll('.color');
 const pixels = document.querySelectorAll('.pixel');
 let color;
 let pixel;
+let paint = "black";
 
 function selectColor(e) {
     for (color of colors){
         color.classList.remove('selected');
     }
     e.target.classList.add('selected');
+    if (e.target.classList.contains('selected')) {
+        paint = e.target.innerText
+    }
 }
 
 function setColor(e) {
-    e.target.style.backgroundColor = 'black';
+    e.target.style.backgroundColor = paint;
 }
 
 for(color of colors) {
