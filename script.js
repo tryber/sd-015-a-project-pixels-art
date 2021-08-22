239b590b78b3fbc97ab57780c1aa63ec22f00bc7
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-08-22 17:23:04 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function addClass(event) {
   removeSelected.classList.remove('selected');
   event.target.classList.add('selected');
 }
+
+let changePaint = document.getElementsByClassName('paint');
+for (let index = 0; index < changePaint.length; index += 1) {
+  changePaint[index].addEventListener('click', addClass);
+}
+
+function pixelPaint(event) {
+  let elementSelected = document.querySelector('.selected');
+  let elementColor = window.getComputedStyle(elementSelected).backgroundColor;
+  let elementoClicado = event.target;
+
+
+}
+let pixelList = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixelList.length; index += 1) {
+  pixelList[index].addEventListener('click',  pixelPaint);
+}
