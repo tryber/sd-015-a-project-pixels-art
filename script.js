cad8beb5d37b6ee35033772e5b4a7e4bfb5ffc5a
Bernardocdr <bernardocdr@gmail.com>
2021-08-22 17:43:53 -0300

    Código reorganizado

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
-var color = document.getElementsByClassName("color");
+// Paleta de cores
 
-color.addEventListener
+
+////para cada cor, executar selecionarCor()
+let colors = document.querySelectorAll(".color");
+let selectedColor = document.getElementsByClassName("selected");
+
+
+colors.forEach(item => {
+  item.addEventListener('click', selecionarCor)
+})
+
+
+//selecionarCor
+function selecionarCor(e) {
+  
+  colors.forEach(color => {
+    color.classList.remove("selected");
+  } )
+
+  e.target.classList.add("selected")
+  let cor = e.target.style.background;
+  console.log(cor)
+}
+
+
+
+
+// Pixels
+
+
+//para cada pixel, executar pintarPixel()
+let pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach(item => {
+  item.addEventListener('click', pintarPixel)
+})
+
+
+//pintarPixel()
+function pintarPixel(event) {
+  event.target.innerHTML.backgroundColor = "black";
+ 
+  
+}
