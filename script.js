9603124f573a198d2e59edf462b6af62dd135293
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-08-21 23:21:57 -0300

    adequa lint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = firsFunction
+window.onload = firsFunction;
 
 function firsFunction() {
   createTable(5);
@@ -6,23 +6,23 @@ function firsFunction() {
   allPixels = document.querySelectorAll('.pixel');
   // gambiarra também pq declarei fora da função
   for (let index = 0; index < allPixels.length; index += 1) {
-    let selectedPixel = document.querySelectorAll('.pixel')[index];
+    const selectedPixel = document.querySelectorAll('.pixel')[index];
     selectedPixel.addEventListener('click', paintPixel);
   }
 }
 
 // Define o bloco 1 como selected
-let mainBlock = document.getElementById('block-one');
+const mainBlock = document.getElementById('block-one');
 mainBlock.classList.add('selected');
 
 // Cria tabela de pixels
 function createTable(boardSize) {
-  let pixelBoard = document.querySelector('#pixel-board');
+  const pixelBoard = document.querySelector('#pixel-board');
   for (let index = 1; index <= boardSize; index += 1) {
-    let pixelLine = document.createElement('div');
+    const pixelLine = document.createElement('div');
     pixelBoard.appendChild(pixelLine);
     for (let index2 = 1; index2 <= boardSize; index2 += 1) {
-      let pixelColumn = document.createElement('div');
+      const pixelColumn = document.createElement('div');
       pixelLine.appendChild(pixelColumn);
       pixelColumn.className = 'pixel';
     }
@@ -30,16 +30,16 @@ function createTable(boardSize) {
 }
 
 // Seleciona cor
-let allColors = document.querySelectorAll('.color');
+const allColors = document.querySelectorAll('.color');
 
 function selectColor(event) {
-  let selected = document.querySelector('.selected');
+  const selected = document.querySelector('.selected');
   selected.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
 for (let index = 0; index < allColors.length; index += 1) {
-  let selectedColor = document.querySelectorAll('.color')[index];
+  const selectedColor = document.querySelectorAll('.color')[index];
   selectedColor.addEventListener('click', selectColor);
 }
 
@@ -47,11 +47,11 @@ for (let index = 0; index < allColors.length; index += 1) {
 let definedColor = 'black';
 
 function defineColor() {
-  let selectedColor = document.querySelector('.selected');
+  const selectedColor = document.querySelector('.selected');
   definedColor = window.getComputedStyle(selectedColor).getPropertyValue('background-color');
 }
 
-let palette = document.querySelector('#color-palette');
+const palette = document.querySelector('#color-palette');
 palette.addEventListener('click', defineColor);
 
 // Pinta os pixels
@@ -62,12 +62,12 @@ function paintPixel(event) {
 }
 
 for (let index = 0; index < allPixels.length; index += 1) {
-  let selectedPixel = document.querySelectorAll('.pixel')[index];
+  const selectedPixel = document.querySelectorAll('.pixel')[index];
   selectedPixel.addEventListener('click', paintPixel);
 }
 
 // Limpa o quadro
-let buttonClear = document.querySelector('#clear-board');
+const buttonClear = document.querySelector('#clear-board');
 
 function clearBoard() {
   for (let index = 0; index < allPixels.length; index += 1) {
@@ -78,68 +78,68 @@ function clearBoard() {
 buttonClear.addEventListener('click', clearBoard);
 
 // Define tamanho do quadro
-let generateBoardButton = document.querySelector('#generate-board')
+const generateBoardButton = document.querySelector('#generate-board');
 
 function changeBoardSize() {
-  let inputValue = document.querySelector('#board-size').value;
-  let boardSize = 0
+  const inputValue = document.querySelector('#board-size').value;
+  let boardSize = 0;
 
   if (inputValue === '') {
-    alert('Board inválido!')
+    alert('Board inválido!');
 
-  } else if (inputValue > 50 ){
+  } else if (inputValue > 50) {
     // Limpa o board
     for (let index = 0; index < allPixels.length; index += 1) {
-      let pixelBoard = document.querySelector('.pixel');
+      const pixelBoard = document.querySelector('.pixel');
       pixelBoard.parentNode.removeChild(pixelBoard);
     }
 
-    boardSize = 50
-    createTable(boardSize)
+    boardSize = 50;
+    createTable(boardSize);
     // gambiarra pq declarei fora da função
     allPixels = document.querySelectorAll('.pixel');
     // gambiarra também pq declarei fora da função
     for (let index = 0; index < allPixels.length; index += 1) {
-      let selectedPixel = document.querySelectorAll('.pixel')[index];
+      const selectedPixel = document.querySelectorAll('.pixel')[index];
       selectedPixel.addEventListener('click', paintPixel);
     }
 
   } else if (inputValue < 5) {
     // Limpa o board
     for (let index = 0; index < allPixels.length; index += 1) {
-      let pixelBoard = document.querySelector('.pixel');
+      const pixelBoard = document.querySelector('.pixel');
       pixelBoard.parentNode.removeChild(pixelBoard);
     }
 
-    boardSize = 5
-    createTable(boardSize)
+    boardSize = 5;
+    createTable(boardSize);
 
     // gambiarra pq declarei fora da função
     allPixels = document.querySelectorAll('.pixel');
     // gambiarra também pq declarei fora da função
     for (let index = 0; index < allPixels.length; index += 1) {
-      let selectedPixel = document.querySelectorAll('.pixel')[index];
+      const selectedPixel = document.querySelectorAll('.pixel')[index];
       selectedPixel.addEventListener('click', paintPixel);
     }
 
   } else {
     // Limpa o board
     for (let index = 0; index < allPixels.length; index += 1) {
-      let pixelBoard = document.querySelector('.pixel');
+      const pixelBoard = document.querySelector('.pixel');
       pixelBoard.parentNode.removeChild(pixelBoard);
     }
 
-    boardSize = inputValue
-    createTable(boardSize)
+    boardSize = inputValue;
+    createTable(boardSize);
 
     // gambiarra pq declarei fora da função
     allPixels = document.querySelectorAll('.pixel');
     // gambiarra também pq declarei fora da função
     for (let index = 0; index < allPixels.length; index += 1) {
-      let selectedPixel = document.querySelectorAll('.pixel')[index];
+      const selectedPixel = document.querySelectorAll('.pixel')[index];
       selectedPixel.addEventListener('click', paintPixel);
     }
   }
 }
 
-generateBoardButton.addEventListener('click', changeBoardSize)
+generateBoardButton.addEventListener('click', changeBoardSize);
