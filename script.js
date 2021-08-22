f0abd6bab04dd94559e4a58d9cb37711ca8c15b4
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-08-21 21:34:43 -0300

    commit sabadao

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
+
+let pixelBoard = document.getElementById("pixel-board"); 
+let navPalette = document.getElementById('color-palette');
+let colorList = document.querySelectorAll('#color-palette div');
+let clearBoard = document.getElementById("clear-board");
 /* Requisito 04 */
-/* function pixframe() {
+/* 1-Selecionei elemento pixel-board/ 2-Denominei ao elemento uma classe/3-inclui ele de fato na div  *  */
+/* Requisito 05 1- Depois fiz um for para criar 25 caixas de pixels (comecei do 1)*/
 
-let pixelboard = document.getElementById("pixel-board");
-let newPixFrame = document.createElement("div");
-   pixelboard.appendChild(newPixFrame);
-   newPixFrame.className = "pixel"
+navPalette.addEventListener('click', selectColor);
 
-};
-
-pixframe(); */
-
-/* função para verificar se a cor da paleta é branca */
-/* 
-function checkColor (){
-   let checkColorPallet = document.querySelectorAll(".pixel");
-
-   if (checkColorPallet  backgroundColor ="white") {
-      
-   }
-  
-  
-   console.log(checkColorPallet)
+for(i =1; i <= 25 ; i += 1){
+let div = document.createElement("div"); 
+div.className = "pixel"; 
+pixelBoard.appendChild(div); 
 }
 
-checkColor(); */
+/* Requisito 07 */
+    
+      function removeSelected() {
+  for (let i = 0; i < colorList.length; i += 1) {
+    colorList[i].classList.remove('selected');
+  }
+}
+
+function selectColor(event) {
+  removeSelected();
+  event.target.classList.add('selected');
+}
+
+
+
