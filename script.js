c442f310f1640d0802bdec7df4e5f78ee5324e71
Raynan <raynanfernandes@yahoo.com.br>
2021-08-23 14:33:51 -0300

    Ate o requisito 9

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-primeiraCor.classList.add("selected");
+window.onload = function() {
+    let primeiraCor = document.querySelector(".color");
+    primeiraCor.classList.add("selected");
+}
 
-let segundaCor = document.querySelector("#segundaCor");
-let terceiraCor = document.querySelector("#terceiraCor");
-let quartaCor = document.querySelector("#quartaCor");
 let primeiraCor = document.querySelector(".primeiraCor");
 let segundaCor = document.querySelector(".segundaCor");
 let terceiraCor = document.querySelector(".terceiraCor");
 let quartaCor = document.querySelector(".quartaCor");
 
-primeiraCor.addEventListener("click",adicionarClasseSelected);
-segundaCor.addEventListener("click",adicionarClasseSelected);
-terceiraCor.addEventListener("click",adicionarClasseSelected);
-quartaCor.addEventListener("click",adicionarClasseSelected);
 function adicionarSelected (evento) {
     let eventoSelected = document.querySelector(".selected");
     eventoSelected.classList.remove("selected");
@@ -25,20 +21,9 @@ quartaCor.addEventListener("click",adicionarSelected);
 
 let pixelBoard = document.querySelector("#pixel-board");
 primeiraCor.style.backgroundColor = "black";
-segundaCor.style.backgroundColor = "blue";
-terceiraCor.style.backgroundColor = "chocolate";
-quartaCor.style.backgroundColor = "deeppink";
-pixelBoard.addEventListener('click', changeColor);
-let pixelBoard = document.getElementById("pixel-board");
-
-function adicionarClasseSelected (evento) {
-    let selected = document.querySelector(".selected");
-    selected.classList.remove("selected");
-    evento.target.classList.add("selected");
 segundaCor.style.backgroundColor = "red";
 terceiraCor.style.backgroundColor = "yellow";
 quartaCor.style.backgroundColor = "blue";
-}
 
 function colorirPixel (evento) {
     let corSelected = document.querySelector(".selected").style.backgroundColor;
@@ -46,11 +31,7 @@ function colorirPixel (evento) {
 }
 pixelBoard.addEventListener("click",colorirPixel);
 
-function changeColor(event) {
-    let colorSelected = document.querySelector(".selected").style.backgroundColor;
-    event.target.style.backgroundColor = colorSelected;
-}
-
+//Botao Clear:
 let main = document.querySelector("main");
 let botaoClear = document.createElement("button");
 botaoClear.innerHTML = "Limpar Tudo!";
@@ -65,4 +46,4 @@ function clearBoard () {
         pixel.style.backgroundColor = "white";
     }
 }
-botaoClear.addEventListener("click",clearBoard);
+botaoClear.addEventListener("click", clearBoard);
