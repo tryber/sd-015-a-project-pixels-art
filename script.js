7c08dbd0d66933ba027bf4b19036a1d6b12a2c3c
Davio <davioplp@gmail.com>
2021-08-23 01:25:59 -0300

    final

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Cria as cores aleatorias
 function criarCoresAleatorias() {
   const red = Math.round(Math.random() * 255);
   const green = Math.round(Math.random() * 255);
   const blue = Math.round(Math.random() * 255);
   return (`rgb(${red}, ${green}, ${blue})`);
 }
+
+// Coloca as cores criadas aleatoriamente na Paleta de cores e a cor preta como promeira cor.
 window.onload = function coresPaleta() {
   document.getElementById('cor1').style.backgroundColor = 'black';
   document.getElementById('cor2').style.backgroundColor = criarCoresAleatorias();
@@ -11,11 +14,13 @@ window.onload = function coresPaleta() {
   document.getElementById('cor4').style.backgroundColor = criarCoresAleatorias();
 };
 
+// Cria os 25 quadrados em braco para serem pintados.
 for (let i = 0; i < 25; i += 1) {
   const wh = document.createElement('cor');
   wh.className = 'pixel';
   document.getElementById('pixel-board').appendChild(wh);
 }
+
 // Ao carregar a pagina a cor preta é a primeira cor selecionada automatamente.
 const blackFirst = document.getElementById('cor1');
 blackFirst.classList.add('selected');
@@ -28,32 +33,33 @@ function changeSelected() {
   }
 }
 
+// Seleciona a nova cor na paleta de cores.
 function newSelected(cor) {
   changeSelected();
   cor.target.classList.add('selected');
 }
 
+// Adiciona o evento de click nas 4 cores da paleta de cores.
 for (let i = 0; i < paleta.length; i += 1) {
   paleta[i].addEventListener('click', newSelected);
 }
 
-const allQuads = document.querySelectorAll('.pixel');
-
+// Pinta o quadrado em braco com a cor selecionada na paleta de cores.
 function pintaPixel(quad) {
   const quadTarget = quad.target;
   quadTarget.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
 }
 
-for (let i = 0; i < allQuads.length; i += 1) {
-  allQuads[i].addEventListener('click', pintaPixel);
+// Adiciona o evento de click nos quadrados em branco.
+for (let i = 0; i < document.querySelectorAll('.pixel').length; i += 1) {
+  document.querySelectorAll('.pixel')[i].addEventListener('click', pintaPixel);
 }
 
-const clearPixelBoard = document.getElementById('clear-board');
-
+// Função que lipa todos os quadrados para a cor branca novamente.
 function buttonClear() {
   const allQuad = document.querySelectorAll('.pixel');
   for (let i = 0; i < allQuad.length; i += 1) {
     allQuad[i].style.backgroundColor = 'white';
   }
 }
-clearPixelBoard.addEventListener('click', buttonClear);
+document.getElementById('clear-board').addEventListener('click', buttonClear);
