ebc2af075d7b6cc2002dae910639be54165f535b
Pedro Figueiredo <pedrofigueiredo25@outlook.com>
2021-08-23 18:34:05 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.onload = function () {
     document.getElementById('black').classList.add('selected');
   } // Adiciona a cor preta como default selected
+
+  const colorPallete = document.getElementById("color-palette")
+
+  colorPallete.addEventListener("click", function(event){
+    for (let index = 0; index < colorPallete.children.length; index += 1){
+      colorPallete.children[index].classList.remove('selected')
+    }
+    event.target.classList.add("selected")
+  })
