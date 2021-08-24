babd1a254ac82e1b10b177e59814976ad9a6c9b8
Leandro Basilio <leandroab07@gmail.com>
2021-08-23 23:27:42 -0300

    Adiciona funcionalidades para que o tamanho da tabela de pixels possa ser gerado pelo usuário

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let paitingPixels = document.querySelector('#pixel-board');
 let colors = document.getElementsByClassName('color');
-let colorSelected = document.querySelectorAll('.color')
+let colorSelected = document.querySelectorAll('.color');
 let selected = document.querySelector('.selected');
 let pixels = document.getElementsByClassName('pixel');
+let send = document.getElementById('generate-board');
+let number = 5;
 
 function makeTable(number) {
   for (let i = 0; i < number; i += 1) {
@@ -18,7 +20,42 @@ function makeTable(number) {
   }
 }
 
-makeTable(5);
+makeTable(number);
+
+send.onclick = function() {
+  let newSize = document.getElementById('board-size').value;
+  if (newSize === '') {
+    window.alert("Board inválido!")
+  }
+  
+  if (newSize < 5) {
+    number = 5;
+  } else if (newSize > 50) {
+    number = 50;
+  } else {
+    number = newSize
+  }
+
+  for (let i = 0; i < paitingPixels.children.length; i = 0) {
+    paitingPixels.removeChild(paitingPixels.children[0]);
+  }
+  makeTable(number);
+
+  for (let i = 0; i < document.querySelectorAll('.pixel').length; i += 1) {
+  document.querySelectorAll('.pixel')[i].addEventListener('click', function() {
+    if (code === 0) {
+      color = 'black';
+    } else if (code === 1) {
+      color = 'blue';
+    } else if (code === 2) {
+      color = 'yellow';
+    } else if (code === 3) {
+      color = 'red';
+    }
+    event.target.style.backgroundColor = color;
+  });
+}
+}
 
 let code = 0;
 
