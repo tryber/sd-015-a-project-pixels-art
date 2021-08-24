cd08a8a4b3b306c68c69bd7a15bc6ed38fd852de
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-08-23 23:34:55 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const pixBoard = document.querySelector('#pixel-board');
 const paletaDeCores = document.querySelectorAll('.color');
+
 function criaTabela() {
   for (let coluna = 0; coluna < 5; coluna+=1) {
     let columnPixel = document.createElement('div')
@@ -23,11 +24,23 @@ function mudaSelected() {
   }
 }
 
-function adicionaSelected(cor) {
+function adicionaSelected(pixel) {
   mudaSelected();
-  cor.target.classList.add('selected');
+  pixel.target.classList.add('selected');
 }
 
+// Feito com a ajuda de colegas e monitoria
 for (let i = 0; i < paletaDeCores.length; i++) {
-  paletaDeCores[i].addEventListener('click', adicionaSelected)
+  paletaDeCores[i].addEventListener('click', adicionaSelected);
+}
+
+function coloriPixel(evento) {
+  let classSelected = document.querySelector('.selected')
+  selectedCor = window.getComputedStyle(classSelected, null).getPropertyValue('background-color');
+  evento.target.style.background = selectedCor;
+}
+
+const pixel = document.getElementsByClassName('pixel') 
+for (let index = 0; index < pixel.length; index+=1) {
+  pixel[index].addEventListener('click', coloriPixel);
 }
