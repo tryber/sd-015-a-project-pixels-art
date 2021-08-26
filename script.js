eab182f07fed8156e6ab3efbd63ffbcf14213dda
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2021-08-26 17:28:06 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-window.onload
+const pixelLines = document.getElementsByClassName('pixel-lines');
+
+function addPixelsLines() {
+    for (let index = 0; index < pixelLines.length; index += 1) {
+        for (let index2 = 0; index2 < 5; index2 += 1){
+            const pixel = document.createElement('div');
+            pixel.className = 'pixel';
+            pixelLines[index].appendChild(pixel);
+        } 
+    }    
+}
+addPixelsLines();
+
+
+
+
+//Requisito 8
+
+const boardPixel = document.getElementById('pixel-board')
+boardPixel.addEventListener('click', (event) => {
+    if(event.target.className === 'pixel') {
+        const colorCurrent = document.querySelector('.selected').id;
+        const colorSelected = event.target;
+        colorSelected.style.backgroundColor = colorCurrent; 
+    }
+})
