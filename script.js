22152adb6cf6b846d994880916ab4698918e6b0d
Gabriel Henrique <G3brielhfsouza@outlook.com>
2021-08-26 11:44:41 -0300

    requisito 4 refeito em javascript

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const colorPallete = document.querySelector('#color-palette');
+// const Pixel = document.querySelector('.pixel');
+const BOard = document.getElementById('pixel-board');
 
+const line = 5;
+const collum = 5;
+
+for (let index = 0; index < line; index += 1) {
+  for (let i = 0; i < collum; i += 1) {
+    const li = document.createElement('li');
+    li.classList.add('pixel');
+    BOard.appendChild(li);
+  }
+}
 
 function selectColor(event) {
-  const removeselcted = document.querySelector('.selected');
-  removeselcted.classList.remove('selected');
+  const removeselected = document.querySelector('.selected');
+  removeselected.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
 colorPallete.addEventListener('click', selectColor);
-
-
-function colorizeSquare(SQUaRE) {
-
-}
-
-
