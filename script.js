181bc962ded0f88b81affdea497ac1d3944d6b70
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-08-28 22:22:50 -0300

    requisito 7

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ for (let i = 0; i < 25; i += 1) {
   pixel.className = 'pixel';
   pixelBoard.appendChild(pixel);
 }
+
+function clickColor(clickEvent) {
+  const selection = document.querySelector('.selected');
+  selection.classList.remove('selected');
+  clickEvent.target.classList.add('selected');
+}
+
+const clickC = document.getElementById('color-palette');
+clickC.addEventListener('click', clickColor);
