25e34773c6ce77912507e3a87cd2110aeadaa065
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-08-30 17:04:28 -0300

    corrige e alinha tabela de pixels

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function createBox() {
+// função que cria a paleta de Cores
+function criaPaleta() {
   const colorArray = ['black', 'purple', 'orange', 'cyan'];
   for (let i = 0; i < colorArray.length; i += 1) {
     const colorDiv = document.createElement('div');
@@ -12,4 +13,20 @@ function createBox() {
   }
 }
 
-createBox();
+criaPaleta();
+
+// função que cria a tabela
+function criaTabela() {
+  for (let i = 0; i < 25; i += 1) {
+    const tablePixel = document.createElement('div');
+    const tableItem = document.createElement('li');
+    const parentElement = document.getElementById('pixel-board');
+
+    tablePixel.className = 'pixel';
+    tablePixel.style.backgroundColor = 'white';
+    parentElement.appendChild(tablePixel);
+    tablePixel.appendChild(tableItem);
+  }
+}
+
+criaTabela();
