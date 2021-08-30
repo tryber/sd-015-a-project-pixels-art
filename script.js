6013c8ae8bc26d499c910d28529db00807d3f7eb
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-08-30 18:43:39 -0300

    adiciona funçao que guardam os elementos da tabela de pixels

--- script.js
+++ script.js
@@ -25,3 +25,28 @@ function criaCoresPaleta(parent) {
 
 criaPaleta();
 criaCoresPaleta('color-palette');
+
+// Funções responsáveis pela criação dos elementos da tabela
+function criaPixel(parent, value) {
+  for (let i = 0; i < value; i += 1) {
+    const pixelDiv = document.createElement('div');
+    const pixelItem = document.createElement('li');
+    const parentElement = document.getElementById(parent);
+
+    pixelDiv.className = 'pixel';
+    pixelDiv.style.backgroundColor = 'white';
+    parentElement.appendChild(pixelDiv);
+    pixelDiv.appendChild(pixelItem);
+  }
+}
+
+function removePixel(value) {
+  for (let i = 0; i < value; i += 1) {
+    const selectAllPixel = document.getElementsByClassName('pixel');
+
+    selectAllPixel[0].remove();
+  }
+}
+
+criaPixel();
+removePixel();
