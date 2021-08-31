1b62b10748ffa0124d2196470342c02e1446bf3a
Istvan <olivaistvan@gmail.com>
2021-08-31 13:28:33 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const paleta = document.getElementById('color-palette');
 const cores = document.querySelectorAll('.color');
-const seleciona = document.getElementsByClassName('selected');
 const input = document.getElementById('board-size');
-const button = document.getElementById('generate-board')
+const button = document.getElementById('generate-board');
 
+// Alerta botão VQV
 button.addEventListener('click', function(){
     if(input.value === ''){
         alert('Board inválido!');
     }
 })
 
+// Requisito 07
+paleta.addEventListener('click', function(event){
+    let selecionada = document.querySelector('.selected');
+    let selecionar = event.target;
+    selecionada.classList.remove('selected');
+    selecionar.classList.add('selected')
+    console.log(selecionar)
+})
