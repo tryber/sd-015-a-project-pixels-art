769ca566101eb7c03257ba6ea871b9f6055abc59
Elias Paiva <elias.jpaiva@gmail.com>
2021-10-26 08:44:41 +0200

    feito até requesito 8. passou no teste local (npm)

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 // Requesito 7
-let selectSection = document.querySelector('#color-palette');
+const selectSection = document.querySelector('#color-palette');
 
 selectSection.addEventListener('click', (event) => {
-  
-  if (event.target.className.includes ('color')){
-    console.log(document.querySelector('.selected').classList.remove("selected"));
-  console.log(event.target.classList.add("selected")); 
+  if (event.target.className.includes('color')) {
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
   }
-  
-})
+});
 
 // Requesito 8
-let selectMain = document.querySelector('#pixel-board');
-
+const selectMain = document.querySelector('#pixel-board');
 selectMain.addEventListener('click', (event) => {
-  let selected = document.querySelector('.selected');
-  let color = window.getComputedStyle(selected).getPropertyValue("background-color");
-
+  const selected = document.querySelector('.selected');
+  const color = window.getComputedStyle(selected).getPropertyValue('background-color');
   if (event.target.classList.contains('pixel')) {
-      event.target.style.backgroundColor = color;
+    event.target.style.backgroundColor = color;
   }
-})
+});
 
 // Requesito 9
-let creatButton = document.createElement('button');
+const creatButton = document.createElement('button');
 creatButton.id = 'clear-board';
-creatButton.innerHTML = "Limpar";
-let selectDiv = document.querySelector('#creatButton');
+creatButton.innerHTML = 'Limpar';
+creatButton.style.marginTop = '40px';
+const selectDiv = document.querySelector('#creatButton');
 selectDiv.appendChild(creatButton);
 
 creatButton.addEventListener('click', (event) => {
-  let selectPixel = document.querySelectorAll('#pixel-board .pixel');
+  const selectPixel = document.querySelectorAll('#pixel-board .pixel');
   for (let i = 0; i < selectPixel.length; i++) {
-    let clearButton = selectPixel[i];
-    clearButton.style.backgroundColor = "white";
+    const clearButton = selectPixel[i];
+    clearButton.style.backgroundColor = 'white';
   }
-
-})
-
-
+});
