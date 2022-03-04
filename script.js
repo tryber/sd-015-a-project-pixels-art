7569e4f70187302d219cb4f8542628e82c757125
Anderson-Alencar <anderson23alencar@gmail.com>
2022-03-04 10:58:52 -0300

    prepara repositorio para git pessoal

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // construir o quadro de pixels
 const pixelBoard = document.querySelector('#pixel-board');
 
-function buildBoard (lines) {
+function buildBoard(lines) {
   for (let i = 0; i < lines; i += 1) {
-    let lineBoard = document.createElement('div');
+    const lineBoard = document.createElement('div');
     lineBoard.classList.add('line-board');
 
     for (let iPx = 0; iPx < lines; iPx += 1) {
-      let onePixel = document.createElement('div');
+      const onePixel = document.createElement('div');
       onePixel.classList.add('pixel');
 
       lineBoard.appendChild(onePixel);
@@ -18,36 +18,36 @@ function buildBoard (lines) {
 }
 
 buildBoard(5);
-//seleciona cor
-let selectedColor = document.querySelectorAll('.color');
+// seleciona cor
+const selectedColor = document.querySelectorAll('.color');
 
 function selectColor() {
-  for (let color of selectedColor) {
-    color.addEventListener('click', function() {
-      for (let color of selectedColor){
+  for (const color of selectedColor) {
+    color.addEventListener('click', () => {
+      for (const color of selectedColor) {
         color.classList.remove('selected');
-      } 
+      }
       color.classList.add('selected');
     });
   }
 }
 selectColor();
 // add cor selecionada ao pixel
-let pixels = document.querySelectorAll('.pixel');
+const pixels = document.querySelectorAll('.pixel');
 
 function paintPixel() {
-  for (let pixel of pixels) {
-    pixel.addEventListener('click', function(){
-      let actualSelected = document.querySelector('.selected');
+  for (const pixel of pixels) {
+    pixel.addEventListener('click', () => {
+      const actualSelected = document.querySelector('.selected');
       pixel.classList.remove('pink', 'blue', 'green', 'black');
-  
-      if (actualSelected === selectedColor[0]){
+
+      if (actualSelected === selectedColor[0]) {
         pixel.classList.add('black');
-      } else if (actualSelected === selectedColor[1]){
+      } else if (actualSelected === selectedColor[1]) {
         pixel.classList.add('pink');
-      } else if (actualSelected === selectedColor[2]){
+      } else if (actualSelected === selectedColor[2]) {
         pixel.classList.add('blue');
-      } else if (actualSelected === selectedColor[3]){
+      } else if (actualSelected === selectedColor[3]) {
         pixel.classList.add('green');
       }
     });
@@ -56,30 +56,32 @@ function paintPixel() {
 paintPixel();
 
 // botão limpar
-let clearButton = document.getElementById('clear-board');
-clearButton.addEventListener('click', clearBoard);
+const clearButton = document.getElementById('clear-board');
 
 function clearBoard() {
-  for (let pixel of pixels) {
+  for (const pixel of pixels) {
     pixel.classList.remove('black', 'pink', 'blue', 'green');
   }
 }
+
+clearButton.addEventListener('click', clearBoard);
 // escolher tamanho do quadrado
 const btnGenerator = document.querySelector('#generate-board');
 const boardSize = document.querySelector('#board-size');
-btnGenerator.addEventListener('click', generateBoard);
 
 function generateBoard() {
   if (boardSize.value === '') {
     alert('Board inválido!');
   } else if (boardSize.value > 50) {
-    pixelBoard.innerHTML = "";
-    buildBoard(50);  
+    pixelBoard.innerHTML = '';
+    buildBoard(50);
   } else if (boardSize.value < 5) {
-    pixelBoard.innerHTML = "";
-    buildBoard(5);  
+    pixelBoard.innerHTML = '';
+    buildBoard(5);
   } else {
-    pixelBoard.innerHTML = "";
-    buildBoard(boardSize.value);  
-  }  
+    pixelBoard.innerHTML = '';
+    buildBoard(boardSize.value);
+  }
 }
+
+btnGenerator.addEventListener('click', generateBoard);
